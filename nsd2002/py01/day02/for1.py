3f83b967b43e53b0afb5314c036961d4d764cd9b
zhangzhg <zhangzg@tedu.cn>
2020-07-15 17:04:20 +0800

    nsd2002 py01 day02 17:00

---
+++ nsd2002/py01/day02/for1.py
@@ -0,0 +1,16 @@
+s1 = 'abc'
+l1 = ['tom', 'jerry', 'jack', 'rose']
+t1 = (10, 20, 30)
+d1 = {'name': 'nb', 'age': 20}
+
+for zifu in s1:
+    print(zifu)
+
+for name in l1:
+    print(name)
+
+for i in t1:
+    print(i)
+
+for key in d1:
+    print(key, d1[key])
