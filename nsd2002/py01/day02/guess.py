925aac39ad49414bfc21d6e6499d565fbcb22e10
zhangzhg <zhangzg@tedu.cn>
2020-07-15 16:48:39 +0800

    nsd2002 py01 day02 16:50

---
+++ nsd2002/py01/day02/guess.py
@@ -0,0 +1,17 @@
+import random
+
+num = random.randint(1, 100)
+i = 0
+
+while i < 7:
+    i += 1
+    answer = int(input("number: "))
+    if answer > num:
+        print('猜大了')
+    elif answer < num:
+        print('猜小了')
+    else:
+        print('猜对了')
+        break
+else:
+    print('正确答案是: ', num)
