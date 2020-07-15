dfdf6629eeef30fe3ba5e477e7e568ce870e630f
zhangzhg <zhangzg@tedu.cn>
2020-07-15 11:42:23 +0800

    nsd2002 py01 day02 11:45

---
+++ nsd2002/py01/day02/grade.py
@@ -0,0 +1,12 @@
+fenshu = input('分数:')
+
+if int(fenshu) >= 90:
+    print('优秀')
+elif int(fenshu) >= 80:
+    print('good')
+elif int(fenshu) >= 70:
+    print('良')
+elif int(fenshu) >= 60:
+    print('及格')
+else:
+    print('你要努力了')
