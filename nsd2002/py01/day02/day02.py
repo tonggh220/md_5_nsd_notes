f0b309a7ec1743245363622f7f3e181153405335
zhangzhg <zhangzg@tedu.cn>
2020-07-15 10:43:04 +0800

    nsd2002 py01 day02 10:40

--- nsd2002/py01/day02/day02.py
+++ nsd2002/py01/day02/day02.py
@@ -9,17 +9,28 @@
 # else:
 #     print('no')
 
-if -0.0:
-    print('值为0，假')
+# if -0.0:
+#     print('值为0，假')
+#
+# if ' ':
+#     print('空格也是一个字符，为真')
+#
+# if '':
+#     print('空字符串，假')
+#
+# if [10, 20]:
+#     print('非空列表，为真')
+#
+# if (10, 20):
+#     print('非空元组，为真')
 
-if ' ':
-    print('空格也是一个字符，为真')
+a = 10
+b = 20
+if a < b:
+    s = a
+else:
+    s = b
+print(s)
 
-if '':
-    print('空字符串，假')
-
-if [10, 20]:
-    print('非空列表，为真')
-
-if (10, 20):
-    print('非空元组，为真')
+s1 = a if a < b else b
+print(s1)
