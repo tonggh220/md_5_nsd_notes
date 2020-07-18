89cf36064476b228ca57f294a6ce2cdfe703bbe5
zhangzhg <zhangzg@tedu.cn>
2020-07-18 15:53:24 +0800

    nsd2002 py01 day04 15:50

--- nsd2002/py01/day04/day04.py
+++ nsd2002/py01/day04/day04.py
@@ -1,3 +1,10 @@
 s1 = 'Python'
-sorted(s1)
 
+# for i in range(len(s1)):  # [0, 1, 2, 3, 4, 5]
+#     print(i, s1[i])
+
+for data in enumerate(s1):
+    print(data)  # data是由下标和下标对应字符构成的元组
+
+for i, ch in enumerate(s1):
+    print(i, ch)  # 可以将元组中的两项内容分别赋值给两个变量
