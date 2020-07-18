00f618871018803d2533d7eef1aa2c27d600a01e
zhangzhg <zhangzg@tedu.cn>
2020-07-18 17:43:58 +0800

    nsd2002 py01 day04 17:40

--- nsd2002/py01/day04/day04.py
+++ nsd2002/py01/day04/day04.py
@@ -1,10 +1,18 @@
-s1 = 'Python'
+# s1 = 'Python'
 
 # for i in range(len(s1)):  # [0, 1, 2, 3, 4, 5]
 #     print(i, s1[i])
 
-for data in enumerate(s1):
-    print(data)  # data是由下标和下标对应字符构成的元组
+# for data in enumerate(s1):
+#     print(data)  # data是由下标和下标对应字符构成的元组
+#
+# for i, ch in enumerate(s1):
+#     print(i, ch)  # 可以将元组中的两项内容分别赋值给两个变量
 
-for i, ch in enumerate(s1):
-    print(i, ch)  # 可以将元组中的两项内容分别赋值给两个变量
+s1 = '123a45'
+for ch in s1:
+    if ch not in '1234567890':
+        print('no')
+        break
+else:
+    print('yes')
