e98dea546a48dfbc98d72b69ebb9dd519f644df4
zhangzhg <zhangzg@tedu.cn>
2020-07-20 16:21:26 +0800

    nsd2002 py01 day05 16:20

---
+++ nsd2002/py01/day05/userdb.py
@@ -0,0 +1,28 @@
+def register():
+    "注册"
+
+def login():
+    "登陆"
+
+def show_menu():
+    "显示菜单"
+    funcs = {'0': register, '1': login}
+    prompt = """(0) 注册
+(1) 登陆
+(2) 退出
+请选择(0/1/2): """
+
+    while 1:
+        choice = input(prompt).strip()
+        if choice not in ['0', '1', '2']:
+            print('无效的输入，请重试。')
+            continue
+
+        if choice == '2':
+            print('Bye-bye')
+            break
+
+        funcs[choice]()
+
+if __name__ == '__main__':
+    show_menu()
