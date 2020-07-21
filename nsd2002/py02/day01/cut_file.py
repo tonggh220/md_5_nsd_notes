5acf77f88a98cd61d92392c35f76d42444599e77
zhangzhg <zhangzg@tedu.cn>
2020-07-21 10:39:55 +0800

    nsd2002 py02 day01 10:40

--- nsd2002/py02/day01/cut_file.py
+++ nsd2002/py02/day01/cut_file.py
@@ -7,5 +7,13 @@ t12 = time.strptime('2019-05-15 12:00:00', '%Y-%m-%d %H:%M:%S')
 with open(logfile) as fobj:
     for line in fobj:
         t = time.strptime(line[:19], '%Y-%m-%d %H:%M:%S')
-        if t9 <= t <= t12:
+        if t > t12:
+            break
+        if t >= t9:
             print(line, end='')
+
+# with open(logfile) as fobj:
+#     for line in fobj:
+#         t = time.strptime(line[:19], '%Y-%m-%d %H:%M:%S')
+#         if t9 <= t <= t12:
+#             print(line, end='')
