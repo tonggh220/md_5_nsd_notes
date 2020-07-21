6e588c457dd069a299aa1ffa10b0f7095881a0cd
zhangzhg <zhangzg@tedu.cn>
2020-07-21 11:56:34 +0800

    nsd2002 py02 day01 12:00

--- nsd2002/py02/day01/mydiv.py
+++ nsd2002/py02/day01/mydiv.py
@@ -1,4 +1,33 @@
-n = int(input('number: '))
-result = 100 / n
-print(result)
-print('Done')
+# try:
+#     n = int(input('number: '))
+#     result = 100 / n
+#     print(result)
+#     print('Done')
+# except ValueError:
+#     print('只接受非0数字')
+# except ZeroDivisionError:
+#     print('只接受非0数字')
+# except KeyboardInterrupt:
+#     print('\nBye-bye')
+# except EOFError:
+#     print('\nBye-bye')
+
+# try:
+#     n = int(input('number: '))
+#     result = 100 / n
+#     print(result)
+#     print('Done')
+# except (ValueError, ZeroDivisionError):
+#     print('只接受非0数字')
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+
+try:
+    n = int(input('number: '))
+    result = 100 / n
+    print(result)
+    print('Done')
+except (ValueError, ZeroDivisionError) as e:  # 将异常保存为变量e
+    print('只接受非0数字: ', e)
+except (KeyboardInterrupt, EOFError):
+    print('\nBye-bye')
