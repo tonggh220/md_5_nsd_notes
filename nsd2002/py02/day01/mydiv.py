2266de98b80204fa184cc93ea05821a2f2151946
zhangzhg <zhangzg@tedu.cn>
2020-07-21 14:16:51 +0800

    nsd2002 py02 day01 14:20

--- nsd2002/py02/day01/mydiv.py
+++ nsd2002/py02/day01/mydiv.py
@@ -22,12 +22,37 @@
 # except (KeyboardInterrupt, EOFError):
 #     print('\nBye-bye')
 
+# try:
+#     n = int(input('number: '))
+#     result = 100 / n
+#     print(result)
+#     print('Done')
+# except (ValueError, ZeroDivisionError) as e:  # 将异常保存为变量e
+#     print('只接受非0数字: ', e)
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+
+# try:
+#     n = int(input('number: '))
+#     result = 100 / n
+# except (ValueError, ZeroDivisionError) as e:  # 将异常保存为变量e
+#     print('只接受非0数字: ', e)
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+# else:
+#     print(result)
+#
+# print('Done')
+
 try:
     n = int(input('number: '))
     result = 100 / n
-    print(result)
-    print('Done')
 except (ValueError, ZeroDivisionError) as e:  # 将异常保存为变量e
     print('只接受非0数字: ', e)
 except (KeyboardInterrupt, EOFError):
     print('\nBye-bye')
+    exit(101)  # 程序遇到exit函数将会彻底结束
+else:
+    print(result)
+finally:
+    print('Done')
