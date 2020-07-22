3116e49ae7a8d56d5ee726ea3cde9378e0836386
zhangzhg <zhangzg@tedu.cn>
2020-07-22 15:46:28 +0800

    nsd2002 py02 day02 15:50

--- nsd2002/py02/day02/day02.py
+++ nsd2002/py02/day02/day02.py
@@ -8,8 +8,17 @@
 # if __name__ == '__main__':
 #     func1()
 
+from random import randint
 
+def func1(x):
+    # 整数求余，返回值只有1或0。1为真，0为假
+    return x % 2
 
-
-
+if __name__ == '__main__':
+    nums = [randint(1, 100) for i in range(10)]
+    result1 = filter(func1, nums)
+    result2 = filter(lambda x: x % 2, nums)
+    print(nums)
+    print(list(result1))
+    print(list(result2))
 
