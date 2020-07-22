6ea5ac253d66ed18f35efeba773aadd4d94ed2cb
zhangzhg <zhangzg@tedu.cn>
2020-07-22 11:30:08 +0800

    nsd2002 py02 day02 11:30

--- nsd2002/py02/day02/day02.py
+++ nsd2002/py02/day02/day02.py
@@ -1,9 +1,15 @@
-def func1():
-    print('Func 1')
-    func2()
+# def func1():
+#     print('Func 1')
+#     func2()
+#
+# def func2():
+#     print('This is func2')
+#
+# if __name__ == '__main__':
+#     func1()
 
-def func2():
-    print('This is func2')
 
-if __name__ == '__main__':
-    func1()
+
+
+
+
