0c911274cb7aa24f99e0a10bbf7f6372521e10e9
zhangzhg <zhangzg@tedu.cn>
2020-07-22 14:33:02 +0800

    nsd2002 py02 day02 14:30

--- nsd2002/py02/day02/math_game.py
+++ nsd2002/py02/day02/math_game.py
@@ -11,9 +11,32 @@
 # Continue(y/n)? No
 # Bye-bye
 
+from random import randint, choice
+
 def exam():
     "出题，用户作答"
-    print('exam')
+    # 随机生成2个数字
+    nums = [randint(1, 100) for i in range(2)]
+    nums.sort(reverse=True)  # 降序排列
+
+    # 随机选择加减法
+    op = choice('+-')
+
+    # 计算出标准答案
+    if op == '+':
+        result = nums[0] + nums[1]
+    else:
+        result = nums[0] - nums[1]
+
+    # 用户作答
+    prompt = '%s %s %s = ' % (nums[0], op, nums[1])
+    answer = int(input(prompt))
+    if answer == result:
+        print('不错哟!!!')
+    else:
+        print('不对哟!!!')
+
+    print('Answer: %s%s' % (prompt, result))
 
 def main():
     "程序的代码逻辑"
