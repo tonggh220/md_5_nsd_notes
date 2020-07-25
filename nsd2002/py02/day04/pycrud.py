37760ffd7414ef8d78118d54be106fc477a07f86
zhangzhg <zhangzg@tedu.cn>
2020-07-25 14:55:32 +0800

    nsd2002 py02 day04 14:50

--- nsd2002/py02/day04/pycrud.py
+++ nsd2002/py02/day04/pycrud.py
@@ -11,11 +11,31 @@ conn = pymysql.connect(
 )
 cur = conn.cursor()
 #################################
-insert1 = 'INSERT INTO employees VALUES (%s, %s, %s)'
+# insert1 = 'INSERT INTO employees VALUES (%s, %s, %s)'
 # 插入一行数据
-cur.execute(insert1, (1, 'tom', 'tom@tedu.cn'))
+# cur.execute(insert1, (1, 'tom', 'tom@tedu.cn'))
 # 插入多行数据
-cur.executemany(insert1, [(2, 'jerry', 'jerry@tedu.cn'), (3, 'jack', 'jack@qq.com'), (4, 'rose', 'rose@163.com')])
+# cur.executemany(insert1, [(2, 'jerry', 'jerry@tedu.cn'), (3, 'jack', 'jack@qq.com'), (4, 'rose', 'rose@163.com')])
+
+# 查询
+# select1 = 'SELECT id, name, email FROM employees'
+# cur.execute(select1)
+# result1 = cur.fetchone()    # 取出一个记录
+# result2 = cur.fetchmany(2)  # 继续取出2个记录
+# result3 = cur.fetchall()    # 取出剩余全部记录
+# print(result1)
+# print('*' * 30)
+# print(result2)
+# print('*' * 30)
+# print(result3)
+
+# 更新
+# update1 = 'UPDATE employees SET email=%s WHERE name=%s'
+# cur.execute(update1, ('tom@tarena.com', 'tom'))
+
+# 删除
+delete1 = 'DELETE FROM employees WHERE id=%s'
+cur.execute(delete1, (4,))
 
 #################################
 conn.commit()
