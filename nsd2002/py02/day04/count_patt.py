058b2cec289c1683383eae84752dc7fae8ad877d
zhangzhg <zhangzg@tedu.cn>
2020-07-25 09:58:32 +0800

    nsd2002 py02 day04 10:00

--- nsd2002/py02/day04/count_patt.py
+++ nsd2002/py02/day04/count_patt.py
@@ -1,11 +1,25 @@
+import re
 
 def count_patt(fname, patt):
+    # 为了有更好的匹配效率，先将模式编译
+    cpatt = re.compile(patt)
+    # 创建用于保存结果的变量
+    patt_dict = {}
 
+    # 遍历文件，在每一行找到模式，并统计出现的次数
+    with open(fname) as fobj:
+        for line in fobj:
+            m = cpatt.search(line)
+            if m:  # 如果匹配到了，返回匹配对象，非空对象为真
+                key = m.group()
+                patt_dict[key] = patt_dict.get(key, 0) + 1
+
+    return patt_dict
 
 if __name__ == '__main__':
     fname = 'access_log'
-    ip = ''
-    br = ''
+    ip = '^(\d+\.){3}\d+'  # 1234.56789.1.10  123.12.18.3
+    br = 'Chrome|Firefox|MSIE'
     result1 = count_patt(fname, ip)
     result2 = count_patt(fname, br)
     print(result1)
