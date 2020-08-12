f0ba7a19e72a7521f652e3c698e5d3ecd0925e50
zhangzhg <zhangzg@tedu.cn>
2020-08-12 11:46:21 +0800

    nsd2003 py01 day02 11:50

--- nsd2003/py01/day02/login2.py
+++ nsd2003/py01/day02/login2.py
@@ -4,6 +4,6 @@ username = input('username: ')
 password = getpass.getpass('password: ')
 
 if username == 'bob' and password == '123456':
-    print('登陆成功')
+    print('\033[32;1m登陆成功\033[0m')
 else:
-    print('登陆失败')
+    print('\033[31;1m登陆失败\033[0m')
