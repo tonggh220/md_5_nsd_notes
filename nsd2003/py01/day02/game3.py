320b57025dc9546fa243a606a3b65b689c60b327
zhangzhg <zhangzg@tedu.cn>
2020-08-12 14:59:18 +0800

    nsd2003 py01 day02 15:00

---
+++ nsd2003/py01/day02/game3.py
@@ -0,0 +1,24 @@
+import random
+
+# 定义所有可用的选择
+all_choice = ['石头', '剪刀', '布']
+# 定义人赢的情况
+win_list = [['石头', '剪刀'], ['剪刀', '布'], ['布', '石头']]
+prompt = """(0) 石头
+(1) 剪刀
+(2) 布
+请选择(0/1/2): """
+# 人机分别出拳
+computer = random.choice(all_choice)
+ind = int(input(prompt))  # 用户输入的数字正好是all_choice的下标
+player = all_choice[ind]
+
+# 输出人机选择
+print('计算机出了:%s，你出了:%s' % (computer, player))
+# 判断输赢
+if player == computer:
+    print('\033[32;1m平局\033[0m')
+elif [player, computer] in win_list:
+    print('\033[31;1mYou WIN!!!\033[0m')
+else:
+    print('\033[31;1mYou LOSE!!!\033[0m')
