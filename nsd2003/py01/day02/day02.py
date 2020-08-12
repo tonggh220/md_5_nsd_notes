ae69db39acd3237f3866cd6e551974b8ca3d9845
zhangzhg <zhangzg@tedu.cn>
2020-08-12 10:12:51 +0800

    nsd2003 py01 day02 10:10

--- nsd2003/py01/day02/day02.py
+++ nsd2003/py01/day02/day02.py
@@ -1,6 +1,26 @@
-if 3 > 0:
-    print('yes')
+# if 3 > 0:
+#     print('yes')
+#
+# s1 = 'python'
+# if 'th' in s1:
+#     print('ok')
 
-s1 = 'python'
-if 'th' in s1:
-    print('ok')
+if -0.0:
+    print('值为0的数字，假')
+
+if ' ':
+    print('空格也是一个字符，为真')
+
+if [1, 2, 3]:
+    print('非空列表，为真')
+
+if ():
+    print('空元组，为假')
+
+if {'name': 'nb'}:
+    print('非空字典，为真')
+
+if not None:
+    print('None相当于其他语言的null，为假，取反为真')
+
+
