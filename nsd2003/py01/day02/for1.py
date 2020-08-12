d97188ab0f1f1af3d251687d84d0e896bcca7491
zhangzhg <zhangzg@tedu.cn>
2020-08-12 17:17:14 +0800

    nsd2003 py01 day02 17:20

---
+++ nsd2003/py01/day02/for1.py
@@ -0,0 +1,15 @@
+# s1 = 'python'
+# for ch in s1:
+#     print(ch)
+
+# l1 = ['tom', 'jerry']
+# for name in l1:
+#     print(name)
+
+# t1 = (10, 5, 8, 2)
+# for i in t1:
+#     print(i)
+
+d1 = {'name': 'nb', 'age': 20}
+for key in d1:
+    print(key, d1[key])
