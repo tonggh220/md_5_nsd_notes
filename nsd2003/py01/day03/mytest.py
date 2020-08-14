81a4c2c7d2a361f8c31e06c0773b843ecbb28993
zhangzhg <zhangzg@tedu.cn>
2020-08-14 17:49:51 +0800

    nsd2003 py01 day03 17:50

--- nsd2003/py01/day03/mytest.py
+++ nsd2003/py01/day03/mytest.py
@@ -1,4 +1,6 @@
 import star
 
-print(star.hi)
-star.pstar()
+# print(star.hi)
+# star.pstar()
+
+
--- nsd2003/py01/day03/star.py
+++ nsd2003/py01/day03/star.py
@@ -2,3 +2,8 @@ hi = 'Hello World!'
 
 def pstar(n=30):
     print('*' * n)
+
+if __name__ == '__main__':
+    pstar(50)
+    print(hi)
+    pstar(50)
