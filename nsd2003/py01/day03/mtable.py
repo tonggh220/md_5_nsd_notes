c023a66e65725509c4d5c2a09807ec6e5f79d378
zhangzhg <zhangzg@tedu.cn>
2020-08-14 09:39:08 +0800

    nsd2003 py01 day03 9:40

--- nsd2003/py01/day03/mtable.py
+++ nsd2003/py01/day03/mtable.py
@@ -8,7 +8,18 @@
 # print()  # 打印一行hello后，打印\n
 
 # 将上面的代码作为一个整体，执行3遍
-for i in range(1, 4):
-    for j in range(1, 4):
-        print('hello', end='\t')
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print('hello', end='\t')
+#     print()
+
+# 第一行打印1个hello，第i行打印i个Hello
+# for i in range(1, 4):
+#     for j in range(1, i + 1):
+#         print('hello', end='\t')
+#     print()
+
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print('%sx%s=%s' % (j, i, i * j), end='\t')
     print()
