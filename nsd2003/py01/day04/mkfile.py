cfe26b0a91df201d31b269d216338c53d2036ca4
zhangzhg <zhangzg@tedu.cn>
2020-08-15 14:09:53 +0800

    nsd2003 py01 day04 14:10

---
+++ nsd2003/py01/day04/mkfile.py
@@ -0,0 +1,25 @@
+import os
+
+def get_fname():
+    "用于获取一个系统中不存在的文件名"
+    while 1:
+        fname = input('文件名: ')
+        if not os.path.exists(fname):
+            break
+        print('文件已存在，请重试。')
+
+    return fname
+
+def get_content():
+    "用于获取用户输入的多行文本"
+
+def wfile(fname, content):
+    "用于将内容content写入文件fname"
+
+if __name__ == '__main__':
+    # 获取文件名
+    fname = get_fname()
+    # 获取内容
+    content = get_content()
+    # 将内容与到文件
+    wfile(fname, content)
