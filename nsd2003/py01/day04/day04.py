00ac8be904049c7e7b3f372df6ed293d23a39d53
zhangzhg <zhangzg@tedu.cn>
2020-08-15 15:25:13 +0800

    nsd2003 py01 day04 15:30

--- nsd2003/py01/day04/day04.py
+++ nsd2003/py01/day04/day04.py
@@ -1,3 +1,9 @@
-import shutil
-shutil.copyfileobj()
-shutil.copy()
+s1 = 'Python'
+
+print(list(enumerate(s1)))
+
+for data in enumerate(s1):
+    print(data)
+
+for i, ch in enumerate(s1):
+    print(i, ch)
