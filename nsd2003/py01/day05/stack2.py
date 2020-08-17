6f6d530ba2a4cf5049333e43de63fa6f65c2faf5
zhangzhg <zhangzg@tedu.cn>
2020-08-17 12:01:21 +0800

    nsd2003 py01 day05 12：00

---
+++ nsd2003/py01/day05/stack2.py
@@ -0,0 +1,35 @@
+def push_it():
+    "用于压栈"
+    print('push')
+
+def pop_it():
+    "用于出栈"
+    print('pop')
+
+def view_it():
+    "查询"
+    print('view')
+
+def show_menu():
+    "用于显示菜单，实现代码逻辑"
+    funcs = {'0': push_it, '1': pop_it, '2': view_it}  # 将函数存入字典
+    prompt = """(0) 压栈
+(1) 出栈
+(2) 查询
+(3) 退出
+请选择(0/1/2/3): """
+    while 1:
+        choice = input(prompt).strip()  # 清除字符串两边额外的空格
+        if choice not in ['0', '1', '2', '3']:
+            print('无效的输入，请重试。')
+            continue
+
+        if choice == '3':
+            print('Bye-bye')
+            break
+
+        funcs[choice]()  # 在字典中取出相应的函数并调用
+
+
+if __name__ == '__main__':
+    show_menu()
