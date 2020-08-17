da8cbe0c2a6bc6aa8294e1e6db795f3a4e64fd31
zhangzhg <zhangzg@tedu.cn>
2020-08-17 10:51:39 +0800

    nsd2003 py01 day05 10:50

--- nsd2003/py01/day05/day05.py
+++ nsd2003/py01/day05/day05.py
@@ -1,4 +1,4 @@
-s1 = 'Python'
+# s1 = 'Python'
 
 # for i in [0, 1, 2, 3, 4, 5]:
 #     print(i, s1[i])
@@ -6,5 +6,28 @@ s1 = 'Python'
 # for i in range(6):
 #     print(i, s1[i])
 
-for i in range(len(s1)):
-    print(i, s1[i])
+# for i in range(len(s1)):
+#     print(i, s1[i])
+
+# l1 = [10, 15, 20, 10, 30, 10, 4, 10]
+# for i in range(len(l1)):
+#     if 10 in l1:
+#         l1.remove(10)
+#     else:
+#         break
+# print(l1)
+
+# l1 = [10, 15, 20, 10, 30, 10, 4, 10]
+# for i in range(len(l1)):
+#     if 10 not in l1:
+#         break
+#     l1.remove(10)
+# print(l1)
+
+l1 = [10, 15, 20, 10, 30, 10, 4, 10]
+while 1:
+    if 10 not in l1:
+        break
+    l1.remove(10)
+print(l1)
+
