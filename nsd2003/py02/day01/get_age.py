e8641d214ffc5b176f2646f357b0588ba426989a
zhangzhg <zhangzg@tedu.cn>
2020-08-18 15:21:13 +0800

    nsd2003 py02 day01 15:20

--- nsd2003/py02/day01/get_age.py
+++ nsd2003/py02/day01/get_age.py
@@ -3,10 +3,14 @@ def get_age(name, age):
         raise ValueError('年龄超过范围(1~119)')
     print('%s is %s years old' % (name, age))
 
+def get_age2(name, age):
+    assert 0 < age < 120, '年龄超过范围(1~119)'
+    print('%s is %s years old' % (name, age))
+
 if __name__ == '__main__':
     try:
         get_age('nb', 200)
     except ValueError as e:
         print('Error:', e)
 
-
+    get_age2('plj', 188)
