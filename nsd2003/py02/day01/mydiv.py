cacdc4e26a6e7e27c3ea2202a6fb82ce38399309
zhangzhg <zhangzg@tedu.cn>
2020-08-18 12:00:57 +0800

    nsd2003 py02 day01 12:00

--- nsd2003/py02/day01/mydiv.py
+++ nsd2003/py02/day01/mydiv.py
@@ -1,4 +1,13 @@
-n = int(input('number: '))
-result = 100 / n
-print(result)
-print('Done')
+try:
+    n = int(input('number: '))
+    result = 100 / n
+    print(result)
+    print('Done')
+except ValueError:
+    print('请输入非0数字')
+except ZeroDivisionError:
+    print('请输入非0数字')
+except KeyboardInterrupt:
+    print('\nBye-bye')
+except EOFError:
+    print('\nBye-bye')
