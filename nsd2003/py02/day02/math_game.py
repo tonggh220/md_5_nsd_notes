eef274788fb5a0a8d94499eb8dc16c4434356027
zhangzhg <zhangzg@tedu.cn>
2020-08-19 14:59:40 +0800

    nsd2003 py02 day02 15:00

--- nsd2003/py02/day02/math_game.py
+++ nsd2003/py02/day02/math_game.py
@@ -18,7 +18,12 @@ def exam():
     prompt = '%s %s %s = ' % (nums[0], op, nums[1])
     n = 0
     while n < 3:
-        answer = int(input(prompt))
+        try:
+            answer = int(input(prompt))
+        except:  # 不指定具体的异常，可以捕获所有异常。不推荐
+            print()
+            continue
+
         if answer == result:
             print('你真棒！！！')
             break
@@ -31,7 +36,13 @@ def main():
     while 1:
         exam()
         # 去除用户输入的额外空格，并取第一个字符
-        yn = input('Continue(y/n)? ').strip()[0]
+        try:
+            yn = input('Continue(y/n)? ').strip()[0]
+        except IndexError:
+            continue
+        except (KeyboardInterrupt, EOFError):
+            yn = 'n'
+
         if yn in 'nN':
             print('\nBye-bye')
             break
