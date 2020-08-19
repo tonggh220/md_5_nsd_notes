46bc5cf00be7acc7112e50be77cdff98fa2a6345
zhangzhg <zhangzg@tedu.cn>
2020-08-19 17:08:45 +0800

    nsd2003 py02 day02 17:00

--- nsd2003/py02/day02/day02.py
+++ nsd2003/py02/day02/day02.py
@@ -21,17 +21,34 @@
 #     print(list(result1))
 #     print(list(result2))
 
-from random import randint
+# from random import randint
 
-def func1(x):
-    return x + 100
+# def func1(x):
+#     return x + 100
+#
+# if __name__ == '__main__':
+#     nums = [randint(1, 100) for i in range(10)]
+#     print(nums)
+#     result1 = map(func1, nums)
+#     result2 = map(lambda x: x + 100, nums)
+#     print(list(result1))
+#     print(list(result2))
 
-if __name__ == '__main__':
-    nums = [randint(1, 100) for i in range(10)]
-    print(nums)
-    result1 = map(func1, nums)
-    print(list(result1))
 
+# 创建一个取之不尽的数字生成器
+def gen_num():
+  n = 0
+  while 1:
+    yield n
+    n += 1
+
+nums = gen_num()
+
+i = 0
+while i < 10:
+  data = nums.__next__()  # 生成器生成数字时，内部采用的方法
+  print(data)
+  i += 1
 
 
 
