fdbb94843015421433364ef88069d20294d59b07
zhangzhg <zhangzg@tedu.cn>
2020-08-19 15:51:10 +0800

    nsd2003 py02 day02 15:50

--- nsd2003/py02/day02/day02.py
+++ nsd2003/py02/day02/day02.py
@@ -1,9 +1,31 @@
-def func1():
-    print('fn 1')
-    func2()
+# def func1():
+#     print('fn 1')
+#     func2()
+#
+# def func2():
+#     print('fn 2')
+#
+# if __name__ == '__main__':
+#     func1()
 
-def func2():
-    print('fn 2')
+from random import randint
+
+def func1(x):
+    return x % 2  # 整数求余，只有0或1两种情况，0为假，1为真
 
 if __name__ == '__main__':
-    func1()
+    nums = [randint(1, 100) for i in range(10)]
+    result1 = filter(func1, nums)
+    result2 = filter(lambda x: x % 2, nums)
+    print(nums)
+    print(list(result1))
+    print(list(result2))
+
+
+
+
+
+
+
+
+
