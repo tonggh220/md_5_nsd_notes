f35b0dc4a679746023f0c13e08cb03cfa5dbcdd2
zhangzhg <zhangzg@tedu.cn>
2020-08-21 10:28:16 +0800

    nsd2003 py02 day03 10:30

--- nsd2003/py02/day03/backup.py
+++ nsd2003/py02/day03/backup.py
@@ -3,8 +3,28 @@ import os
 import tarfile
 import hashlib
 
+def check_md5(fname):
+    m = hashlib.md5()
+    with open(fname, 'rb') as fobj:
+        while 1:
+            data = fobj.read(4096)
+            if not data:
+                break
+            m.update(data)
+    return m.hexdigest()
+
 def full_backup(src, dst, md5file):
     '完全备份'
+    # 拼接备份文件的路径
+    fname = '%s_full_%s.tar.gz' % (os.path.basename(src), strftime('%Y%m%d'))
+    fname = os.path.join(dst, fname)
+
+    # 打包源目录
+    tar = tarfile.open(fname, 'w:gz')
+    tar.add(src)
+    tar.close()
+
+    # 计算并存储md5值
 
 def incr_backup(src, dst, md5file):
     '增量备份'
@@ -20,4 +40,3 @@ if __name__ == '__main__':
 
 # mkdir -p /tmp/demo/backup
 # cp -r /etc/security /tmp/demo/
-
