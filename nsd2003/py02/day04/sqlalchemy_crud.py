e6cd0ce64d728cf82aab9694fdffe01ca08ac43f
zhangzhg <zhangzg@tedu.cn>
2020-08-22 16:57:38 +0800

    nsd2003 py02 day04 16:55

--- nsd2003/py02/day04/sqlalchemy_crud.py
+++ nsd2003/py02/day04/sqlalchemy_crud.py
@@ -5,8 +5,17 @@ session = Session()
 ##################################################
 # 通过会话实例对数据库进行增删改查
 ##################################################
-
-
+# 新增一个部门
+hr = Department(id=1, dep_name='人事部')
+session.add(hr)
+# 新增多个部门
+ops = Department(id=2, dep_name='运维部')
+dev = Department(id=3, dep_name='开发部')
+qs = Department(id=4, dep_name='测试部')
+finance = Department(id=5, dep_name='财务部')
+sales = Department(id=6, dep_name='销售部')
+market = Department(id=7, dep_name='市场部')
+session.add_all([ops, dev, qs, finance, sales, market])
 ##################################################
 # 确认
 session.commit()
