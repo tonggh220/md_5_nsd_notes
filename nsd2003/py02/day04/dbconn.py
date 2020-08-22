70039d1e0546399158903a8b9065c1865a4bafca
zhangzhg <zhangzg@tedu.cn>
2020-08-22 16:46:57 +0800

    nsd2003 py02 day04 16:50

--- nsd2003/py02/day04/dbconn.py
+++ nsd2003/py02/day04/dbconn.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 # 创建连接数据库的引擎
 engine = create_engine(
@@ -9,6 +10,7 @@ engine = create_engine(
     # echo=True  # 在屏幕上输出调试信息，生产环境不要打开
 )
 Base = declarative_base()  # 生成映射类的基类
+Session = sessionmaker(bind=engine)  # 创建到数据库的会话加接类
 
 # 声明映射类
 class Department(Base):
