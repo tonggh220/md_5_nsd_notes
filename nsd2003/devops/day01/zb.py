de6a4187b77e2c1afd897fb2acc596f064526a6a
zhangzhg <zhangzg@tedu.cn>
2020-08-24 10:59:53 +0800

    nsd2003 devops day01 11:00

---
+++ nsd2003/devops/day01/zb.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+print('starting...')
+ret_val = os.fork()
+if ret_val:
+    print('parent')
+    time.sleep(60)
+    print('parent done')
+else:
+    print('child')
+    time.sleep(30)
+    print('child done')
+
