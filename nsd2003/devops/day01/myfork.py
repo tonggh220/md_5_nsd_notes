d2dc4f2ca0e1cb89db22836a8c817c15033a02b1
zhangzhg <zhangzg@tedu.cn>
2020-08-24 10:33:48 +0800

    nsd2003 devops day01 10:30

--- nsd2003/devops/day01/myfork.py
+++ nsd2003/devops/day01/myfork.py
@@ -4,13 +4,19 @@ import os
 # os.fork()
 # print('Hello World!')
 
-print('starting...')
-ret_val = os.fork()  # 如果是父进程，值非0，为真；如果是子进程，值是0，为假
-if ret_val:
-    print('in parent')
-else:
-    print('in child')
-print('Hello World!')
+# print('starting...')
+# ret_val = os.fork()  # 如果是父进程，值非0，为真；如果是子进程，值是0，为假
+# if ret_val:
+#     print('in parent')
+# else:
+#     print('in child')
+# print('Hello World!')
 
+print('starting...')
+for i in range(3):
+    ret_val = os.fork()
+    if not ret_val:
+        print('Hello World!')
+        exit()
 
 
