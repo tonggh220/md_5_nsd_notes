27d047f0b4637a53a454304801e1e4aa0a5fcddd
zhangzhg <zhangzg@tedu.cn>
2020-08-25 17:44:39 +0800

    nsd2003 devops day02 17:40

--- nsd2003/devops/day02/zabbix.py
+++ nsd2003/devops/day02/zabbix.py
@@ -5,11 +5,39 @@ url = 'http://192.168.1.154/zabbix/api_jsonrpc.php'
 headers = {'Content-Type': 'application/json; charset=UTF-8'}
 ##################################
 # 对于不需要认证即可获得的信息，可以直接查
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "apiinfo.version",
+#     "params": [],
+#     "id": 1  # 随便指定一个数字，表示作业ID
+# }
+##################################
+# 获取隐私信息，需要token。获取用户的token: 7e811a3f758ccbdcaba211d1431bac47
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "user.login",
+#     "params": {
+#         "user": "Admin",
+#         "password": "zabbix"
+#     },
+#     "id": 1
+# }
+##################################
+# 查询组，获取组id: 2
 data = {
     "jsonrpc": "2.0",
-    "method": "apiinfo.version",
-    "params": [],
-    "id": 1  # 随便指定一个数字，表示作业ID
+    "method": "hostgroup.get",
+    "params": {
+        "output": "extend",
+        "filter": {
+            "name": [
+                # "Zabbix servers",
+                "Linux servers"
+            ]
+        }
+    },
+    "auth": "7e811a3f758ccbdcaba211d1431bac47",
+    "id": 1
 }
 ##################################
 r = requests.post(url, headers=headers, data=json.dumps(data))
