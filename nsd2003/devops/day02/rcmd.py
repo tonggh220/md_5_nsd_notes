137a9d26cdbabf799407c33a4a6c0d1389cfd977
zhangzhg <zhangzg@tedu.cn>
2020-08-25 09:25:00 +0800

    nsd2003 devops day02 9:25

---
+++ nsd2003/devops/day02/rcmd.py
@@ -0,0 +1,29 @@
+import paramiko
+import sys
+
+def rcmd(host, user, passwd, port=22, cmds=None):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, port=port, username=user, password=passwd)
+    stdin, stdout, stderr = ssh.exec_command(cmds)
+    out = stdout.read()
+    err = stderr.read()
+    if out:
+        print('[%s] \033[32;1mOUT\033[0m:\n%s' % (host, out.decode()))
+    if err:
+        print('[%s] \033[31;1mERROR\033[0m:\n%s' % (host, err.decode()))
+    ssh.close()
+
+if __name__ == '__main__':
+    # rcmd('127.0.0.1', 'root', 'redhat', cmds='id root; id zhangsan')
+    ipfile = sys.argv[1]
+    cmds = ' '.join(sys.argv[2:])
+    with open(ipfile) as fobj:
+        for line in fobj:
+            ip = line.strip()
+            rcmd(ip, 'root', 'redhat', 22, cmds)
+
+# vim servers.txt
+# 192.168.1.154
+# 127.0.0.1
+# python3 rcmd.py servers.txt id root
