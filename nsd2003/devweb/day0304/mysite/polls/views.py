2ab5777c709c53176deb7ca5f482beecf71b3872
zhangzhg <zhangzg@tedu.cn>
2020-09-02 15:24:24 +0800

    nsd2003 devweb day03 15:30

--- nsd2003/devweb/day0304/mysite/polls/views.py
+++ nsd2003/devweb/day0304/mysite/polls/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render
 def index(request):
     # index函数通过render函数找到一个名为index.html网页模板文件，返回给客户端
     return render(request, 'index.html')
+
+def detail(request, qid):
+    # qid用于接收来自于url的参数<int:qid>
+    # {'qid': qid}将成为detail.html的变量和值，即 qid=数字
+    return render(request, 'detail.html', {'qid': qid})
