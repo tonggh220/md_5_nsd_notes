498c93891b7bcd59fd24c54254c5d64289d66c56
MrZhangzhg <zhangzg@tedu.cn>
2020-09-08 18:02:50 +0800

    nsd2004 py01 day01 18:00

--- nsd2004/py01/day01/hello.py
+++ nsd2004/py01/day01/hello.py
@@ -8,9 +8,14 @@
 #
 # print(s1)
 
-user = input('username: ')
-print(user)
+# user = input('username: ')
+# print(user)
 
+a = round(5 / 3, 2)
+print(a)
 
-
+# 2小时3分5秒 -> ?秒
+# 2 * 60 * 60 + 3 * 60 + 5
+# 0x235 -> 10进制数?
+# 2 * 16 * 16 + 3 * 16 + 5
 
