95637e0b56802793d11ccfc26b4e3aedc5d7c96e
MrZhangzhg <zhangzg@tedu.cn>
2020-09-09 10:18:59 +0800

    nsd2004 py01 day02 10：20

--- nsd2004/py01/day02/day02.py
+++ nsd2004/py01/day02/day02.py
@@ -2,19 +2,26 @@
 #     print('yes')
 #     print('ok')
 
-if -0.0:
-    print('任何值为0的数字，都是假')
+# if -0.0:
+#     print('任何值为0的数字，都是假')
+#
+# if ' ':
+#     print('空格也是一个字符，为真')
+#
+# if [1, 2, 3]:
+#     print('非空列表，为真')
+#
+# if ():
+#     print('空元组，为假')
+#
+# if {'name': 'nb'}:
+#     print('非空字典，为真')
 
-if ' ':
-    print('空格也是一个字符，为真')
-
-if [1, 2, 3]:
-    print('非空列表，为真')
-
-if ():
-    print('空元组，为假')
-
-if {'name': 'nb'}:
-    print('非空字典，为真')
-
-
+a = 10
+b = 20
+# if a < b:
+#     s1 = a
+# else:
+#     s1 = b
+# print(s1)
+s2 = a if a < b else b
