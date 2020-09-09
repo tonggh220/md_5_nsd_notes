6d5e2a617f812e9253fd03a18cacc69eb98c1a29
MrZhangzhg <zhangzg@tedu.cn>
2020-09-09 17:59:26 +0800

    nsd2004 py01 day02 18:01

--- nsd2004/py01/day02/for.py
+++ nsd2004/py01/day02/for.py
@@ -1,16 +1,19 @@
-s1 = 'abc'
-l1 = [10, 20, 30]
-t1 = ('tom', 'jerry', 'bob', 'alice')
-d1 = {'name': 'nb', 'age': 20}
+# s1 = 'abc'
+# l1 = [10, 20, 30]
+# t1 = ('tom', 'jerry', 'bob', 'alice')
+# d1 = {'name': 'nb', 'age': 20}
+#
+# for ch in s1:
+#     print(ch)
+#
+# for i in l1:
+#     print(i)
+#
+# for name in t1:
+#     print(name)
+#
+# for key in d1:
+#     print(key, d1[key])
 
-for ch in s1:
-    print(ch)
-
-for i in l1:
+for i in range(10):
     print(i)
-
-for name in t1:
-    print(name)
-
-for key in d1:
-    print(key, d1[key])
