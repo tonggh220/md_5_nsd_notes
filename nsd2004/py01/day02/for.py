8118ce8c9aa66f45ffc82000f078e42984bce86e
MrZhangzhg <zhangzg@tedu.cn>
2020-09-09 17:02:01 +0800

    nsd2004 py01 day02 17:01

---
+++ nsd2004/py01/day02/for.py
@@ -0,0 +1,16 @@
+s1 = 'abc'
+l1 = [10, 20, 30]
+t1 = ('tom', 'jerry', 'bob', 'alice')
+d1 = {'name': 'nb', 'age': 20}
+
+for ch in s1:
+    print(ch)
+
+for i in l1:
+    print(i)
+
+for name in t1:
+    print(name)
+
+for key in d1:
+    print(key, d1[key])
