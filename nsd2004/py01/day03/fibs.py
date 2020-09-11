c061c83fe9f7b9125cc3904f46de1c722bcc3062
MrZhangzhg <zhangzg@tedu.cn>
2020-09-11 15:49:51 +0800

    nsd2004 py01 day03 15:50

--- nsd2004/py01/day03/fibs.py
+++ nsd2004/py01/day03/fibs.py
@@ -2,11 +2,10 @@ def mk_fib():
     fib = [0, 1]
 
     n = int(input("数列长度: "))
-
     for i in range(n - 2):
         fib.append(fib[-1] + fib[-2])
 
-    print(fib)
+    return fib
 
-mk_fib()
-mk_fib()
+l1 = mk_fib()
+print(l1)
