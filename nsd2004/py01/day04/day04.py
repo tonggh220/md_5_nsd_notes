56c81f53d3471ffbb4f16fc14c88d225d5ed8d5d
MrZhangzhg <zhangzg@tedu.cn>
2020-09-12 16:01:37 +0800

    nsd2004 py01 day04 16:00

--- nsd2004/py01/day04/day04.py
+++ nsd2004/py01/day04/day04.py
@@ -1,4 +1,23 @@
-import shutil
+# names = ['tom', 'jerry', 'jack', 'rose', 'bob']
 
-shutil.copyfileobj()
-shutil.copy()
+# for i in [0, 1, 2, 3]:
+#     print(i, names[i])
+
+# for i in range(4):
+#     print(i, names[i])
+
+# for i in range(len(names)):
+#     print(i, names[i])
+
+names = ['tom', 'jerry', 'jack', 'rose', 'bob']
+
+# for data in enumerate(names):
+#     print(data)
+
+# for data in enumerate(names):
+#     print(data[0], data[1])
+
+for i, name in enumerate(names):
+    print(i, name)
+
+
