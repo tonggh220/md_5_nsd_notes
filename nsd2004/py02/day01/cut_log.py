efcfe63618fa5c1d39fb94290ca3a795c63a5f17
MrZhangzhg <zhangzg@tedu.cn>
2020-09-15 10:46:05 +0800

    nsd2004 py02 day01 10:50

--- nsd2004/py02/day01/cut_log.py
+++ nsd2004/py02/day01/cut_log.py
@@ -10,5 +10,9 @@ logfile = 'mylog.txt'
 with open(logfile) as fobj:
     for line in fobj:
         t = time.strptime(line[:19], '%Y-%m-%d %H:%M:%S')
-        if t9 <= t <= t12:
+        if t >= t12:
+            break
+        if t >= t9:
             print(line, end='')
+        # if t9 <= t <= t12:
+        #     print(line, end='')
