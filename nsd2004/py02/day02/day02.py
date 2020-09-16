3696815dbeab7bca1d19a18fccfafcff05563301
MrZhangzhg <zhangzg@tedu.cn>
2020-09-16 16:00:34 +0800

    nsd2004 py02 day02 16:00

--- nsd2004/py02/day02/day02.py
+++ nsd2004/py02/day02/day02.py
@@ -1,9 +1,28 @@
-def func1():
-    print('in func1')
-    func2()
+# def func1():
+#     print('in func1')
+#     func2()
+#
+# def func2():
+#     print('in func2')
+#
+# if __name__ == '__main__':
+#     func1()
 
-def func2():
-    print('in func2')
+from random import randint
+
+def func1(x):
+    return x % 2
 
 if __name__ == '__main__':
-    func1()
+    nums = [randint(1, 100) for i in range(10)]
+    print(nums)
+    result1 = filter(func1, nums)
+    print(list(result1))
+
+
+
+
+
+
+
+
