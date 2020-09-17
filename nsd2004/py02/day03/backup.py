20b15554ab60330ff0d814cb5adc24cbf141f74d
MrZhangzhg <zhangzg@tedu.cn>
2020-09-17 10:01:22 +0800

    nsd2004 py02 day03 10:00

--- nsd2004/py02/day03/backup.py
+++ nsd2004/py02/day03/backup.py
@@ -1,7 +1,18 @@
+import os
+import tarfile
 from time import strftime
+
 
 def full_backup(src, dst, md5file):
     "用于完全备份"
+    # 拼接出备份文件的绝对路径
+    fname = '%s_full_%s.tar.gz' % (os.path.basename(src), strftime('%Y%m%d'))
+    fname = os.path.join(dst, fname)
+
+    # 完全备份，就是把源目录直接打个压缩包
+    tar = tarfile.open(fname, 'w:gz')
+    tar.add(src)
+    tar.close()
 
 def incr_backup(src, dst, md5file):
     "用于增量备份"
