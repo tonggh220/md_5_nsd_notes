3992a063757cafe0c82909d1457953b16ded4445
MrZhangzhg <zhangzg@tedu.cn>
2020-09-17 16:01:20 +0800

    nsd2004 py02 day03 16:00

---
+++ nsd2004/py02/day03/books.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, title, author):
+        "构造器方法，实例化时自动调用"
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        "打印、显示实例时，将展示这个方法的返回值"
+        return '《%s》' % self.title
+
+    def __call__(self):
+        "使实例对象可以像函数一样调用"
+        print('《%s》是%s编著的' % (self.title, self.author))
+
+if __name__ == '__main__':
+    xyj = Book('西游记', '吴承恩')  # 调用__init__
+    print(xyj)   # 调用__str__
+    xyj()        # 调用__call__
