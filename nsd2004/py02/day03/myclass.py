d6f3cf98e92226d1be21ed9a595177ab99af0428
MrZhangzhg <zhangzg@tedu.cn>
2020-09-17 11:49:20 +0800

    nsd2004 py02 day03 11:50

---
+++ nsd2004/py02/day03/myclass.py
@@ -0,0 +1,11 @@
+class Role:
+    def __init__(self, name, weapon):
+        # 构造器方法，实例化时自动调用，注意，self不是关键字，可以是任何自定义的变量
+        # 绑定在实例身上的属性，在类中任意位置可见可用
+        self.name = name
+        self.weapon = weapon
+
+if __name__ == '__main__':
+    # 实例本身将会自动作为第一个参数传递，本例中是lb
+    lb = Role('吕布', '方天画戟')  # 实例化，创建具体的对象
+    print(lb.name, lb.weapon)
