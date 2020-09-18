1da5929690463e159aca1a2c928ac0357b9ca0a4
MrZhangzhg <zhangzg@tedu.cn>
2020-09-18 12:01:07 +0800

    nsd2004 py02 day04 12:00

---
+++ nsd2004/py02/day04/py_mysql.py
@@ -0,0 +1,24 @@
+import pymysql
+
+# 创建到数据库的连接
+conn = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='tedu.cn',
+    db='nsd2004',
+    charset='utf8mb4',
+)
+
+# 创建游标。游标像文件对象一样，通过文件对象对文件读写；通过游标对数据库进行增删改查
+cur = conn.cursor()
+
+# 编写sql语句
+create_dep = ''
+
+# 执行sql语句
+cur.execute(create_dep)
+
+# 关闭
+cur.close()
+conn.close()
+
