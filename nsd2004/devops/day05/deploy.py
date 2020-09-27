d6d49ac256d9161d2e63edf8e44dde3fcbd51123
MrZhangzhg <zhangzg@tedu.cn>
2020-09-27 16:58:59 +0800

    nsd2004 devops day05 17:00

--- nsd2004/devops/day05/deploy.py
+++ nsd2004/devops/day05/deploy.py
@@ -1,10 +1,15 @@
+import os
 import requests
 import wget
 
 def has_new_ver(ver_url, ver_fname):
     "用于判断是否有新版本，有返回True，没有返回False"
 
+def file_ok(md5url, app_fname):
+    "判断文件是否完好。完好返回True，否则返回False"
 
+def deploy(app_fname, deploy_dir, dest):
+    "部署软件"
 
 if __name__ == '__main__':
     # 判断是否有新版本，没有新版本则退出
@@ -21,7 +26,20 @@ if __name__ == '__main__':
     wget.download(app_url, download_dir)
 
     # 校验新版本文件，如果已损坏则删除
+    md5url = app_url + '.md5'
+    app_fname = app_url.split('/')[-1]
+    app_fname = os.path.join(download_dir, app_fname)
+    if not file_ok(md5url, app_fname):
+        os.remove(app_fname)
+        print("文件下载过程中已损坏")
+        exit(2)
 
     # 部署软件
+    deploy_dir = "/var/www/deploy"
+    dest = "/var/www/html/nsd2004"
+    deploy(app_fname, deploy_dir, dest)
 
     # 更新本地live_ver文件
+    if os.path.exists(ver_fname):
+        os.remove(ver_fname)
+    wget.download(ver_url, ver_fname)
