81838d70eeb37518501c476eeaef682bc47dc23d
MrZhangzhg <zhangzg@tedu.cn>
2020-09-27 18:01:53 +0800

    nsd2004 devops day05 18:00

--- nsd2004/devops/day05/deploy.py
+++ nsd2004/devops/day05/deploy.py
@@ -2,6 +2,7 @@ import os
 import requests
 import wget
 import hashlib
+import tarfile
 
 def has_new_ver(ver_url, ver_fname):
     "用于判断是否有新版本，有返回True，没有返回False"
@@ -39,11 +40,27 @@ def file_ok(md5url, app_fname):
 
 def deploy(app_fname, deploy_dir, dest):
     "部署软件"
+    # 解压
+    tar = tarfile.open(app_fname)
+    tar.extractall(path=deploy_dir)
+    tar.close()
+
+    # 拼接出解压目录的绝对路径
+    app_dir = os.path.basename(app_fname)
+    app_dir = app_dir.replace('.tar.gz', '')
+    app_dir = os.path.join(deploy_dir, app_dir)
+
+    # 如果软链接已存在，则删除它
+    if os.path.exists(dest):
+        os.remove(dest)
+
+    # 创建软链接
+    os.symlink(app_dir, dest)
 
 if __name__ == '__main__':
     # 判断是否有新版本，没有新版本则退出
     ver_url = 'http://192.168.1.103/deploy/live_ver'
-    ver_fname = '/var/www/html/deploy/live_ver'
+    ver_fname = '/var/www/deploy/live_ver'
     if not has_new_ver(ver_url, ver_fname):
         print("未发现新版本")
         exit(1)
