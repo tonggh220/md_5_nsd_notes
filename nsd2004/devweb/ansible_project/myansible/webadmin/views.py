14ae74cff73d166dfa72817eb4c4403369b313bb
MrZhangzhg <zhangzg@tedu.cn>
2020-10-12 09:28:30 +0800

    nsd2004 devweb day06 9:30

--- nsd2004/devweb/ansible_project/myansible/webadmin/views.py
+++ nsd2004/devweb/ansible_project/myansible/webadmin/views.py
@@ -6,5 +6,13 @@ def index(request):
 
 def add_hosts(request):
     # print('-----', request.method, '-----')
+    if request.method == 'POST':
+        group = request.POST.get('group').strip()
+        host = request.POST.get('host').strip()
+        ip = request.POST.get('ip').strip()
+        if group:
+            g = HostGroup.objects.get_or_create(groupname=group)[0]
+            if host and ip:
+                g.host_set.get_or_create(hostname=host, ip_addr=ip)
     groups = HostGroup.objects.all()
     return render(request, 'webadmin/add_hosts.html', {'groups': groups})
