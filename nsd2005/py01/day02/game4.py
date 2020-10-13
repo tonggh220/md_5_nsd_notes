4e082fb5a3542f9ce808e7131aebe4451d2a00e3
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-13 15:51:35 +0800

    nsd2005 py01 day02 15:50

---
+++ nsd2005/py01/day02/game4.py
@@ -0,0 +1,32 @@
+import random
+
+# 定义人胜利的列表
+win_list = [['石头', '剪刀'], ['剪刀', '布'], ['布', '石头']]
+
+# 人机分别出拳
+all_choice = ['石头', '剪刀', '布']
+prompt = """(0) 石头
+(1) 剪刀
+(2) 布
+请选择(0/1/2): """
+# 定义人机胜利计数器
+pwin = 0
+cwin = 0
+
+while cwin < 2 and pwin < 2:
+    computer = random.choice(all_choice)
+    ind = int(input(prompt))
+    player = all_choice[ind]
+
+    # 打印人机选择
+    print("计算机的选择:%s, 您的选择:%s" % (computer, player))
+
+    # 判断并输出胜负
+    if computer == player:
+        print("\033[32;1m平局\033[0m")
+    elif [player, computer] in win_list:
+        pwin += 1
+        print("\033[31;1mYou WIN!!!\033[0m")
+    else:
+        cwin += 1
+        print("\033[31;1mYou LOSE!!!\033[0m")
