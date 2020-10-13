e0ed9c7a432223343ff0ae035e1253d6f0dc8f1f
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-13 11:00:45 +0800

    nsd2005 py01 day02 11:00

--- nsd2005/py01/day02/day02.py
+++ nsd2005/py01/day02/day02.py
@@ -10,17 +10,30 @@
 # else:
 #     print('这个语句会执行')
 
-if 0.0:
-    print('任何值为0的数字都是假')
+# if 0.0:
+#     print('任何值为0的数字都是假')
+#
+# if ' ':
+#     print('空格字符也是一个字符，为真')
+#
+# if [1, 2, 3]:
+#     print('非空列表，为真')
+#
+# if ():
+#     print('空元组，为假')
+#
+# if {'name': 'nb'}:
+#     print('非空字典，为真')
 
-if ' ':
-    print('空格字符也是一个字符，为真')
+# a = 10
+# b = 20
+# if a < b:
+#     s = a
+# else:
+#     s = b
+# print(s)
 
-if [1, 2, 3]:
-    print('非空列表，为真')
-
-if ():
-    print('空元组，为假')
-
-if {'name': 'nb'}:
-    print('非空字典，为真')
+a = 10
+b = 20
+s = a if a < b else b
+print(s)
---
+++ nsd2005/py01/day02/login2.py
@@ -0,0 +1,7 @@
+username = input('username: ')
+password = input('password: ')
+
+if username == 'bob' and password == '123456':
+    print('登陆成功')
+else:
+    print('登陆失败')
