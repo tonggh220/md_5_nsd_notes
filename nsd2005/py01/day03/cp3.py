370a4e721350ebab0c89968b1fb7c74b2972c92f
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-14 15:19:10 +0800

    nsd2005 py01 day03 15:20

---
+++ nsd2005/py01/day03/cp3.py
@@ -0,0 +1,14 @@
+src_fname = '/tmp/girl.jpg'
+dst_fname = '/var/tmp/girl.jpg'
+
+src_fobj = open(src_fname, 'rb')
+dst_fobj = open(dst_fname, 'wb')
+
+while 1:
+    data = src_fobj.read(4096)
+    if not data:
+        break
+    dst_fobj.write(data)
+
+src_fobj.close()
+dst_fobj.close()
