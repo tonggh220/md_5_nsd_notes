48b75f34ac4e178285d4a07b995cc972b8ca785b
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-15 15:39:23 +0800

    nsd2004 py01 day04 15:40

--- nsd2005/py01/day04/day05.py
+++ nsd2005/py01/day04/day05.py
@@ -1,5 +1,11 @@
-import shutil
-import string
+from random import randint
 
-shutil.copyfileobj
-shutil.copy
+nums = [randint(1, 100) for i in range(10)]
+print(nums)
+result1 = reversed(nums)
+# print(result1)
+# for i in result1:
+#     print(i)
+print(list(result1))
+result2 = sorted(nums)
+print(result2)
