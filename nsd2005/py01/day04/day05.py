b19caa05e1e9e6381690f1fb472d1bdeb6d18ef0
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-15 15:53:46 +0800

    nsd2004 py01 day04 15:50

--- nsd2005/py01/day04/day05.py
+++ nsd2005/py01/day04/day05.py
@@ -1,11 +1,32 @@
-from random import randint
-
-nums = [randint(1, 100) for i in range(10)]
-print(nums)
-result1 = reversed(nums)
+# from random import randint
+#
+# nums = [randint(1, 100) for i in range(10)]
+# print(nums)
+# result1 = reversed(nums)
 # print(result1)
 # for i in result1:
 #     print(i)
-print(list(result1))
-result2 = sorted(nums)
-print(result2)
+# print(list(result1))
+# result2 = sorted(nums)
+# print(result2)
+###################################
+#
+# names = ['tom', 'jerry', 'jack', 'rose', 'bob']
+# for i in (0, 1, 2, 3):
+#     print(i, names[i])
+##########
+# for i in range(4):
+#     print(i, names[i])
+##########
+# for i in range(len(names)):
+#     print(i, names[i])
+##########
+#
+names = ['tom', 'jerry', 'jack', 'rose', 'bob']
+# >>> list(enumerate(names))
+# [(0, 'tom'), (1, 'jerry'), (2, 'jack'), (3, 'rose'), (4, 'bob')]
+# for data in enumerate(names):
+#     print(data)
+#############
+for i, name in enumerate(names):
+    print(i, name)
