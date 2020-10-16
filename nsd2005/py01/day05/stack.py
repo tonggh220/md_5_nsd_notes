b27c0841fd52af5cce52361c923bfe3e4055e063
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-16 11:38:28 +0800

    nsd2004 py01 day05 11:40

--- nsd2005/py01/day05/stack.py
+++ nsd2005/py01/day05/stack.py
@@ -1,14 +1,38 @@
 def push_it():
     "用于压栈"
+    print('push')
 
 def pop_it():
     "用于出栈"
+    print('pop')
 
 def view_it():
     "查询"
+    print('view')
 
 def show_menu():
     "用于显示菜单，实现代码逻辑"
+    prompt = """(0) 压栈
+(1) 出栈
+(2) 查询
+(3) 退出
+请选择(0/1/2/3): """
+
+    while 1:
+        choice = input(prompt).strip()  # 删除用户输入字符串两端的空格
+        if choice not in ['0', '1', '2', '3']:
+            print("无效的输入，请重试。")
+            continue
+
+        if choice == '0':
+            push_it()
+        elif choice == '1':
+            pop_it()
+        elif choice == '2':
+            view_it()
+        else:
+            print('Bye-bye')
+            break
 
 if __name__ == '__main__':
     show_menu()
