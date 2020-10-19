0a2acbc01bc704e7d807f6a069e2ceef7c0fab5a
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-19 14:55:16 +0800

    nsd2004 py02 day01 14:50

--- nsd2005/py02/day01/myerr.py
+++ nsd2005/py02/day01/myerr.py
@@ -3,8 +3,14 @@ def get_info(name, age):
         raise ValueError("年龄超过范围(1~119)")
     print('%s is %s years old' % (name, age))
 
+def get_info2(name, age):
+    assert 0 < age < 120, "年龄超过范围(1~119)"
+    print('%s is %s years old' % (name, age))
+
 if __name__ == '__main__':
     try:
         get_info('nb', 200)
     except ValueError as e:  # 将捕获到的异常保存到变量e中
         print(e)
+
+    get_info2('plj', 188)
