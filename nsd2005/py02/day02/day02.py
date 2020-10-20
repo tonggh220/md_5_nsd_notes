f84a3feb53a9ad2a6d2a3a3aaabea16c78d0a485
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-20 16:20:57 +0800

    nsd2004 py02 day02 16:20

--- nsd2005/py02/day02/day02.py
+++ nsd2005/py02/day02/day02.py
@@ -8,15 +8,28 @@
 # if __name__ == '__main__':
 #     fn1()
 
+# from random import randint
+#
+# def func1(x):
+#     return x % 2
+#
+# if __name__ == '__main__':
+#     nums = [randint(1, 100) for i in range(10)]
+#     print(nums)
+#     result1 = filter(func1, nums)
+#     result2 = filter(lambda x: x % 2, nums)
+#     print(list(result1))
+#     print(list(result2))
+
 from random import randint
 
-def func1(x):
-    return x % 2
+def func2(x):
+    return x + 100
 
 if __name__ == '__main__':
     nums = [randint(1, 100) for i in range(10)]
     print(nums)
-    result1 = filter(func1, nums)
-    result2 = filter(lambda x: x % 2, nums)
+    result1 = map(func2, nums)
+    result2 = map(lambda x: x + 100, nums)
     print(list(result1))
     print(list(result2))
