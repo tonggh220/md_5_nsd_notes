2cc8a4b8eaa5918cdcf7c0c41dfefca2b6f160fd
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-20 15:01:38 +0800

    nsd2004 py02 day02 15:00

--- nsd2005/py02/day02/math_game.py
+++ nsd2005/py02/day02/math_game.py
@@ -32,17 +32,31 @@ def exam():
 
     # 用户作答，判断正误
     prompt = "%s %s %s = " % (nums[0], op, nums[1])
-    answer = int(input(prompt))
-    if answer == result:
-        print('不错哟！')
-    else:
+    i = 0
+    while i < 3:
+        try:
+            answer = int(input(prompt))
+        except:  # 不填写具体的异常，可以捕获所有异常，不推荐
+            print()  # 打印空行
+            continue
+
+        if answer == result:
+            print('不错哟！')
+            break
         print('不对哟！')
+        i += 1
 
 def main():
     "主程序代码逻辑"
     while 1:
         exam()
-        yn = input("Continue(y/n)? ").strip()[0]  # 去除字符串两端空白字符后，取第一个字符
+        try:
+            yn = input("Continue(y/n)? ").strip()[0]  # 去除字符串两端空白字符后，取第一个字符
+        except IndexError:
+            continue
+        except (KeyboardInterrupt, EOFError):
+            yn = 'n'
+
         if yn in 'nN':
             print('\nBye-bye')
             break
