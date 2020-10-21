8e7ff215d0769eaffee026284ffe2e893c613f9d
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-21 09:51:07 +0800

    nsd2004 py02 day03 9:50

---
+++ nsd2005/py02/day03/backup.py
@@ -0,0 +1,21 @@
+from time import strftime
+
+def full_backup(src, dst, md5file):
+    "完全备份"
+
+
+def incr_backup(src, dst, md5file):
+    "增量备份"
+    
+
+if __name__ == '__main__':
+    src = '/tmp/demo/security'
+    dst = '/tmp/demo/backup'
+    md5file = '/tmp/demo/backup/md5.data'
+    if strftime('%a') == 'Mon':
+        full_backup(src, dst, md5file)
+    else:
+        incr_backup(src, dst, md5file)
+
+# mkdir -p /tmp/demo/backup
+# cp -r /etc/security /tmp/demo
