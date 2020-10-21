2d1d9cab1896a2f8714f6faf0b43db4b634de529
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-21 10:20:49 +0800

    nsd2004 py02 day03 10:20

--- nsd2005/py02/day03/backup.py
+++ nsd2005/py02/day03/backup.py
@@ -1,12 +1,25 @@
+import os
+import tarfile
 from time import strftime
 
 def full_backup(src, dst, md5file):
     "完全备份"
+    # 拼接出备份文件名
+    fname = '%s_full_%s.tar.gz' % (os.path.basename(src), strftime('%Y%m%d'))
+    fname = os.path.join(dst, fname)
+
+    # 打tar包
+    tar = tarfile.open(fname, 'w:gz')
+    tar.add(src)
+    tar.close()
+
+    # 计算每个文件的md5值
+    md5dict = {}
 
 
 def incr_backup(src, dst, md5file):
     "增量备份"
-    
+
 
 if __name__ == '__main__':
     src = '/tmp/demo/security'
