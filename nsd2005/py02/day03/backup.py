c86ac4ed42cfc587f5bfef9a5f9bb799ac6b63b5
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-21 10:40:53 +0800

    nsd2004 py02 day03 10:40

--- nsd2005/py02/day03/backup.py
+++ nsd2005/py02/day03/backup.py
@@ -1,6 +1,18 @@
 import os
 import tarfile
+import hashlib
+import pickle
 from time import strftime
+
+def check_md5(fname):
+    m = hashlib.md5()
+    with open(fname, 'rb') as fobj:
+        while 1:
+            data = fobj.read(4096)
+            if not data:
+                break
+            m.update(data)
+    return m.hexdigest()
 
 def full_backup(src, dst, md5file):
     "完全备份"
@@ -15,6 +27,14 @@ def full_backup(src, dst, md5file):
 
     # 计算每个文件的md5值
     md5dict = {}
+    for path, folders, files in os.walk(src):
+        for file in files:
+            key = os.path.join(path, file)
+            md5dict[key] = check_md5(key)
+
+    # 保存md5字典到文件
+    with open(md5file, 'wb') as fobj:
+        pickle.dump(md5dict, fobj)
 
 
 def incr_backup(src, dst, md5file):
@@ -25,7 +45,7 @@ if __name__ == '__main__':
     src = '/tmp/demo/security'
     dst = '/tmp/demo/backup'
     md5file = '/tmp/demo/backup/md5.data'
-    if strftime('%a') == 'Mon':
+    if strftime('%a') != 'Mon':
         full_backup(src, dst, md5file)
     else:
         incr_backup(src, dst, md5file)
