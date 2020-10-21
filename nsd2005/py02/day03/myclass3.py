e86b170316abce434b75332f0f83210092d283d0
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-21 14:28:07 +0800

    nsd2004 py02 day03 14:30

--- nsd2005/py02/day03/myclass3.py
+++ nsd2005/py02/day03/myclass3.py
@@ -4,6 +4,13 @@ class Weapon:
         self.type = type
         self.strength = strength
 
+class Role:
+    def __init__(self, name, weapon):
+        self.name = name
+        self.weapon = weapon
+
 if __name__ == '__main__':
     ji = Weapon('方天画戟', '物理攻击', 100)
     print(ji.wname, ji.type, ji.strength)
+    lb = Role('吕布', ji)
+    print(lb.name, lb.weapon.wname, lb.weapon.type, lb.weapon.strength)
