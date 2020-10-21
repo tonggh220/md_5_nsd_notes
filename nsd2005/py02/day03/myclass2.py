51e326dbc09c52f00a8b67d1103d3e50e4bf004c
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-21 11:59:23 +0800

    nsd2004 py02 day03 12:00

--- nsd2005/py02/day03/myclass2.py
+++ nsd2005/py02/day03/myclass2.py
@@ -5,7 +5,18 @@ class Role:
         self.name = name
         self.weapon = weapon
 
+    def show_me(self):
+        # 绑定在实例身上的属性，在类中任意位置可见可用
+        print('我是%s，擅用%s' % (self.name, self.weapon))
+
+    def speak(self, words):
+        # 没有绑定在实例身上的变量，只是局部变量，只能用在函数中
+        hh = 'Hahaha'
+        print(hh)
+        print(words)
 
 if __name__ == '__main__':
     lb = Role('吕布', '方天画戟')  # 实例化，创建具体的实例对象
     print(lb.name, lb.weapon)
+    lb.show_me()
+    lb.speak('马中赤兔，人中吕布')
