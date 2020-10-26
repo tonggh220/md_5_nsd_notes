45615cabc2c59e3c4fc87a59f8c9864a1a6ceb73
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-26 17:46:41 +0800

    nsd2004 devops day02 17:50

--- nsd2005/devops/day02/zabbix.py
+++ nsd2005/devops/day02/zabbix.py
@@ -3,6 +3,28 @@ import json
 
 url = 'http://192.168.1.179/zabbix/api_jsonrpc.php'
 headers = {'Content-Type': 'application/json; charset=UTF-8'}
-data = {}
+#################################
+# 对于不需要认证即可获得的信息，可以直接查
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "apiinfo.version",
+#     "params": [],
+#     "id": 1
+# }
+##################################
+# 获取隐私信息，需要token。获取用户的token: 35d1262303910aa34852d585914eac35
+data = {
+    "jsonrpc": "2.0",
+    "method": "user.login",
+    "params": {
+        "user": "Admin",
+        "password": "zabbix"
+    },
+    "id": 1
+}
+
+
+
+#################################
 r = requests.post(url, headers=headers, data=json.dumps(data))
 print(r.json())
