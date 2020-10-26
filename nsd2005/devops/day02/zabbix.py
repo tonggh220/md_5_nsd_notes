848f0dfe6241438f4cf9f31a999de2d7fe1935c4
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-26 18:04:09 +0800

    nsd2004 devops day02 18:00

--- nsd2005/devops/day02/zabbix.py
+++ nsd2005/devops/day02/zabbix.py
@@ -13,17 +13,85 @@ headers = {'Content-Type': 'application/json; charset=UTF-8'}
 # }
 ##################################
 # 获取隐私信息，需要token。获取用户的token: 35d1262303910aa34852d585914eac35
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "user.login",
+#     "params": {
+#         "user": "Admin",
+#         "password": "zabbix"
+#     },
+#     "id": 1
+# }
+##################################
+# 查询组，获取组id: 2
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "hostgroup.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "name": [
+#                 # "Zabbix servers",
+#                 "Linux servers"
+#             ]
+#         }
+#     },
+#     "auth": "35d1262303910aa34852d585914eac35",
+#     "id": 1
+# }
+##################################
+# 获取模板信息, id: 10001'
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "template.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "host": [
+#                 "Template OS Linux",
+#                 # "Template OS Windows"
+#             ]
+#         }
+#     },
+#     "auth": "35d1262303910aa34852d585914eac35",
+#     "id": 1
+# }
+##################################
+# 新建名为web3的主机，在Linux servers组中，应用Template os linux模板
 data = {
     "jsonrpc": "2.0",
-    "method": "user.login",
+    "method": "host.create",
     "params": {
-        "user": "Admin",
-        "password": "zabbix"
+        "host": "web3",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 1,
+                "ip": "192.168.1.2",
+                "dns": "",
+                "port": "10050"
+            }
+        ],
+        "groups": [
+            {
+                "groupid": "2"
+            }
+        ],
+        "templates": [
+            {
+                "templateid": "10001"
+            }
+        ],
+        "inventory_mode": 0,
+        "inventory": {
+            "macaddress_a": "01234",
+            "macaddress_b": "56768"
+        }
     },
+    "auth": "35d1262303910aa34852d585914eac35",
     "id": 1
 }
-
-
 
 #################################
 r = requests.post(url, headers=headers, data=json.dumps(data))
