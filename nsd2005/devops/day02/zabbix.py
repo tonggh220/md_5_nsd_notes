7a3aa2f88f7251298b7da97669ff7b8f34627ba0
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-26 17:35:02 +0800

    nsd2004 devops day02 17:30

---
+++ nsd2005/devops/day02/zabbix.py
@@ -0,0 +1,8 @@
+import requests
+import json
+
+url = 'http://192.168.1.179/zabbix/api_jsonrpc.php'
+headers = {'Content-Type': 'application/json; charset=UTF-8'}
+data = {}
+r = requests.post(url, headers=headers, data=json.dumps(data))
+print(r.json())
