f5bbc4242b05fc9702bfc65e3718503bec015474
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-27 14:51:08 +0800

    nsd2004 devops day03 14:50

--- nsd2005/devops/day03/adhoc.py
+++ nsd2005/devops/day03/adhoc.py
@@ -9,50 +9,52 @@ import ansible.constants as C
 
 # since API is constructed for CLI it expects certain options to always be set, named tuple 'fakes' the args parsing options object
 # 命令选项。connection可用的值有local表示本地执行，有ssh表示远程登陆执行，有smart表示智能判断
-Options = namedtuple('Options', ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'diff'])
+Options = namedtuple('Options',
+                     ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'diff'])
 # options = Options(connection='local', module_path=['/to/mymodules'], forks=10, become=None, become_method=None, become_user=None, check=False, diff=False)
-options = Options(connection='ssh', module_path=['/to/mymodules'], forks=10, become=None, become_method=None, become_user=None, check=False, diff=False)
+options = Options(connection='ssh', module_path=['/to/mymodules'], forks=10, become=None, become_method=None,
+                  become_user=None, check=False, diff=False)
 
-# initialize needed objects
-loader = DataLoader() # Takes care of finding and reading yaml, json and ini files
+# DataLoader负责查找和读取yaml，json和ini文件并转换成Python能识别的格式
+loader = DataLoader()
 passwords = dict(vault_pass='secret')
 
-# create inventory, use path to host config file as source or hosts in a comma separated string
-inventory = InventoryManager(loader=loader, sources='localhost,')
+# 主机清单，有两种实现方式。一种是使用主机清单文件列表，另一种方式，是将所有的主机通过逗号进行分隔，组成一个字符串
+# inventory = InventoryManager(loader=loader, sources='localhost,')
+inventory = InventoryManager(loader=loader, sources=['myansible/hosts'])
 
-# variable manager takes care of merging all the different sources to give you a unifed view of variables available in each context
+# 用于处理变量
 variable_manager = VariableManager(loader=loader, inventory=inventory)
 
-# create datastructure that represents our play, including tasks, this is basically what our YAML loader does internally.
-play_source =  dict(
-        name = "Ansible Play",
-        hosts = 'localhost',
-        gather_facts = 'no',
-        tasks = [
-            dict(action=dict(module='shell', args='ls'), register='shell_out'),
-            dict(action=dict(module='debug', args=dict(msg='{{shell_out.stdout}}')))
-         ]
-    )
+# 创建表示我们的工作（包括任务）的数据结构，这基本上是我们的yaml加载程序在内部执行的操作。
+play_source = dict(
+    name="Ansible Play",
+    hosts='dbservers',  # 在哪些主机上执行任务
+    gather_facts='no',
+    tasks=[
+        dict(action=dict(module='shell', args='ls'), register='shell_out'),
+        dict(action=dict(module='debug', args=dict(msg='{{shell_out.stdout}}')))
+    ]
+)
 
-# Create play object, playbook objects use .load instead of init or new methods,
-# this will also automatically create the task objects from the info provided in play_source
+# 创建play对象
 play = Play().load(play_source, variable_manager=variable_manager, loader=loader)
 
-# Run it - instantiate task queue manager, which takes care of forking and setting up all objects to iterate over host list and tasks
+# 运行play
 tqm = None
 try:
     tqm = TaskQueueManager(
-              inventory=inventory,
-              variable_manager=variable_manager,
-              loader=loader,
-              options=options,
-              passwords=passwords,
-          )
-    result = tqm.run(play) # most interesting data for a play is actually sent to the callback's methods
+        inventory=inventory,
+        variable_manager=variable_manager,
+        loader=loader,
+        options=options,
+        passwords=passwords,
+    )
+    result = tqm.run(play)  # most interesting data for a play is actually sent to the callback's methods
 finally:
-    # we always need to cleanup child procs and the structres we use to communicate with them
+    # 清理
     if tqm is not None:
         tqm.cleanup()
 
-    # Remove ansible tmpdir
+    # 删除临时目录
     shutil.rmtree(C.DEFAULT_LOCAL_TMP, True)
