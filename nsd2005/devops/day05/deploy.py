2375b0ab96240d4a89fc2a3faed1cee877fe6247
MrZhangzhg <zhangzhg@tedu.cn>
2020-10-29 17:38:36 +0800

    nsd2004 devops day05 18:00

--- nsd2005/devops/day05/deploy.py
+++ nsd2005/devops/day05/deploy.py
@@ -1,17 +1,62 @@
 import requests
 import wget
 import os
+import hashlib
+import tarfile
 
 
 def has_new_ver(ver_url, ver_fname):
     "判断是否有新版本，有返回True，没有返回False"
+    # 本地不存在版本文件，则有新版本
+    if not os.path.isfile(ver_fname):
+        return True
+
+    # 如果本地和网上版本不一致，则有新版本
+    with open(ver_fname) as fobj:
+        local_ver = fobj.read()
+
+    r = requests.get(ver_url)
+    if local_ver != r.text:
+        return True
+    else:
+        return False
 
 
 def file_ok(md5url, app_fname):
     "判断文件是否完好，完好返回True，否则返回False"
+    # 计算本地文件md5值
+    m = hashlib.md5()
+    with open(app_fname, 'rb') as fobj:
+        while 1:
+            data = fobj.read(4096)
+            if not data:
+                break
+            m.update(data)
+
+    r = requests.get(md5url)
+    if m.hexdigest() == r.text.strip():  # 删除结尾的\n
+        return True
+    else:
+        return False
 
 def deploy(app_fname, deploy_dir, dest):
     "部署软件"
+    # 解压
+    tar = tarfile.open(app_fname)
+    tar.extractall(path=deploy_dir)
+    tar.close()
+
+    # 拼接出解压目录的绝对路径
+    app_dir = os.path.basename(app_fname)
+    app_dir = app_dir.replace('.tar.gz', '')
+    app_dir = os.path.join(deploy_dir, app_dir)
+
+    # 如果软链接存在，先删除它
+    if os.path.exists(dest):
+        os.remove(dest)
+
+    # 创建软链接
+    os.symlink(app_dir, dest)
 
 if __name__ == '__main__':
     # 判断是否有新版本
@@ -42,3 +87,6 @@ if __name__ == '__main__':
     deploy(app_fname, deploy_dir, dest)
 
     # 更新本地版本文件
+    if os.path.exists(ver_fname):
+        os.remove(ver_fname)
+    wget.download(ver_url, ver_fname)
