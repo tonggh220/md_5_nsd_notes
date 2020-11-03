00ead63f4b9e66117e3fabb107df7c29fbfc327c
MrZhangzhg <zhangzhg@tedu.cn>
2020-11-03 17:34:59 +0800

    nsd2004 devweb day03 17:30

--- nsd2005/devweb/mysite/polls/models.py
+++ nsd2005/devweb/mysite/polls/models.py
@@ -5,7 +5,10 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
 
+    def __str__(self):
+        return "问题:%s" % self.question_text
+
 class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    q = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
