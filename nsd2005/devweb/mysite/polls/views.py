c4f4d475dd5e24976d1dca1067277a20ecebbfd4
MrZhangzhg <zhangzhg@tedu.cn>
2020-11-03 14:47:16 +0800

    nsd2004 devweb day03 14:50

--- nsd2005/devweb/mysite/polls/views.py
+++ nsd2005/devweb/mysite/polls/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# 用户发起的请求，请求对象将会自动成为第一个参数传给函数
+def index(request):
+    # 通过render函数找到名为index.html的网页模板，返回给客户端
+    return render(request, 'index.html')
