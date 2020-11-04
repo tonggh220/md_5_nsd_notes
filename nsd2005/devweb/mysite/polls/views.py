bd6d009960556d4a3675a0543fc67a43a7edc01c
MrZhangzhg <zhangzhg@tedu.cn>
2020-11-04 17:21:56 +0800

    nsd2004 devweb day04 17:20

--- nsd2005/devweb/mysite/polls/views.py
+++ nsd2005/devweb/mysite/polls/views.py
@@ -18,8 +18,16 @@ def result(request, qid):
     return render(request, 'result.html', {'qid': qid})
 
 def vote(request, qid):
-    print('-' * 30)
-    print(dir(request))
-    print('-' * 30)
-    print(request.POST)
+    # print('-' * 30)
+    # print(dir(request))
+    # print('-' * 30)
+    # print(request.POST)
+    question = Question.objects.get(id=qid)
+    # request有名为POST的属性，存储用户通过Post方法提交的数据。它是一个字典对象
+    choice_id = request.POST.get('choice_id')
+    # 取出相应的选项，并把票数加1
+    choice = question.choice_set.get(id=choice_id)
+    choice.votes += 1
+    choice.save()
+    # 重定向到投票结果页, result是urls.py中定义的url名称
     return redirect('result', qid)
