e5b26fec7670978e0642e0d69447b27d9db00a5b
MrZhangzhg <zhangzhg@tedu.cn>
2020-11-05 16:54:58 +0800

    nsd2004 devweb day04 17:00

--- nsd2005/devweb/ansible_project/myansible/webadmin/views.py
+++ nsd2005/devweb/ansible_project/myansible/webadmin/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from webadmin.models import HostGroup
 
-# Create your views here.
+def index(request):
+    return render(request, 'webadmin/index.html')
+
+def add_hosts(request):
+    groups = HostGroup.objects.all()
+    return render(request, 'webadmin/add_hosts.html', {'groups': groups})
