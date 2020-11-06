a8d753cdd48763ff9077d9baacf9ae4668191e60
MrZhangzhg <zhangzhg@tedu.cn>
2020-11-06 10:41:42 +0800

    nsd2004 devweb day06 10:40

--- nsd2005/devweb/ansible_project/myansible/webadmin/views.py
+++ nsd2005/devweb/ansible_project/myansible/webadmin/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from webadmin.models import HostGroup
+from webadmin.models import HostGroup, Module
 
 def index(request):
     return render(request, 'webadmin/index.html')
@@ -19,3 +19,15 @@ def add_hosts(request):
 
     groups = HostGroup.objects.all()
     return render(request, 'webadmin/add_hosts.html', {'groups': groups})
+
+def add_modules(request):
+    if request.method == 'POST':
+        module = request.POST.get('module').strip()  # 去除字符串两边额外的空格
+        param = request.POST.get('param').strip()
+        if module:  # 如果module非空
+            m = Module.objects.get_or_create(modulename=module)[0]
+            if param:   # 如果param也是非空的
+                m.argument_set.get_or_create(arg_text=param)
+
+    modules = Module.objects.all()
+    return render(request, 'webadmin/add_modules.html', {'modules': modules})
