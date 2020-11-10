669d2dabe82599cde73df7a6ab7a1d8af43d2317
zhangzhg <zhangzg@tedu.cn>
2020-11-10 09:42:45 +0800

    nsd2006 py01 day02 9:40

---
+++ nsd2006/py01/day02/day02.py
@@ -0,0 +1,26 @@
+# s1 = 'python'
+#
+# if 3 > 0:
+#     print('yes')
+#     print('ok')
+#
+# if 'th' in s1:
+#     print('string in s1')
+
+if 0.0:
+    print('任何值为0的数字都是假')
+
+if 10:
+    print('非0为真')
+
+if ' ':
+    print('非空字符串为真')
+
+if []:
+    print('空列表，为假')
+
+if (1, 2, 3):
+    print('非空元组，为真')
+
+if {}:
+    print('空字典，为假')
