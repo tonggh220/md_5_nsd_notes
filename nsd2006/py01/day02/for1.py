cc09ee8420ee90a1df41a4cf1eabbe7c166e984c
zhangzhg <zhangzg@tedu.cn>
2020-11-10 16:32:29 +0800

    nsd2006 py01 day02 16:30

---
+++ nsd2006/py01/day02/for1.py
@@ -0,0 +1,19 @@
+s1 = 'abcd'
+names = ['tom', 'jerry', 'jack', 'rose']
+nums = (10, 8, 20)
+d1 = {'name': 'nb', 'age': 20}
+
+for ch in s1:
+    print(ch)
+print('*' * 30)
+
+for name in names:
+    print(name)
+print('*' * 30)
+
+for i in nums:
+    print(i)
+print('*' * 30)
+
+for k in d1:
+    print(k, d1[k])
