53a0deec26fa6aa33a811481d564057ff62d17ce
zhangzhg <zhangzg@tedu.cn>
2020-11-11 17:48:44 +0800

    nsd2006 py01 day03 18:00

--- nsd2006/py01/day03/demo.py
+++ nsd2006/py01/day03/demo.py
@@ -1,6 +1,5 @@
 import star
-import random
 
-star.pstar()
-print(star.hi)
-star.pstar()
+# star.pstar()
+# print(star.hi)
+# star.pstar()
--- nsd2006/py01/day03/star.py
+++ nsd2006/py01/day03/star.py
@@ -2,3 +2,7 @@ hi = "hello world!"
 
 def pstar(n=30):
     print('*' * n)
+
+if __name__ == '__main__':
+    pstar(50)
+    pstar(80)
