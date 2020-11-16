3ab4cc7d2c531991b817445d4aeb42764edb951b
zhangzhg <zhangzg@tedu.cn>
2020-11-16 14:45:08 +0800

    nsd2006 py02 day01 14:40

--- nsd2006/py02/day01/myerr.py
+++ nsd2006/py02/day01/myerr.py
@@ -7,7 +7,14 @@ def get_info2(name, age):
     assert 0 < age < 120, "年龄超出范围(1~119)"
     print('%s is %s years old' % (name, age))
 
-
 if __name__ == '__main__':
-    get_info('nb', 20)
-    get_info2('plj', 188)
+    try:
+        name = input("name: ")
+        age = int(input("age: "))
+        get_info(name, age)
+        get_info2('plj', 188)
+    except (ValueError, AssertionError) as e:
+        print('Error:', e)
+    except (KeyboardInterrupt, EOFError):
+        print('\nBye-bye')
+        exit(1)
