4de9a17ab5068aaaf4591061cf52b7339f459428
zhangzhg <zhangzg@tedu.cn>
2020-11-16 12:00:34 +0800

    nsd2006 py02 day01 12:00

--- nsd2006/py02/day01/mydiv.py
+++ nsd2006/py02/day01/mydiv.py
@@ -1,13 +1,45 @@
+# try:
+#     num = int(input("number: "))
+#     result = 100 / num
+#     print(result)
+#     print('Done')
+# except ValueError:
+#     print("请输入非0输字")
+# except ZeroDivisionError:
+#     print("请输入非0输字")
+# except KeyboardInterrupt:
+#     print("\nBye-bye")
+# except EOFError:
+#     print("\nBye-bye")
+
+# try:
+#     num = int(input("number: "))
+#     result = 100 / num
+#     print(result)
+#     print('Done')
+# except (ValueError, ZeroDivisionError):
+#     print("请输入非0输字")
+# except (KeyboardInterrupt, EOFError):
+#     print("\nBye-bye")
+
+# try:
+#     num = int(input("number: "))
+#     result = 100 / num
+#     print(result)
+#     print('Done')
+# except (ValueError, ZeroDivisionError) as e:  # 将异常保存到变量e
+#     print("请输入非0输字:", e)
+# except (KeyboardInterrupt, EOFError):
+#     print("\nBye-bye")
+
 try:
     num = int(input("number: "))
     result = 100 / num
+except (ValueError, ZeroDivisionError) as e:  # 将异常保存到变量e
+    print("请输入非0输字:", e)
+except (KeyboardInterrupt, EOFError):
+    print("\nBye-bye")
+else:
     print(result)
-    print('Done')
-except ValueError:
-    print("请输入非0输字")
-except ZeroDivisionError:
-    print("请输入非0输字")
-except KeyboardInterrupt:
-    print("\nBye-bye")
-except EOFError:
-    print("\nBye-bye")
+
+print('Done')
