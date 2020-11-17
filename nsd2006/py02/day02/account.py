a22456134b8ffbc8004b35484170586b4237d3b4
zhangzhg <zhangzg@tedu.cn>
2020-11-17 10:43:46 +0800

    nsd2006 py02 day02 10:40

--- nsd2006/py02/day02/account.py
+++ nsd2006/py02/day02/account.py
@@ -60,7 +60,11 @@ def show_menu():
             pickle.dump(init_data, fobj)
 
     while 1:
-        choice = input(prompt).strip()
+        try:
+            choice = input(prompt).strip()
+        except (KeyboardInterrupt, EOFError):
+            choice = '3'  # 如果用户按下ctrl+c或ctrl+d，算用户选了3
+
         if choice not in ['0', '1', '2', '3']:
             print("无效的输入，请重试。")
             continue
