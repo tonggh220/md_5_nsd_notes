d43afd2b09454a31359bb9d3c306e91d6a27638a
zhangzhg <zhangzg@tedu.cn>
2020-11-17 14:39:25 +0800

    nsd2006 py02 day02 14:40

--- nsd2006/py02/day02/math_game.py
+++ nsd2006/py02/day02/math_game.py
@@ -1,6 +1,28 @@
+from random import randint, choice
+
 def exam():
     "出题，用户作答"
-    print('exam')
+    # 随机取出2个数字
+    nums = [randint(1, 100) for i in range(2)]
+    nums.sort(reverse=True)  # 降序排列
+
+    # 随机选出加减法
+    op = choice('+-')
+    # 计算出标准答案
+    if op == '+':
+        result = nums[0] + nums[1]
+    else:
+        result = nums[0] - nums[1]
+
+    # 用户作答，判断对错
+    prompt = '%s %s %s = ' % (nums[0], op, nums[1])
+    answer = int(input(prompt))
+    if answer == result:
+        print("不错哟～")
+    else:
+        print("不对哟～")
+
+    print("正确答案是：\n%s%s" % (prompt, result))
 
 def main():
     "主程序代码逻辑"
