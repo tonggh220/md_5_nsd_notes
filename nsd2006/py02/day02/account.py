9ac58d58ce6739fff409a31d65af87e20761e920
zhangzhg <zhangzg@tedu.cn>
2020-11-17 09:43:17 +0800

    nsd2006 py02 day02 9:40

--- nsd2006/py02/day02/account.py
+++ nsd2006/py02/day02/account.py
@@ -1,3 +1,7 @@
+import os
+import pickle
+from time import strftime
+
 def save():
     "用于记录收入"
     print('save')
@@ -18,6 +22,13 @@ def show_menu():
 (2) 查询
 (3) 退出
 请选择(0/1/2/3): """
+    fname = "account.data"
+    # 如果记账文件不存在，需要初始化它
+    if not os.path.exists(fname):
+        init_data = [[strftime("%Y-%m-%d"), 0, 0, 10000, "init data"]]
+        with open(fname, 'wb') as fobj:
+            pickle.dump(init_data, fobj)
+            
     while 1:
         choice = input(prompt).strip()
         if choice not in ['0', '1', '2', '3']:
