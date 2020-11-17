c677b0aa9e472534ab19d50a43df4a2aa7370880
zhangzhg <zhangzg@tedu.cn>
2020-11-17 10:01:35 +0800

    nsd2006 py02 day02 10:00

--- nsd2006/py02/day02/account.py
+++ nsd2006/py02/day02/account.py
@@ -2,15 +2,27 @@ import os
 import pickle
 from time import strftime
 
-def save():
+def save(fname):
     "用于记录收入"
-    print('save')
+    amount = int(input("金额: "))
+    comment = input("备注: ")
+    date = strftime("%Y-%m-%d")
+    # 从记账文件中取出全部的收支记录，并计算余额
+    with open(fname, 'rb') as fobj:
+        records = pickle.load(fobj)
+    balance = records[-1][-2] + amount
 
-def cost():
+    # 构建最新一笔记录，并写入文件
+    record = [date, amount, 0, balance, comment]
+    records.append(record)
+    with open(fname, 'wb') as fobj:
+        pickle.dump(records, fobj)
+
+def cost(fname):
     "用于记录开销"
     print('cost')
 
-def query():
+def query(fname):
     "用于查询收支情况"
     print('query')
 
@@ -28,7 +40,7 @@ def show_menu():
         init_data = [[strftime("%Y-%m-%d"), 0, 0, 10000, "init data"]]
         with open(fname, 'wb') as fobj:
             pickle.dump(init_data, fobj)
-            
+
     while 1:
         choice = input(prompt).strip()
         if choice not in ['0', '1', '2', '3']:
@@ -39,7 +51,7 @@ def show_menu():
             print('\nBye-bye')
             break
 
-        funcs[choice]()
+        funcs[choice](fname)
 
 if __name__ == '__main__':
     show_menu()
