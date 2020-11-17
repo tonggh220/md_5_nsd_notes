f21c1c7f860b27fc96b101fdceb50b042d17cd07
zhangzhg <zhangzg@tedu.cn>
2020-11-17 16:02:39 +0800

    nsd2006 py02 day02 16:00

--- nsd2006/py02/day02/day02.py
+++ nsd2006/py02/day02/day02.py
@@ -1,8 +1,22 @@
-def func1():
-    print('in func1')
-    func2()
+# def func1():
+#     print('in func1')
+#     func2()
+#
+# def func2():
+#     print('in func2')
+#
+# func1()
 
-def func2():
-    print('in func2')
+from random import randint
 
-func1()
+def func1(x):
+    # 整数求余，返回值只有1或0。1为真，0为假
+    return x % 2
+
+if __name__ == '__main__':
+    nums = [randint(1, 100) for i in range(10)]
+    print(nums)
+    result1 = filter(func1, nums)
+    result2 = filter(lambda x: x % 2, nums)
+    print(list(result1))
+    print(list(result2))
