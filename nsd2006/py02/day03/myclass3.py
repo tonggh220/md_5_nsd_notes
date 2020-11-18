579acc2a484a59d7147a230b3a97ae0bdabdea41
zhangzhg <zhangzg@tedu.cn>
2020-11-18 14:44:54 +0800

    nsd2006 py02 day03 14:40

--- nsd2006/py02/day03/myclass3.py
+++ nsd2006/py02/day03/myclass3.py
@@ -11,8 +11,16 @@ class Role:
         print(hh)
         print(words)
 
+    def walk(self):
+        print('walk walk walk')
+
 class Warrior(Role):  # 括号中的类是该类的父类，也称作基类
-    pass
+    def attack(self, target):
+        print('与%s近身肉搏' % target)
+
+class Mage(Role):
+    def attack(self, target):
+        print('远程打击:%s' % target)
 
 if __name__ == '__main__':
     lb = Warrior('吕布', '方天画戟')  # 实例化，通过类创建具体的实例对象
@@ -21,3 +29,9 @@ if __name__ == '__main__':
     zf = Warrior('张飞', '丈八蛇矛')
     zf.show_me()
     zf.speak('我是燕人张飞张冀德')
+    zgl = Mage('诸葛亮', '羽扇')
+    zgl.show_me()
+    lb.walk()
+    zgl.walk()
+    lb.attack('刘备')
+    zgl.attack('曹操')
