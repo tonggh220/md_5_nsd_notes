cb8b90185a2585e6402189f7218f433866f699b5
zhangzhg <zhangzg@tedu.cn>
2020-11-18 11:42:51 +0800

    nsd2006 py02 day03 11:40

---
+++ nsd2006/py02/day03/myclass.py
@@ -0,0 +1,14 @@
+class Role:
+    def __init__(self, name, weapon):
+        # 构造器方法，实例化时自动调用，注意，self不是关键字，可以是任何合法变量名
+        # 绑定在实身上的属性，可以在类中任意位置使用
+        self.name = name
+        self.weapon = weapon
+
+    def show_me(self):
+        print('我是%s，擅用%s' % (self.name, self.weapon))
+
+if __name__ == '__main__':
+    # 创建实例时，如果类中有__init__方法，自动调用它，实例本身自动成为第一个参数
+    lb = Role('吕布', '方天画戟')  # 实例化，通过类创建具体的实例对象
+    lb.show_me()
