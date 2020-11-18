235baaa6fc3ef170ed2c9f69c20aeb7b462a0080
zhangzhg <zhangzg@tedu.cn>
2020-11-18 15:45:23 +0800

    nsd2006 py02 day03 15:50

--- nsd2006/py02/day03/books.py
+++ nsd2006/py02/day03/books.py
@@ -8,6 +8,11 @@ class Book:
         # 打印、显示实例时，返回这个方法返回的字符串
         return "《%s》" % self.title
 
+    def __call__(self):
+        # 使得实例对象能够像函数一样调用
+        print("《%s》是%s编著的" % (self.title, self.author))
+
 if __name__ == '__main__':
     xyj = Book('西游记', '吴承恩')  # 调用__init__
     print(xyj)   # 调用__str__
+    xyj()        # 调用__call__
