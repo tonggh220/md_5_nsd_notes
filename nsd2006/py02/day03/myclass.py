85acee4727f83cb9e4aa3215ac078c5f9a3f419b
zhangzhg <zhangzg@tedu.cn>
2020-11-18 11:51:57 +0800

    nsd2006 py02 day03 11:50

--- nsd2006/py02/day03/myclass.py
+++ nsd2006/py02/day03/myclass.py
@@ -8,7 +8,14 @@ class Role:
     def show_me(self):
         print('我是%s，擅用%s' % (self.name, self.weapon))
 
+    def speak(self, words):
+        # 没有绑在实例身上的变量，就是局部变量，只能在函数中使用
+        hh = 'Hahahahahaha'
+        print(hh)
+        print(words)
+
 if __name__ == '__main__':
     # 创建实例时，如果类中有__init__方法，自动调用它，实例本身自动成为第一个参数
     lb = Role('吕布', '方天画戟')  # 实例化，通过类创建具体的实例对象
     lb.show_me()
+    lb.speak('马中赤兔，人中吕布')
