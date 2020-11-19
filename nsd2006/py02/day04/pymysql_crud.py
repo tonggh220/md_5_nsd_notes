43a099f66c6bdfbaa7138d4f076d6087e56e8c35
zhangzhg <zhangzg@tedu.cn>
2020-11-19 14:07:26 +0800

    nsd2006 py02 day04 14:10

--- nsd2006/py02/day04/pymysql_crud.py
+++ nsd2006/py02/day04/pymysql_crud.py
@@ -22,16 +22,24 @@ cursor = conn.cursor()
 # cursor.executemany(insert1, [(2, '运维部'), (3, '开发部'), (4, '测试部'), (5, '财务部')])
 ####################################
 # 查询
-select1 = "SELECT id, dep_name FROM departments"
-cursor.execute(select1)
-result1 = cursor.fetchone()    # 取出一行数据
-result2 = cursor.fetchmany(2)  # 继续取出2行数据
-result3 = cursor.fetchall()    # 继续取出剩余全部数据
-print(result1)
-print('*' * 30)
-print(result2)
-print('*' * 30)
-print(result3)
+# select1 = "SELECT id, dep_name FROM departments"
+# cursor.execute(select1)
+# result1 = cursor.fetchone()    # 取出一行数据
+# result2 = cursor.fetchmany(2)  # 继续取出2行数据
+# result3 = cursor.fetchall()    # 继续取出剩余全部数据
+# print(result1)
+# print('*' * 30)
+# print(result2)
+# print('*' * 30)
+# print(result3)
+####################################
+# 修改
+# update1 = "UPDATE departments SET dep_name=%s WHERE dep_name=%s"
+# cursor.execute(update1, ('人力资源部', '人事部'))
+####################################
+# 删除
+delete1 = "DELETE FROM departments WHERE id=%s"
+cursor.execute(delete1, (5,))
 
 
 ####################################
