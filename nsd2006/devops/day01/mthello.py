b2e3f7c85a366edf450c45f2a61e8c0fa766bf53
zhangzhg <zhangzg@tedu.cn>
2020-11-20 11:56:38 +0800

    nsd2006 devops day01 11:56

--- nsd2006/devops/day01/mthello.py
+++ nsd2006/devops/day01/mthello.py
@@ -1,9 +1,19 @@
+# import threading
+#
+# def hello():
+#     print('Hello World!')
+#
+# if __name__ == '__main__':
+#     for i in range(3):
+#         t = threading.Thread(target=hello)  # 创建工作线程
+#         t.start()  # 启动工作线程，相当于调用target()
+
 import threading
 
-def hello():
-    print('Hello World!')
+def hello(word):
+    print('Hello %s!' % word)
 
 if __name__ == '__main__':
-    for i in range(3):
-        t = threading.Thread(target=hello)  # 创建工作线程
-        t.start()  # 启动工作线程，相当于调用target()
+    for w in ['World', 'China', 'Tedu']:
+        t = threading.Thread(target=hello, args=(w,))  # 创建工作线程
+        t.start()  # 启动工作线程，相当于调用target(*args)
