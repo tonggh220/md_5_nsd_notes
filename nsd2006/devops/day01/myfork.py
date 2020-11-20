f32483ef2eb9564401bca10f823d5fd762d73111
zhangzhg <zhangzg@tedu.cn>
2020-11-20 10:34:54 +0800

    nsd2006 devops day01 10:30

--- nsd2006/devops/day01/myfork.py
+++ nsd2006/devops/day01/myfork.py
@@ -1,5 +1,29 @@
+# import os
+#
+# print('starting...')
+# os.fork()
+# print('Hello World!')
+
+# import os
+#
+# print('starting...')
+#
+# ret_val = os.fork()
+# if ret_val:
+#     print('in parent')
+# else:
+#     print('in child')
+#
+# print('Hello World!')
+
+
 import os
 
 print('starting...')
-os.fork()
-print('Hello World!')
+
+for i in range(3):
+    ret_val = os.fork()
+    if not ret_val:
+        print('Hello World!')
+        exit()
+
