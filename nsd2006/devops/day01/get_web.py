20cbd2a2840b3c82e8ed407198687eebe6c6b4b6
zhangzhg <zhangzg@tedu.cn>
2020-11-20 16:51:47 +0800

    nsd2006 devops day01 16:50

--- nsd2006/devops/day01/get_web.py
+++ nsd2006/devops/day01/get_web.py
@@ -1,7 +1,17 @@
 import os
 import wget
+import re
 
-def get_patt(fname, patt):
+def get_patt(fname, patt, charset=None):
+    result = []
+    cpatt = re.compile(patt)
+    with open(fname, encoding=charset) as fobj:
+        for line in fobj:
+            m = cpatt.search(line)
+            if m:
+                result.append(m.group())
+
+    return result
 
 if __name__ == '__main__':
     # 下载网易首页
@@ -10,12 +20,12 @@ if __name__ == '__main__':
     url163 = 'http://www.163.com'
     if not os.path.exists(img_dir):
         os.mkdir(img_dir)
-    if not os.path.exists(url163):
+    if not os.path.exists(fname163):
         wget.download(url163, fname163)
 
     # 取出网易站点上所有的图片url
-    img_patt = ''
-    img_list = get_patt(fname163, img_patt)
+    img_patt = '(http|https)://[\w./-]+\.(jpg|jpeg|png|gif)'
+    img_list = get_patt(fname163, img_patt, 'gbk')
 
     # 下载图片
     for url in img_list:
