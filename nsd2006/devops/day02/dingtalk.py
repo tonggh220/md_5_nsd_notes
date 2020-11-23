7eb1ee70d68f2ec02851752f1d38ee65ed36eaf3
zhangzhg <zhangzg@tedu.cn>
2020-11-23 16:46:47 +0800

    nsd2006 devops day02 16:50

--- nsd2006/devops/day02/dingtalk.py
+++ nsd2006/devops/day02/dingtalk.py
@@ -1,20 +1,38 @@
 import requests
 import json
 
-url = ''
+url = 'https://oapi.dingtalk.com/robot/send?access_token=429e5549e8e107e1849e1466d4164cc4b27a6ad30acc57d88bb1b2ab25a7c1f1'
 headers = {'Content-Type': 'application/json; charset=UTF-8'}
+# data = {
+#     "msgtype": "text",
+#     "text": {
+#         "content": "我就是我, 是不一样的烟火 好好学习天天向上"
+#     },
+#     "at": {  # @哪些电话号码
+#         "atMobiles": [
+#             # "156xxxx8827",
+#             # "189xxxx8325"
+#         ],
+#         "isAtAll": False   # 是否@所有人
+#     }
+# }
+
 data = {
-    "msgtype": "text",
-    "text": {
-        "content": "我就是我, 是不一样的烟火 好好学习天天向上"
+    "msgtype": "markdown",
+    "markdown": {
+        "title": "Offer",
+        "text": """## 入职邀请
+您已被我公司录取，请于2020-12-10来报到，公司详情参见：[TEDU](http://www.tedu.cn)
+![](http://cdn.tmooc.cn/bsfile//imgad///17a5069fdce747a6ae107cb6c2db1199.jpg)
+好好学习天天向上"""
     },
-    "at": {  # @哪些电话号码
+    "at": {
         "atMobiles": [
-            # "156xxxx8827",
-            # "189xxxx8325"
+            # "150XXXXXXXX"
         ],
-        "isAtAll": False   # 是否@所有人
+        "isAtAll": False
     }
 }
+
 r = requests.post(url, headers=headers, data=json.dumps(data))
 print(r.json())
