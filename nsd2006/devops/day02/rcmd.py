fdb5c625e5a3ae761550cdf868ec0a51b21b73e2
zhangzhg <zhangzg@tedu.cn>
2020-11-23 09:16:45 +0800

    nsd2006 devops day02 9:20

---
+++ nsd2006/devops/day02/rcmd.py
@@ -0,0 +1,17 @@
+import paramiko
+
+def rcmd(host, user, passwd, port=22, cmds=None):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, port=port, username=user, password=passwd)
+    stdin, stdout, stderr = ssh.exec_command(cmds)
+    out = stdout.read()
+    err = stderr.read()
+    if out:
+        print("[%s] \033[32;1mOUT\033[0m:\n%s" % (host, out.decode()))
+    if err:
+        print("[%s] \033[31;1mERROR\033[0m:\n%s" % (host, err.decode()))
+    ssh.close()
+
+if __name__ == '__main__':
+    rcmd('127.0.0.1', 'root', 'redhat', 22, 'id root; id alice')
