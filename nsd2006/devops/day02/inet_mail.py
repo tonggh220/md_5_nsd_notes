88358eb3f0c9d09e2ec817c8ed0d2c2d9733c965
zhangzhg <zhangzg@tedu.cn>
2020-11-23 11:00:25 +0800

    nsd2006 devops day02 11:00

---
+++ nsd2006/devops/day02/inet_mail.py
@@ -0,0 +1,27 @@
+from email.mime.text import MIMEText
+from email.header import Header
+import smtplib
+import getpass
+
+def inet_mail(body, sender, receivers, subject, host, passwd):
+    # 准备邮件, plain表示纯文本
+    message = MIMEText(body, 'plain', 'utf8')  # 正文
+    message['From'] = Header(sender, 'utf8')
+    message['To'] = Header(receivers[0], 'utf8')
+    message['Subject'] = Header(subject, 'utf8')
+
+    # 发送邮件
+    smtp = smtplib.SMTP()
+    smtp.connect(host)
+    # smtp.starttls()  # 如果服务器要求安全加接，则打开此注释
+    smtp.login(sender, passwd)
+    smtp.sendmail(sender, receivers, message.as_bytes())
+
+if __name__ == '__main__':
+    body = "python发送互联网邮件测试\n"
+    sender = "zhangzhigang79@qq.com"
+    receivers = ['zhangzhigang79@qq.com', 'zhangzhigang79@126.com']
+    subject = "py test"
+    host = 'smtp.qq.com'
+    passwd = getpass.getpass()  # 密码不是登陆密码，而是授权码
+    inet_mail(body, sender, receivers, subject, host, passwd)
