7546804c1bc6671cf28fd72fcb39f314b1873cf6
zhangzhg <zhangzg@tedu.cn>
2020-11-23 17:59:18 +0800

    nsd2006 devops day02 18:00

--- nsd2006/devops/day02/zabbix.py
+++ nsd2006/devops/day02/zabbix.py
@@ -24,24 +24,75 @@ headers = {'Content-Type': 'application/json; charset=UTF-8'}
 # }
 ###################################
 # 查询Linux Servers组的信息
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "hostgroup.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "name": [
+#                 # "Zabbix servers",
+#                 "Linux servers"  # 'groupid': '2'
+#             ]
+#         }
+#     },
+#     "auth": "56428ec28bd99f57a3b3422c35ce1110",
+#     "id": 1
+# }
+###################################
+# 查询Template os linux模板的id
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "template.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "host": [
+#                 "Template OS Linux",
+#                 # "Template OS Windows"  # 'templateid': '10001'
+#             ]
+#         }
+#     },
+#     "auth": "56428ec28bd99f57a3b3422c35ce1110",
+#     "id": 1
+# }
+###################################
+# 创建名为web1的主机，加入到Linux servers组中，应用Template OS linux模板
 data = {
     "jsonrpc": "2.0",
-    "method": "hostgroup.get",
+    "method": "host.create",
     "params": {
-        "output": "extend",
-        "filter": {
-            "name": [
-                # "Zabbix servers",
-                "Linux servers"  # 'groupid': '2'
-            ]
+        "host": "web1",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 1,
+                "ip": "192.168.1.10",
+                "dns": "",
+                "port": "10050"
+            }
+        ],
+        "groups": [
+            {
+                "groupid": "2"
+            }
+        ],
+        "templates": [
+            {
+                "templateid": "10001"
+            }
+        ],
+        "inventory_mode": 0,
+        "inventory": {
+            "macaddress_a": "01234",
+            "macaddress_b": "56768"
         }
     },
     "auth": "56428ec28bd99f57a3b3422c35ce1110",
     "id": 1
 }
 
-
 ###################################
 r = requests.post(url, headers=headers, data=json.dumps(data))
 print(r.json())  # 主要关心result的内容
-
