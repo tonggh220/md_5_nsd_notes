ffebf3931a1cdc3868ed90e9f9765b1ac510e48d
zhangzhg <zhangzg@tedu.cn>
2020-11-24 15:39:51 +0800

    nsd2006 devops day03 15:40

--- nsd2006/devops/day03/adhoc.py
+++ nsd2006/devops/day03/adhoc.py
@@ -9,8 +9,10 @@ import ansible.constants as C
 
 # 为ansible设置默认选项
 # connection是连接方式。local表示本地执行；ssh表示远程连接服务器执行；smart表示智能选择
-Options = namedtuple('Options', ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'diff'])
-options = Options(connection='ssh', module_path=['/to/mymodules'], forks=10, become=None, become_method=None, become_user=None, check=False, diff=False)
+Options = namedtuple('Options',
+                     ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'diff'])
+options = Options(connection='ssh', module_path=['/to/mymodules'], forks=10, become=None, become_method=None,
+                  become_user=None, check=False, diff=False)
 
 # Dataloader用于读取、分析json/yaml/ini文件，把它们表示成python能识别的数据类型
 loader = DataLoader()
@@ -22,15 +24,15 @@ inventory = InventoryManager(loader=loader, sources=['myansible/hosts'])
 # 变量管理器
 variable_manager = VariableManager(loader=loader, inventory=inventory)
 # 创建play源
-play_source =  dict(
-        name = "Ansible Play",  # play名
-        hosts = 'dbservers',    # 在哪些主机上执行命令
-        gather_facts = 'no',    # 不收集facts信息
-        tasks = [
-            dict(action=dict(module='shell', args='ls /home'), register='shell_out'),
-            dict(action=dict(module='debug', args=dict(msg='{{shell_out.stdout}}')))
-         ]
-    )
+play_source = dict(
+    name="Ansible Play",  # play名
+    hosts='dbservers',  # 在哪些主机上执行命令
+    gather_facts='no',  # 不收集facts信息
+    tasks=[
+        dict(action=dict(module='user', args='name=bob state=present'), register='shell_out'),
+        dict(action=dict(module='debug', args=dict(msg='{{shell_out}}')))
+    ]
+)
 
 # Create play object, playbook objects use .load instead of init or new methods,
 # this will also automatically create the task objects from the info provided in play_source
@@ -40,13 +42,13 @@ play = Play().load(play_source, variable_manager=variable_manager, loader=loader
 tqm = None
 try:
     tqm = TaskQueueManager(
-              inventory=inventory,
-              variable_manager=variable_manager,
-              loader=loader,
-              options=options,
-              passwords=passwords,
-          )
-    result = tqm.run(play) # most interesting data for a play is actually sent to the callback's methods
+        inventory=inventory,
+        variable_manager=variable_manager,
+        loader=loader,
+        options=options,
+        passwords=passwords,
+    )
+    result = tqm.run(play)  # most interesting data for a play is actually sent to the callback's methods
 finally:
     # we always need to cleanup child procs and the structres we use to communicate with them
     if tqm is not None:
