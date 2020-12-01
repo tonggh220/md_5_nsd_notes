ca7e4e66edba8a4e42567dd405e4e2a9ca4caeb7
zhangzhg <zhangzg@tedu.cn>
2020-12-01 15:34:37 +0800

    nsd2006 devweb day03 15:30

--- nsd2006/devweb/day0304/mysite/polls/urls.py
+++ nsd2006/devweb/day0304/mysite/polls/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from polls import views
 
 urlpatterns = [
@@ -9,5 +9,7 @@ urlpatterns = [
     # path函数中的路径，支持变量。声明一个名为qid的变量，类型是int, 变量将会传递给detail函数
     # 变量类型还有str和slug，slug很少应用到
     path('<int:qid>', views.detail, name='detail'),
+    # path函数也可以替换为正则表达式，如上面的写法，也可以写为：
+    # re_path('^(\d+)$', views.detail, name='detail'),
     path('<int:qid>/result', views.result, name='result'),
 ]
