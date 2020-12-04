2e26499f70d39668c0e168947a60655d1ec9aa41
zhangzhg <zhangzg@tedu.cn>
2020-12-04 09:25:40 +0800

    nsd2006 devweb day06 9:25

--- nsd2006/devweb/ansible_project/myansible/webadmin/views.py
+++ nsd2006/devweb/ansible_project/myansible/webadmin/views.py
@@ -5,5 +5,9 @@ def index(request):
     return render(request, 'webadmin/index.html')
 
 def add_hosts(request):
+    # print('-' * 50)
+    # print(request.method)
+    # print('-' * 50)
+    
     groups = HostGroup.objects.all()
     return render(request, 'webadmin/add_hosts.html', {'groups': groups})
