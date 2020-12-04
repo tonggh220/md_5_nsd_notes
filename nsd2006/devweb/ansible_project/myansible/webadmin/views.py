77a5ae9646cb480b152cdb93d820f12776bfa35c
zhangzhg <zhangzg@tedu.cn>
2020-12-04 09:48:36 +0800

    nsd2006 devweb day06 9:50

--- nsd2006/devweb/ansible_project/myansible/webadmin/views.py
+++ nsd2006/devweb/ansible_project/myansible/webadmin/views.py
@@ -8,6 +8,14 @@ def add_hosts(request):
     # print('-' * 50)
     # print(request.method)
     # print('-' * 50)
-    
+    if request.method == 'POST':
+        group = request.POST.get('group').strip()  # 去除字符串两站空白字符
+        host = request.POST.get('host').strip()
+        ip = request.POST.get('ip').strip()
+        if group:  # 如果group非空
+            g = HostGroup.objects.get_or_create(groupname=group)[0]
+            if host and ip:  # 如果host和ip也是非空的
+                g.host_set.get_or_create(hostname=host, ip_addr=ip)
+
     groups = HostGroup.objects.all()
     return render(request, 'webadmin/add_hosts.html', {'groups': groups})
