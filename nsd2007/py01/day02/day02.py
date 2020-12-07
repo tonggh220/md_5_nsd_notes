4188b8cccab2adc01b35983da025a650acf17d26
zhangzhg <zhangzg@tedu.cn>
2020-12-07 14:27:38 +0800

    nsd2007 py01 day01 14:30

---
+++ nsd2007/py01/day02/day02.py
@@ -0,0 +1,16 @@
+# if 3 > 0:
+#     print('yes')
+#     print('ok')
+
+# if 0.0:
+#     print('任何值为0的数字都是False')
+#
+# if 10:
+#     print('非0为真')
+
+if ' ':
+    print('任何非空对象都为真，空为假')
+    print('空格字符，也是一个字符')
+
+if []:
+    print('空列表，为假')
