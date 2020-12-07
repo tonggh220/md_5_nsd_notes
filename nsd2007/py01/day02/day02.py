f48df707a4902391cef3bd828396f03819f01de2
zhangzhg <zhangzg@tedu.cn>
2020-12-07 14:53:46 +0800

    nsd2007 py01 day01 14:50

--- nsd2007/py01/day02/day02.py
+++ nsd2007/py01/day02/day02.py
@@ -8,9 +8,22 @@
 # if 10:
 #     print('非0为真')
 
-if ' ':
-    print('任何非空对象都为真，空为假')
-    print('空格字符，也是一个字符')
+# if ' ':
+#     print('任何非空对象都为真，空为假')
+#     print('空格字符，也是一个字符')
+#
+# if []:
+#     print('空列表，为假')
 
-if []:
-    print('空列表，为假')
+# a = 10
+# b = 20
+# if a < b:
+#     s1 = a
+# else:
+#     s1 = b
+# print(s1)
+
+a = 10
+b = 20
+s1 = a if a < b else b
+print(s1)
---
+++ nsd2007/py01/day02/login.py
@@ -0,0 +1,9 @@
+import getpass
+
+username = input("username: ")
+password = getpass.getpass("password: ")
+
+if username == "bob" and password == '123456':
+    print("Login Successful")
+else:
+    print("Login Incorrect")
