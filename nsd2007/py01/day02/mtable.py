a12b4f4b562a75f6f36766108aa895bf1d93d109
zhangzhg <zhangzg@tedu.cn>
2020-12-08 15:05:02 +0800

    nsd2007 py01 day02 15:00

---
+++ nsd2007/py01/day02/mtable.py
@@ -0,0 +1,13 @@
+# for j in range(1, 4):   # [1, 2, 3]
+#     print('Hello', end='\t')
+# print()
+
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print('Hello', end='\t')
+#     print()
+
+for i in range(1, 10):
+    for j in range(1, i + 1):
+        print('Hello', end='\t')
+    print()
