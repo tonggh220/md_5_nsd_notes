d77de0800772cf2f39496911d40b9ffabf6d7c4b
zhangzhg <zhangzg@tedu.cn>
2020-12-09 14:38:52 +0800

    nsd2007 py01 day03 14:40

---
+++ nsd2007/py01/day03/cp4.py
@@ -0,0 +1,16 @@
+import sys
+
+def copy(src_fname, dst_fname):
+    src_fobj = open(src_fname, 'rb')
+    dst_fobj = open(dst_fname, 'wb')
+
+    while 1:
+        data = src_fobj.read(4096)
+        if not data:  # data为空时表示假，取反为真；data非空时为真，取反为假
+            break
+        dst_fobj.write(data)
+
+    src_fobj.close()
+    dst_fobj.close()
+
+copy(sys.argv[1], sys.argv[2])
---
+++ nsd2007/py01/day03/pos.py
@@ -0,0 +1,5 @@
+import sys
+
+print(sys.argv)
+print('*' * 30)
+print(sys.argv[0], sys.argv[1], sys.argv[2])
