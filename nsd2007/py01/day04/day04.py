63fed8e6c53ba260e077a5fd2a63ab1ccb009cd7
zhangzhg <zhangzg@tedu.cn>
2020-12-10 15:23:45 +0800

    nsd2007 py01 day04 15:24

--- nsd2007/py01/day04/day04.py
+++ nsd2007/py01/day04/day04.py
@@ -1,13 +1,26 @@
 names = ['tom', 'jerry', 'bob', 'alice']
 
+# >>> list(enumerate(names))
+# [(0, 'tom'), (1, 'jerry'), (2, 'bob'), (3, 'alice')]
+
+# for data in enumerate(names):
+#     print(data)
+
+# for data in enumerate(names):
+#     print(data[0], data[1])
+
+for i, name in enumerate(names):
+    print(i, name)
+
+
 # for i in [0, 1, 2, 3]:
 #     print(i, names[i])
 
 # for i in range(4):
 #     print(i, names[i])
 
-for i in range(len(names)):
-    print(i, names[i])
+# for i in range(len(names)):
+#     print(i, names[i])
 
 # 0 tom
 # 1 jerry
