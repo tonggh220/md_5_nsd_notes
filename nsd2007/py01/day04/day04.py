67dd8072123d9fa9cd5b32925cac7e4270025688
zhangzhg <zhangzg@tedu.cn>
2020-12-10 17:31:39 +0800

    nsd2007 py01 day04 17:30

--- nsd2007/py01/day04/day04.py
+++ nsd2007/py01/day04/day04.py
@@ -1,4 +1,4 @@
-names = ['tom', 'jerry', 'bob', 'alice']
+# names = ['tom', 'jerry', 'bob', 'alice']
 
 # >>> list(enumerate(names))
 # [(0, 'tom'), (1, 'jerry'), (2, 'bob'), (3, 'alice')]
@@ -9,8 +9,8 @@ names = ['tom', 'jerry', 'bob', 'alice']
 # for data in enumerate(names):
 #     print(data[0], data[1])
 
-for i, name in enumerate(names):
-    print(i, name)
+# for i, name in enumerate(names):
+#     print(i, name)
 
 
 # for i in [0, 1, 2, 3]:
@@ -26,3 +26,16 @@ for i, name in enumerate(names):
 # 1 jerry
 # 2 bob
 # 3 alice
+
+def isdigit(s):   # s = '1234' / s = '123a456'
+    for ch in s:
+        if ch not in '0123456789':
+            print('不全是数字')
+            break
+    else:
+        print('是数字')
+
+if __name__ == '__main__':
+    isdigit('1234')
+    isdigit('123a456')
+
