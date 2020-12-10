75d907ff02056381e0bf137f0629dbc48129f77e
zhangzhg <zhangzg@tedu.cn>
2020-12-10 16:49:31 +0800

    nsd2007 py01 day04 16:50

--- nsd2007/py01/day04/adduser.py
+++ nsd2007/py01/day04/adduser.py
@@ -10,8 +10,15 @@ def add_user(username, password, fname):
         exit(1)   # 程序遇到exit将会彻底结束。数字1是退出码，即$?的值
 
     # 创建用户，设置密码
+    subprocess.run('useradd %s' % username, shell=True)
+    subprocess.run(
+        'echo %s | passwd --stdin %s' % (password, username), shell=True
+    )
 
     # 用户信息写入文件
+    info = 'username:%s\npassword:%s\n' % (username, password)
+    with open(fname, 'a') as fobj:   # a表示追加方式打开文件
+        fobj.write(info)
 
 if __name__ == '__main__':
     username = sys.argv[1]
