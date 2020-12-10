4e59bac1f598d1770afb27778dce783968caf077
zhangzhg <zhangzg@tedu.cn>
2020-12-10 15:14:35 +0800

    nsd2007 py01 day04 15:20

--- nsd2007/py01/day04/day04.py
+++ nsd2007/py01/day04/day04.py
@@ -1,5 +1,15 @@
-import shutil
-import string
+names = ['tom', 'jerry', 'bob', 'alice']
 
-shutil.copyfileobj()
-shutil.copy()
+# for i in [0, 1, 2, 3]:
+#     print(i, names[i])
+
+# for i in range(4):
+#     print(i, names[i])
+
+for i in range(len(names)):
+    print(i, names[i])
+
+# 0 tom
+# 1 jerry
+# 2 bob
+# 3 alice
