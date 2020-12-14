ba671902c4474b4a2b9a827c6071c3e10ec817c4
zhangzhg <zhangzg@tedu.cn>
2020-12-14 14:08:54 +0800

    nsd2007 py02 day01 14:10

--- nsd2007/py02/day01/mydiv.py
+++ nsd2007/py02/day01/mydiv.py
@@ -1,4 +1,7 @@
-num = int(input("number: "))
-result = 100 / num
-print(result)
-print('Done')
+try:
+    num = int(input("number: "))
+    result = 100 / num
+    print(result)
+    print('Done')
+except ValueError:
+    print("必须输入非0数字")
