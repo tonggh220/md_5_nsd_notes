bd1e48d1783716553a9ef134c5e44ed5b8450b1a
zhangzhg <zhangzg@tedu.cn>
2020-12-14 14:33:04 +0800

    nsd2007 py02 day01 14:32

--- nsd2007/py02/day01/mydiv.py
+++ nsd2007/py02/day01/mydiv.py
@@ -22,13 +22,25 @@
 # except (KeyboardInterrupt, EOFError):
 #     print('\nBye-bye')
 
+# try:
+#     num = int(input("number: "))
+#     result = 100 / num
+#     print(result)
+#     print('Done')
+# except (ValueError, ZeroDivisionError) as e:  # 把异常保存到变量e
+#     print("必须输入非0数字:", e)
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+
+
 try:
     num = int(input("number: "))
     result = 100 / num
-    print(result)
-    print('Done')
 except (ValueError, ZeroDivisionError) as e:  # 把异常保存到变量e
     print("必须输入非0数字:", e)
 except (KeyboardInterrupt, EOFError):
     print('\nBye-bye')
+else:   # 只有不发生异常时，才会执行else语句
+    print(result)
 
+print('Done')
