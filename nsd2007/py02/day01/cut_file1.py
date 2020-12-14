a2dfdcff721952ca0849bd7b029fd4ac1a36c584
zhangzhg <zhangzg@tedu.cn>
2020-12-14 10:48:58 +0800

    nsd2007 py02 day01 10:50

---
+++ nsd2007/py02/day01/cut_file1.py
@@ -0,0 +1,12 @@
+import time
+
+# 定义9点和12点的时间
+t9 = time.strptime('2019-05-15 09:00:00', '%Y-%m-%d %H:%M:%S')
+t12 = time.strptime('2019-05-15 12:00:00', '%Y-%m-%d %H:%M:%S')
+
+# 取出文件的每一行的时间，如果时间介于9点和12点之间，就输出
+with open('mylog.txt') as fobj:
+    for line in fobj:
+        t = time.strptime(line[:19], '%Y-%m-%d %H:%M:%S')
+        if t9 <= t <= t12:
+            print(line, end='')
