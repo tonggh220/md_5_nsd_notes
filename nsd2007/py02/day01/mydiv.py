5ec8616ec4f345dc54e351de4b4685cfd76ae69c
zhangzhg <zhangzg@tedu.cn>
2020-12-14 14:20:23 +0800

    nsd2007 py02 day01 14:20

--- nsd2007/py02/day01/mydiv.py
+++ nsd2007/py02/day01/mydiv.py
@@ -1,15 +1,26 @@
+# try:
+#     num = int(input("number: "))
+#     result = 100 / num
+#     print(result)
+#     print('Done')
+# except ValueError:
+#     print("必须输入非0数字")
+# except ZeroDivisionError:
+#     print("必须输入非0数字")
+# except KeyboardInterrupt:
+#     print('\nBye-bye')
+# except EOFError:
+#     print('\nBye-bye')
+
 try:
     num = int(input("number: "))
     result = 100 / num
     print(result)
     print('Done')
-except ValueError:
+except (ValueError, ZeroDivisionError):
     print("必须输入非0数字")
-except ZeroDivisionError:
-    print("必须输入非0数字")
-except KeyboardInterrupt:
+except (KeyboardInterrupt, EOFError):
     print('\nBye-bye')
-except EOFError:
-    print('\nBye-bye')
+
 
 
