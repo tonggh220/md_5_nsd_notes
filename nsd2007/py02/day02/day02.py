3d537fd1c31a433d461aeee78889cc4edf73f00e
zhangzhg <zhangzg@tedu.cn>
2020-12-15 16:55:39 +0800

    nsd2007 py02 day02 16:50

--- nsd2007/py02/day02/day02.py
+++ nsd2007/py02/day02/day02.py
@@ -8,25 +8,29 @@
 # if __name__ == '__main__':
 #     func1()
 
+# from random import randint
+#
+# def func1(x):
+#     return x % 2  # x%2的结果只有1和0两种情况，0为假，非0为真
+#
+# if __name__ == '__main__':
+#     nums = [randint(1, 100) for i in range(10)]
+#     print(nums)
+#     # 把nums中的偶数过虑掉
+#     result1 = filter(func1, nums)
+#     print(list(result1))
+#     result2 = filter(lambda x: x % 2, nums)
+#     print(list(result2))
+
 from random import randint
 
 def func1(x):
-    return x % 2  # x%2的结果只有1和0两种情况，0为假，非0为真
+    return 100 + x
 
 if __name__ == '__main__':
     nums = [randint(1, 100) for i in range(10)]
     print(nums)
-    # 把nums中的偶数过虑掉
-    result1 = filter(func1, nums)
+    result1 = map(func1, nums)
     print(list(result1))
-
-
-
-
-
-
-
-
-
-
-
+    result2 = map(lambda x: 100 + x, nums)
+    print(list(result2))
