0d814f2720570f77e247326eaa657e3af54ceb4e
zhangzhg <zhangzg@tedu.cn>
2020-12-15 09:39:58 +0800

    nsd2007 py02 day02 9:40

---
+++ nsd2007/py02/day03/account.py
@@ -0,0 +1,35 @@
+
+def save():
+    "用于记录收入"
+
+def cost():
+    "用于记录开销"
+
+def query():
+    "用于查询收支记录"
+
+def show_menu():
+    "主程序逻辑代码"
+    funcs = {'0': save, '1': cost, '2': query}
+    prompt = """(0) 收入
+(1) 开销
+(2) 查询
+(3) 退出
+请选择(0/1/2/3): """
+
+    while 1:
+        choice = input(prompt).strip()
+        if choice not in ['0', '1', '2', '3']:
+            print("无效的选择，请重试。")
+            continue
+
+        if choice == '3':
+            print('\nBye-bye')
+            break
+
+        funcs[choice]()
+
+if __name__ == '__main__':
+    show_menu()
+
+
