629377b9b15b0b4cf3173eeb606f0685560c0279
zhangzhg <zhangzg@tedu.cn>
2020-12-15 14:47:04 +0800

    nsd2007 py02 day02 14:50

--- nsd2007/py02/day02/day02.py
+++ nsd2007/py02/day02/day02.py
@@ -1,9 +1,10 @@
-def func1():
-    print('in func1')
-    func2()
+# def func1():
+#     print('in func1')
+#     func2()
+#
+# def func2():
+#     print('In Func2')
+#
+# if __name__ == '__main__':
+#     func1()
 
-def func2():
-    print('In Func2')
-
-if __name__ == '__main__':
-    func1()
---
+++ nsd2007/py02/day02/math_game.py
@@ -0,0 +1,23 @@
+# 1 + 1 = 2
+# Very Good!
+# Continue(y/n)? yes
+# 18 + 25 = 30
+# Wrong Answer
+# 18 + 25 = 40
+# Wrong Answer
+# 18 + 25 = 50
+# Wrong Answer
+# The Answer:
+# 18 + 25 = 43
+# Continue(y/n)? n
+# Bye-bye
+
+def exam():
+    "出题，用户作答"
+
+def main():
+    "主程序代码逻辑"
+
+if __name__ == '__main__':
+    main()
+
