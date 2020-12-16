127a42bbcadfab09b75550eb85b5f4327efcc426
zhangzhg <zhangzg@tedu.cn>
2020-12-16 16:28:26 +0800

    nsd2007 py02 day03 16:30

---
+++ nsd2007/py02/day03/myclass5.py
@@ -0,0 +1,17 @@
+class A:
+    def func1(self):
+        print('A func1')
+
+class B:
+    def func2(self):
+        print('B func2')
+
+class C(A, B):
+    def func3(self):
+        print('C func3')
+
+if __name__ == '__main__':
+    c1 = C()
+    c1.func1()
+    c1.func2()
+    c1.func3()
