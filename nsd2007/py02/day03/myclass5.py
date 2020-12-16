1a593073c6187d981853c6b4d21b10362bf327cb
zhangzhg <zhangzg@tedu.cn>
2020-12-16 16:35:15 +0800

    nsd2007 py02 day03 16:40

--- nsd2007/py02/day03/myclass5.py
+++ nsd2007/py02/day03/myclass5.py
@@ -2,16 +2,26 @@ class A:
     def func1(self):
         print('A func1')
 
+    def func4(self):
+        print('AAAA func4')
+
 class B:
     def func2(self):
         print('B func2')
 
-class C(A, B):
+    def func4(self):
+        print('func4 BBBB')
+
+class C(B, A):
     def func3(self):
         print('C func3')
+
+    def func4(self):
+        print('cccc func4')
 
 if __name__ == '__main__':
     c1 = C()
     c1.func1()
     c1.func2()
     c1.func3()
+    c1.func4()
