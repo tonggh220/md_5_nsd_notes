e5073408a4c9d382fddadc9fdcd8b2fba44558c8
zhangzhg <zhangzg@tedu.cn>
2020-12-16 16:56:03 +0800

    nsd2007 py02 day03 16:50

---
+++ nsd2007/py02/day03/books.py
@@ -0,0 +1,18 @@
+class Book:
+    def __init__(self, title, author):
+        "构造器方法，用于将属性绑定到实例身上。实例化时自动调用"
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        "打印、显示实例时，将展示这个方法返回的字符串"
+        return "《%s》" % self.title
+
+    def __call__(self):
+        "实例像函数一样调用时，执行此方法中的代码"
+        print("《%s》是%s编写的" % (self.title, self.author))
+
+if __name__ == '__main__':
+    linux = Book('Linux运维之道', '丁明一')  # 调用__init__
+    print(linux)  # 调用__str__
+    linux()       # 调用__call__
