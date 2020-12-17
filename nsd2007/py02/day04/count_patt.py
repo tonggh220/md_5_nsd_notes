6508526c23b2e4240902de051043c6926d4c14cc
zhangzhg <zhangzg@tedu.cn>
2020-12-17 10:23:00 +0800

    nsd2007 py02 day04 10:20

--- nsd2007/py02/day04/count_patt.py
+++ nsd2007/py02/day04/count_patt.py
@@ -2,6 +2,17 @@ import re
 
 def count_patt(fname, patt):
     "在给定的文件fname中，统计各种模式出现的次数"
+    result = {}
+    cpatt = re.compile(patt)  # 为了更好的执行效率，把模式编译
+    # 在文件中的每一行匹配模式，匹配到就存入字典
+    with open(fname) as fobj:
+        for line in fobj:
+            m = cpatt.search(line)
+            if m != None:  # 如果匹配结果不是None
+                key = m.group()
+                result[key] = result.get(key, 0) + 1
+
+    return result
 
 
 if __name__ == '__main__':
