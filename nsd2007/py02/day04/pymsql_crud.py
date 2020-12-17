5add08331b26e580d7c38563c3111d17a48fde4c
zhangzhg <zhangzg@tedu.cn>
2020-12-17 14:42:44 +0800

    nsd2007 py02 day04 14:40

--- nsd2007/py02/day04/pymsql_crud.py
+++ nsd2007/py02/day04/pymsql_crud.py
@@ -1,3 +1,4 @@
+# create / retrieve / update / delete
 import pymysql
 
 # 创建到数据库的连接
@@ -12,14 +13,15 @@ conn = pymysql.connect(
 # 创建游标。游标类似于文件对象，通过文件对象可以读写文件，通过游标可以对数据库进行增删改查
 cursor = conn.cursor()
 
-# 编写SQL语句
-create_dep = """CREATE TABLE departments(
-id INT, dep_name VARCHAR (20),
-PRIMARY KEY (id)
-)"""
+# 编写SQL语句, 新增记录
+insert1 = "INSERT INTO departments VALUES(%s, %s)"
 
-# 通过游标执行sql语句
-cursor.execute(create_dep)
+# 新增一个部门
+cursor.execute(insert1, (1, '人事部'))
+
+# 增加多个部门
+deps = [(2, '运维部'), (3, '开发部'), (4, '测试部'), (5, '财务部')]
+cursor.executemany(insert1, deps)
 
 # 如果是增删改操作，必须执行确认
 conn.commit()
