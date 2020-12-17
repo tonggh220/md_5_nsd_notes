87a85d4d2ffa93a82bff9812e392e5a4488f9f59
zhangzhg <zhangzg@tedu.cn>
2020-12-17 14:53:23 +0800

    nsd2007 py02 day04 14:50

--- nsd2007/py02/day04/pymsql_crud.py
+++ nsd2007/py02/day04/pymsql_crud.py
@@ -13,15 +13,29 @@ conn = pymysql.connect(
 # 创建游标。游标类似于文件对象，通过文件对象可以读写文件，通过游标可以对数据库进行增删改查
 cursor = conn.cursor()
 
+#######################################
 # 编写SQL语句, 新增记录
-insert1 = "INSERT INTO departments VALUES(%s, %s)"
+# insert1 = "INSERT INTO departments VALUES(%s, %s)"
 
 # 新增一个部门
-cursor.execute(insert1, (1, '人事部'))
+# cursor.execute(insert1, (1, '人事部'))
 
 # 增加多个部门
-deps = [(2, '运维部'), (3, '开发部'), (4, '测试部'), (5, '财务部')]
-cursor.executemany(insert1, deps)
+# deps = [(2, '运维部'), (3, '开发部'), (4, '测试部'), (5, '财务部')]
+# cursor.executemany(insert1, deps)
+
+#######################################
+# 查询
+select1 = "SELECT id, dep_name FROM departments"
+cursor.execute(select1)
+result1 = cursor.fetchone()    # 取出一条数据
+result2 = cursor.fetchmany(2)  # 继续取出2条记录
+result3 = cursor.fetchall()    # 继续取出剩余所有记录
+print(result1)
+print('-' * 50)
+print(result2)
+print('-' * 50)
+print(result3)
 
 # 如果是增删改操作，必须执行确认
 conn.commit()
