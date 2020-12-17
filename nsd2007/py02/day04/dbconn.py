0ddf68d75e2f01d6ddf223126c5544db76dd7d8d
zhangzhg <zhangzg@tedu.cn>
2020-12-17 16:36:46 +0800

    nsd2007 py02 day04 16:40

--- nsd2007/py02/day04/dbconn.py
+++ nsd2007/py02/day04/dbconn.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -7,7 +7,20 @@ engine = create_engine(
     # mysql+pymysql://用户名:密码@服务器地址/数据库?参数
     'mysql+pymysql://root:tedu.cn@127.0.0.1/tedu2007?charset=utf8',
     encoding='utf8',
-    echo=True  # 在屏幕上输出调试信息，生产环境不要打开
+    # echo=True  # 在屏幕上输出调试信息，生产环境不要打开
 )
 Base = declarative_base()  # 生成映射类的基类
 Session = sessionmaker(bind=engine)  # 创建到数据库的会话连接类
+
+# 声明映射类
+class Department(Base):  # 必须继承于Base
+    __tablename__ = 'departments'  # 固定格式，声明该类与哪张表关联
+    # 类变量对应字段名，必须是Column的实例
+    # 数据库的INT类型，sqlalchemy定义为Integer类
+    id = Column(Integer, primary_key=True)
+    dep_name = Column(String(20), unique=True)  # String对应VARCHAR
+
+if __name__ == '__main__':
+    # 数据库中如果不存在表，则创建；存在则忽略
+    Base.metadata.create_all(engine)
+
