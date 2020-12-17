fcdc4e2c7f5ae361870f56107e98e61e146cb1cd
zhangzhg <zhangzg@tedu.cn>
2020-12-17 09:55:09 +0800

    nsd2007 py02 day04 9:50

--- nsd2007/py02/day04/count_patt.py
+++ nsd2007/py02/day04/count_patt.py
@@ -1,3 +1,4 @@
+import re
 
 def count_patt(fname, patt):
     "在给定的文件fname中，统计各种模式出现的次数"
@@ -5,8 +6,8 @@ def count_patt(fname, patt):
 
 if __name__ == '__main__':
     fname = 'access_log'
-    ip = ''
-    br = ''
+    ip = '^(\d+\.){3}\d+'  # 1.234.5678.9091  202.13.5.129
+    br = 'Chrome|Firefox|MSIE'
     result1 = count_patt(fname, ip)
     result2 = count_patt(fname, br)
     print(result1)
