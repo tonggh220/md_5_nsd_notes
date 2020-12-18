b2dc2926caac74f5110c4b359a1f136eb195fbbf
zhangzhg <zhangzg@tedu.cn>
2020-12-18 17:45:04 +0800

    nsd2007 py02 day05 18:00

--- nsd2007/devops/day01/get_web.py
+++ nsd2007/devops/day01/get_web.py
@@ -1,8 +1,19 @@
 import os
 import wget
+import re
 
-def get_patt(fname, patt):
+def get_patt(fname, patt, charset=None):
     "在文件中找到所有模式字符串，返回列表"
+    result = []
+    cpatt = re.compile(patt)
+
+    with open(fname, encoding=charset) as fobj:
+        for line in fobj:
+            m = cpatt.search(line)
+            if m != None:
+                result.append(m.group())
+
+    return result
 
 
 if __name__ == '__main__':
@@ -16,8 +27,8 @@ if __name__ == '__main__':
         wget.download(url163, fname163)
 
     # 取出网易首页文件中所有图片的url
-    img_patt = ''
-    img_list = get_patt(fname163, img_patt)
+    img_patt = '(http|https)://[\w./-]+\.(jpg|jpeg|png|gif)'
+    img_list = get_patt(fname163, img_patt, 'gbk')
 
     # 把img_list中的每个图片下载下来
     for img_url in img_list:
