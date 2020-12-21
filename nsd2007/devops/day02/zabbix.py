1c85b1f7fe27b82ba6d4994442413c96b88f640a
zhangzhg <zhangzg@tedu.cn>
2020-12-21 18:07:05 +0800

    nsd2007 devops day02 18:00

--- nsd2007/devops/day02/zabbix.py
+++ nsd2007/devops/day02/zabbix.py
@@ -13,13 +13,83 @@ headers = {'Content-Type': 'application/json; charset=UTF-8'}
 # }
 ###############################
 # 获取隐私信息，需要token。获取用户的token: e8521637d1fc44b65d5ceeebc02ba842
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "user.login",
+#     "params": {
+#         "user": "Admin",
+#         "password": "zabbix"
+#     },
+#     "id": 1
+# }
+###############################
+# 查询组，获取组id: 2
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "hostgroup.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "name": [
+#                 # "Zabbix servers",
+#                 "Linux servers"
+#             ]
+#         }
+#     },
+#     "auth": "e8521637d1fc44b65d5ceeebc02ba842",
+#     "id": 1
+# }
+###############################
+# 获取模板信息, id: 10001
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "template.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "host": [
+#                 "Template OS Linux",
+#                 # "Template OS Windows"
+#             ]
+#         }
+#     },
+#     "auth": "e8521637d1fc44b65d5ceeebc02ba842",
+#     "id": 1
+# }
+###############################
+# 新建名为web1的主机，在Linux servers组中，应用Template os linux模板
 data = {
     "jsonrpc": "2.0",
-    "method": "user.login",
+    "method": "host.create",
     "params": {
-        "user": "Admin",
-        "password": "zabbix"
+        "host": "web1",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 1,
+                "ip": "192.168.1.254",
+                "dns": "",
+                "port": "10050"
+            }
+        ],
+        "groups": [
+            {
+                "groupid": "2"
+            }
+        ],
+        "templates": [
+            {
+                "templateid": "10001"
+            }
+        ],
+        "inventory_mode": 0,
+        "inventory": {
+            "macaddress_a": "01234",
+            "macaddress_b": "56768"
+        }
     },
+    "auth": "e8521637d1fc44b65d5ceeebc02ba842",
     "id": 1
 }
 
