08d42a89352aff50e59aab244d6e1a8fbd9fe681
zhangzhg <zhangzg@tedu.cn>
2020-12-21 16:55:22 +0800

    nsd2007 devops day02 17:00

--- nsd2007/devops/day02/dingtalk.py
+++ nsd2007/devops/day02/dingtalk.py
@@ -3,17 +3,36 @@ import json
 
 url = ''
 headers = {'Content-Type': 'application/json; charset=UTF-8'}
+########################
+# data = {
+#     "msgtype": "text",
+#     "text": {
+#         "content": "我就是我, 是不一样的烟火@156xxxx8827好好学习天天向上"
+#     },
+#     "at": {
+#         "atMobiles": [   # @手机号
+#             # "156xxxx8827",
+#             # "189xxxx8325"
+#         ],
+#         "isAtAll": False   # @所有人
+#     }
+# }
+########################
 data = {
-    "msgtype": "text",
-    "text": {
-        "content": "我就是我, 是不一样的烟火@156xxxx8827好好学习天天向上"
+    "msgtype": "markdown",
+    "markdown": {
+        "title": "元旦放假通知",
+        "text": """## 元旦放假通知
+![](http://p6.itc.cn/q_70/images03/20201217/9effd2dd87934cfca874eb5e6d488fa4.jpeg)
+好好学习天天向上
+[TMOOC](http://tmooc.cn)
+"""
     },
     "at": {
-        "atMobiles": [   # @手机号
-            # "156xxxx8827",
-            # "189xxxx8325"
+        "atMobiles": [
+            # "150XXXXXXXX"
         ],
-        "isAtAll": False   # @所有人
+        "isAtAll": False
     }
 }
 
