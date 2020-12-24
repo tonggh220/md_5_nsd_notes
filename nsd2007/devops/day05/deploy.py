b8f758a767ab79574d7e8459604d82b6de377a5f
zhangzhg <zhangzg@tedu.cn>
2020-12-24 16:43:21 +0800

    nsd2007 devops day05 16:45

--- nsd2007/devops/day05/deploy.py
+++ nsd2007/devops/day05/deploy.py
@@ -1,3 +1,6 @@
+import wget
+import requests
+
 
 def has_new_ver(ver_url, ver_fname):
     "检查是否有新版本，有返回True，没有返回False"
@@ -14,6 +17,10 @@ if __name__ == '__main__':
         exit(1)
 
     # 下载新版本软件
+    download_dir = '/var/www/download'
+    r = requests.get(ver_url)
+    app_url = 'http://192.168.1.103/deploy/pkgs/myweb-%s.tar.gz.md5' % r.text
+    wget.download(app_url, download_dir)
 
     # 检查文件是否完好，如已损坏则删除并退出
 
