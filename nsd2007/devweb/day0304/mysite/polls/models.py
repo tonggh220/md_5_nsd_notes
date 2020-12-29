a6c9c631a55177b03251c1493d55776d26af2364
zhangzhg <zhangzg@tedu.cn>
2020-12-29 16:39:30 +0800

    nsd2007 devweb day03 16:45

--- nsd2007/devweb/day0304/mysite/polls/models.py
+++ nsd2007/devweb/day0304/mysite/polls/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    q = models.ForeignKey(Question, on_delete=models.CASCADE)
