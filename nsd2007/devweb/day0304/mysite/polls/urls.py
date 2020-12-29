08013bb08b775f94efd36e5647b437e4fa3fe11e
zhangzhg <zhangzg@tedu.cn>
2020-12-29 15:21:55 +0800

    nsd2007 devweb day03 15:25

--- nsd2007/devweb/day0304/mysite/polls/urls.py
+++ nsd2007/devweb/day0304/mysite/polls/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     # 如果poll/后面为空，则调用views.py中index函数
     # name='index'是给http://x.x.x.x./polls/这个url起的名
     path('', views.index, name='index'),
+    # path()函数中的路径，可以支持变量。声明一个名为qid、类型为int的变量
+    # qid指的是问题编号。除了int类型外，还可以有str和slug。slug基本不用
+    path('<int:qid>', views.detail, name='detail'),
 ]
