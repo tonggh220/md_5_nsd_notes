34dd9d89339af0df48580860c436e4ef6c3a55be
zhangzhg <zhangzg@tedu.cn>
2020-12-29 14:37:08 +0800

    nsd2007 devweb day03 14:40

--- nsd2007/devweb/day0304/mysite/polls/urls.py
+++ nsd2007/devweb/day0304/mysite/polls/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from polls import views
 
-urlpatterns = []
+urlpatterns = [
+    # 从http://x.x.x.x/polls/后面开始匹配
+    # 如果poll/后面为空，则调用views.py中index函数
+    # name='index'是给http://x.x.x.x./polls/这个url起的名
+    path('', views.index, name='index'),
+]
