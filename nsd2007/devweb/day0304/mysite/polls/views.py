c746f014b6f322e1bad7e06281dfd56dd3aba9b7
zhangzhg <zhangzg@tedu.cn>
2020-12-30 16:14:41 +0800

    nsd2007 devweb day04 16:10

--- nsd2007/devweb/day0304/mysite/polls/views.py
+++ nsd2007/devweb/day0304/mysite/polls/views.py
@@ -11,8 +11,11 @@ def index(request):
 
 def detail(request, qid):
     # qid用于接收来自于url的参数qid
+    # 取出指定的问题，将其发给前端网页
+    question = Question.objects.get(id=qid)
+    return render(request, 'detail.html', {'question': question})
     # {'qid': qid}将成为detail.html中的变量和值，即 qid=数字
-    return render(request, 'detail.html', {'qid': qid})
+    # return render(request, 'detail.html', {'qid': qid})
 
 def result(request, qid):
     return render(request, 'result.html', {'qid': qid})
