01c58e902f878d55d70bb1e6f7fda2432f2158d1
zhangzhg <zhangzg@tedu.cn>
2020-12-30 14:10:11 +0800

    nsd2007 devweb day04 14:15

--- nsd2007/devweb/day0304/mysite/polls/views.py
+++ nsd2007/devweb/day0304/mysite/polls/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from polls.models import Question
 
 # 用户发起的请求，将会成为一个请求对象，作为第一个参数传给函数
 # 因此，函数至少需要有一个参数进行接收
 def index(request):
+    # 取出所有的问题，按发布时间降序排列
+    questions = Question.objects.order_by('-pub_date')
     # 通过render函数找到一个网页模板文件，返回给客户端
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'questions': questions})
 
 def detail(request, qid):
     # qid用于接收来自于url的参数qid
