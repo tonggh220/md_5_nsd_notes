3aef161faa3d696d9f05c2659edfab1e40a306d4
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-11 17:46:29 +0800

    nsd2008 py01 day02 17:50

---
+++ nsd2008/py01/day02/for1.py
@@ -0,0 +1,15 @@
+# s1 = 'abc'
+# for ch in s1:
+#     print(ch)
+
+# l1 = [10, 20, 30]
+# for i in l1:
+#     print(i)
+
+# names = ('tom', 'jerry', 'jack', 'rose')
+# for name in names:
+#     print(name)
+
+d1 = {'name': 'nb', 'age': 20}
+for key in d1:
+    print(key, d1[key])
