6baf6ae0a4b0a03b98fcb64a0d511f333b3524fa
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-11 10:17:56 +0800

    nsd2008 py01 day02 10:15

--- nsd2008/py01/day02/day02.py
+++ nsd2008/py01/day02/day02.py
@@ -1,7 +1,22 @@
-if 3 > 0:
-    print('yes')
-    print('ok')
+# if 3 > 0:
+#     print('yes')
+#     print('ok')
+#
+# s1 = 'python'
+# if 'th' in s1:
+#     print('yeah')
 
-s1 = 'python'
-if 'th' in s1:
-    print('yeah')
+if 0.0:
+    print('任何值为0的数字都是假')
+
+if ' ':
+    print('空格字符也是一个字符，为真')
+
+if []:
+    print('空列表，为假')
+
+if (10, 20):
+    print('非空元组，为真')
+
+if {'name': 'nb'}:
+    print('非空字典，为真')
