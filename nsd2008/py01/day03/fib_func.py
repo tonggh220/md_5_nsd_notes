61eeea5a8f6e80de73ced9baa6cba219c357c181
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-12 16:33:23 +0800

    nsd2008 py01 day03 16:30

--- nsd2008/py01/day03/fib_func.py
+++ nsd2008/py01/day03/fib_func.py
@@ -1,11 +1,14 @@
-def mk_fib():
+def mk_fib(n):  # 函数需要处理的数据用参数传递
     fib = [0, 1]
-    n = int(input("长度: "))
 
     for i in range(n - 2):
         fib.append(fib[-1] + fib[-2])
 
-    print(fib)
+    return fib  # 返回fib变量代表的列表，不是把fib变量返回
 
-mk_fib()
-mk_fib()
+length = int(input("长度: "))
+result1 = mk_fib(length)
+print(result1)
+result2 = mk_fib(8)
+print(result2)
+
