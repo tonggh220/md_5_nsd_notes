aeb25008a72d19aec0f197f51b91cd06e4b3e676
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-13 15:26:44 +0800

    nsd2008 py01 day04 15:30

--- nsd2008/py01/day04/mkfile.py
+++ nsd2008/py01/day04/mkfile.py
@@ -14,6 +14,16 @@ def get_fname():
 
 def get_content():
     "用于获取用户输入的多行文本"
+    content = []
+
+    print("请输入内容，在单独的一行上输入end结束！")
+    while 1:
+        line = input("(end to quit)> ")
+        if line == 'end':
+            break
+        content.append(line)
+
+    return content
 
 def wfile(fname, content):
     "用于将内容content写入文件fname"
