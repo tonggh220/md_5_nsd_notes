a593f49677e5670e24336cee323e84ab3b4c3d87
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-13 15:41:37 +0800

    nsd2008 py01 day04 15:40

--- nsd2008/py01/day04/mkfile.py
+++ nsd2008/py01/day04/mkfile.py
@@ -21,17 +21,22 @@ def get_content():
         line = input("(end to quit)> ")
         if line == 'end':
             break
+        # content.append(line + '\n')  # 在行尾加上回车\n
         content.append(line)
 
     return content
 
 def wfile(fname, content):
     "用于将内容content写入文件fname"
+    with open(fname, 'w') as fobj:
+        fobj.writelines(content)
 
 if __name__ == '__main__':
     # 获取文件名
     fname = get_fname()
     # 获取内容
     content = get_content()
+    # 将列表中每个字符串结尾拼接\n后，再赋值回给content
+    content = [line + '\n' for line in content]
     # 将内容与到文件
     wfile(fname, content)
