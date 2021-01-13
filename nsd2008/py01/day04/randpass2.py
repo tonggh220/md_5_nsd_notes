0ae01cb6b5c608813eaa89779228012c72f84027
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-13 10:38:03 +0800

    nsd2008 py01 day04 10:40

--- nsd2008/py01/day04/randpass2.py
+++ nsd2008/py01/day04/randpass2.py
@@ -1,8 +1,10 @@
 from random import choice
+from string import ascii_letters, digits
+
+# 定义可用字符集
+all_chs = ascii_letters + digits
 
 def mk_pwd(n=8):
-    # 定义可用字符集
-    all_chs = '1234567890qwertyuiopasdfghjkzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'
     # 创建用于保存结果的变量
     result = ''
     # 在可用字符集中随机选一个字符，拼接到结果变量中，共运行n次
