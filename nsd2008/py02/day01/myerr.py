cd0dd3541b3e5dd8bb710ade33410b461ca0e8d2
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-15 14:48:59 +0800

    nsd2008 py02 day01 14:50

---
+++ nsd2008/py02/day01/myerr.py
@@ -0,0 +1,7 @@
+def get_info(name, age):
+    if not 0 < age < 120:
+        raise ValueError("年龄超出范围(1~119)")
+    print('%s is %d years old' % (name, age))
+
+if __name__ == '__main__':
+    get_info('nb', 20)
