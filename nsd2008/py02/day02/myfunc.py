99f214ac9e6336ec277d573afcbe036e08949a83
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-16 15:52:46 +0800

    nsd2008 py02 day02 15:50

--- nsd2008/py02/day02/myfunc.py
+++ nsd2008/py02/day02/myfunc.py
@@ -1,11 +1,24 @@
 from random import randint
 
+# def fn(x):
+#     # 对于整数n，除以2只能是1或0，1为真，0为假
+#     return x % 2
+#
+# if __name__ == '__main__':
+#     nums = [randint(1, 100) for i in range(10)]
+#     print(nums)
+#     result1 = filter(fn, nums)
+#     print(list(result1))
+#     result2 = filter(lambda x: x % 2, nums)
+#     print(list(result2))
+
 def fn(x):
-    # 对于整数n，除以2只能是1或0，1为真，0为假
-    return x % 2
+    return x + 100
 
 if __name__ == '__main__':
     nums = [randint(1, 100) for i in range(10)]
     print(nums)
-    result1 = filter(fn, nums)
+    result1 = map(fn, nums)
     print(list(result1))
+    result2 = map(lambda x: x + 100, nums)
+    print(list(result2))
