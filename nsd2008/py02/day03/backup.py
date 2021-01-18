e4107aca01e6695a77b83c29c6fd0a2f81ce4aa2
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-18 10:18:10 +0800

    nsd2008 py02 day03 10:20

--- nsd2008/py02/day03/backup.py
+++ nsd2008/py02/day03/backup.py
@@ -1,13 +1,21 @@
 from time import strftime
+import os
 
-def full_backup():
+def full_backup(src, dst, md5file):
     "用于完全备份"
 
-def incr_backup():
+def incr_backup(src, dst, md5file):
     "用于增量备份"
 
 if __name__ == '__main__':
-    if strftime("%a") == "Mon":
-        full_backup()
+    src = '/tmp/demo/security'
+    dst = '/tmp/demo/backup'
+    md5file = '/tmp/demo/backup/md5.data'
+    # 如果是周一，或者是第一次备份，都执行完全备份
+    if (strftime("%a") == "Mon") or (not os.path.exists(md5file)):
+        full_backup(src, dst, md5file)
     else:
-        incr_backup()
+        incr_backup(src, dst, md5file)
+
+# mkdir -p /tmp/demo/backup
+# cp -r /etc/security /tmp/demo/
