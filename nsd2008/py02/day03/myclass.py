10bd0d74058da57f36729175a896d6baf5302c58
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-18 14:12:51 +0800

    nsd2008 py02 day03 14:10

--- nsd2008/py02/day03/myclass.py
+++ nsd2008/py02/day03/myclass.py
@@ -1,5 +1,13 @@
 class Role:
-    pass
+    def __init__(self, name, mp, hp, weapon):
+        # 被称作构造器方法，实例化时自动调用。self不是关键字
+        # 用任意名称都可以，当创建时例时，用于将属性绑定到实例身上
+        self.name = name
+        self.mp = mp
+        self.hp = hp
+        self.weapon = weapon
 
 if __name__ == '__main__':
-    lb = Role()
+    # 自动调用__init__，创建具体的实例对象
+    lb = Role('吕布', 300, 500, '方天画戟')
+    print(lb.name, lb.weapon, lb.mp, lb.hp)
