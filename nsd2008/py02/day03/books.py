17bb6e38e83e8f3e249904f97181b0ef0546deb6
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-18 16:34:30 +0800

    nsd2008 py02 day03 16:40

--- nsd2008/py02/day03/books.py
+++ nsd2008/py02/day03/books.py
@@ -8,8 +8,13 @@ class Book:
         "打印、显示实例时，将展示这个方法返回的字符串"
         return '《%s》' % self.title
 
+    def __call__(self):
+        "实例像函数一样调用时，执行此方法中的代码"
+        print('《%s》是%s编著的' % (self.title, self.author))
+
 if __name__ == '__main__':
     linux = Book('Linux运维之道', '丁明一')  # 调用__init__
     print(linux)  # 调用__str__
+    linux()
 
 
