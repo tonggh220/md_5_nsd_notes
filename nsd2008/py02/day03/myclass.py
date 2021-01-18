9d40d4d93baa517d5486d40ba42107f13595f19d
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-18 14:33:15 +0800

    nsd2008 py02 day03 14:30

--- nsd2008/py02/day03/myclass.py
+++ nsd2008/py02/day03/myclass.py
@@ -11,8 +11,14 @@ class Role:
         # 绑定在实例身上的方法，在类中任意位置可见可用
         print("我是%s，擅用%s" % (self.name, self.weapon))
 
+    def speak(self, words):
+        # 没有绑在实例身上的变量，只是函数的局部变量，只能用在当前函数中
+        hh = '呵呵'
+        print(hh, words)
+
 if __name__ == '__main__':
     # 自动调用__init__，创建具体的实例对象
     lb = Role('吕布', 300, 500, '方天画戟')
     print(lb.name, lb.weapon, lb.mp, lb.hp)
     lb.show_me()
+    lb.speak('马中赤兔，人中吕布！')
