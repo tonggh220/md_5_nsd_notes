4894b1c9c1da78cc3938245c9520f2071c7ce4a7
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-18 14:22:35 +0800

    nsd2008 py02 day03 14:20

--- nsd2008/py02/day03/myclass.py
+++ nsd2008/py02/day03/myclass.py
@@ -7,7 +7,12 @@ class Role:
         self.hp = hp
         self.weapon = weapon
 
+    def show_me(self):
+        # 绑定在实例身上的方法，在类中任意位置可见可用
+        print("我是%s，擅用%s" % (self.name, self.weapon))
+
 if __name__ == '__main__':
     # 自动调用__init__，创建具体的实例对象
     lb = Role('吕布', 300, 500, '方天画戟')
     print(lb.name, lb.weapon, lb.mp, lb.hp)
+    lb.show_me()
