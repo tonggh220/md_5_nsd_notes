37b01997b22e3eed77b2258c0d5c23960fd6b228
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-19 15:35:44 +0800

    nsd2008 py02 day04 15:40

---
+++ nsd2008/py02/day04/dbconn.py
@@ -0,0 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# 创建连接到数据库的引擎
+engine = create_engine(
+    # mysql+pymysql://用户名:密码@服务器/数据库?字符串参数
+    'mysql+pymysql://root:tedu.cn@127.0.0.1/tedu2008?charset=utf8',
+    encoding='utf8',
+    echo=True  # 在屏幕上打印调试信息，生产环境不要打开
+)
+Base = declarative_base()  # 生成映射类的基类
+Session = sessionmaker(bind=engine)  # 创建到数据库的会话类
+
+
