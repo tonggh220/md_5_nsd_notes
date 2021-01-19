31a38f7bcc775f5b4b77d8ac2d0dfba16c82df26
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-19 11:51:15 +0800

    nsd2008 py02 day04 10:51

---
+++ nsd2008/py02/day04/py_mysql.py
@@ -0,0 +1,27 @@
+import pymysql
+
+# 创建到数据库的连接
+conn = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='tedu.cn',
+    db='nsd2008',
+    charset='utf8mb4',
+)
+
+# 创建游标。游标像文件对象一样，通过文件对象对文件读写；
+# 通过游标对数据库进行增删改查
+cursor = conn.cursor()
+
+# 编写sql语句
+create_dep = ""
+
+# 通过游标执行sql语句
+cursor.execute(create_dep)
+
+# 确认
+conn.commit()
+
+# 关闭
+cursor.close()
+conn.close()
