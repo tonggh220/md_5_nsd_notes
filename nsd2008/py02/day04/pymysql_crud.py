66e534bbd29ed7f0f6ae19c2235488a02cc53681
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-19 14:09:24 +0800

    nsd2008 py02 day04 14:10

---
+++ nsd2008/py02/day04/pymysql_crud.py
@@ -0,0 +1,37 @@
+# 增加(Create)、检索(Retrieve)、更新(Update)和删除(Delete)
+
+import pymysql
+
+# 创建到数据库的连接
+conn = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='tedu.cn',
+    db='nsd2008',
+    charset='utf8mb4',
+)
+
+# 创建游标。游标像文件对象一样，通过文件对象对文件读写；
+# 通过游标对数据库进行增删改查
+cursor = conn.cursor()
+
+##############################
+# 插入语句
+insert1 = "INSERT INTO departments VALUES (%s, %s)"
+# 插入一行数据
+cursor.execute(insert1, (1, '人事部'))
+# 插入多行数据
+cursor.executemany(
+    insert1,
+    [(2, '运维部'), (3, '开发部'), (4, '测试部'), (5, '财务部')]
+)
+
+
+
+##############################
+# 确认
+conn.commit()
+
+# 关闭
+cursor.close()
+conn.close()
