c86e1ffeca32e540302cd151fdec3f3da7d6e333
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-20 11:40:54 +0800

    nsd2008 devops day01 11:40

--- nsd2008/devops/day01/zb2.py
+++ nsd2008/devops/day01/zb2.py
@@ -4,9 +4,7 @@ import time
 ret_val = os.fork()
 if ret_val:
     print('in parent')
-    result = os.waitpid(-1, 0)  # 挂起父进程，直到处理完僵尸进程
-    print(result)
-    time.sleep(15)
+    time.sleep(60)
 else:
     print('in child')
     time.sleep(15)
