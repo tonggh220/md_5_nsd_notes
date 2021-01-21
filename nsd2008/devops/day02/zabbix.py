32e3f3601ece3eef32a3320714fab84e0af5b04a
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-21 17:53:16 +0800

    nsd2008 devops day02 17:55

--- nsd2008/devops/day02/zabbix.py
+++ nsd2008/devops/day02/zabbix.py
@@ -24,16 +24,68 @@ headers = {'Content-Type': 'application/json; charset=UTF-8'}
 # }
 ################################
 # 获取Linux servers组的ID: 2
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "hostgroup.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "name": [
+#                 # "Zabbix servers",
+#                 "Linux servers"
+#             ]
+#         }
+#     },
+#     "auth": "b5479950bf43060f0035d68845b71d8f",
+#     "id": 1
+# }
+################################
+# 获取template os linux模板的信息, id: 10001
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "template.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "host": [
+#                 "Template OS Linux",
+#                 # "Template OS Windows"
+#             ]
+#         }
+#     },
+#     "auth": "b5479950bf43060f0035d68845b71d8f",
+#     "id": 1
+# }
+################################
 data = {
     "jsonrpc": "2.0",
-    "method": "hostgroup.get",
+    "method": "host.create",
     "params": {
-        "output": "extend",
-        "filter": {
-            "name": [
-                # "Zabbix servers",
-                "Linux servers"
-            ]
+        "host": "web1",
+        "interfaces": [
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 1,
+                "ip": "192.168.1.10",
+                "dns": "",
+                "port": "10050"
+            }
+        ],
+        "groups": [
+            {
+                "groupid": "2"
+            }
+        ],
+        "templates": [
+            {
+                "templateid": "10001"
+            }
+        ],
+        "inventory_mode": 0,
+        "inventory": {
+            "macaddress_a": "01234",
+            "macaddress_b": "56768"
         }
     },
     "auth": "b5479950bf43060f0035d68845b71d8f",
