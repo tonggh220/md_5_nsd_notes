fb92f98d6ef86242827cffc1008ce215ff329376
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-25 16:42:29 +0800

    nsd2008 devops day05 16:４0

--- nsd2008/devops/day05/deploy.py
+++ nsd2008/devops/day05/deploy.py
@@ -1,7 +1,15 @@
 
+def has_new_ver(ver_url, ver_fname):
+    "用于判断是否有新版本，有返回True，否则返回False"
+
 
 if __name__ == '__main__':
     # 检查是否有新版本
+    ver_url = 'http://192.168.1.103/deploy/live_ver'
+    ver_fname = '/var/www/deploy/live_ver'
+    if not has_new_ver(ver_url, ver_fname):
+        print("未发现新版本")
+        exit(1)
 
     # 下载最新版本的代码
 
