0df5a3a1e54b4c6906c28d866efea31b8968fd22
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-28 14:21:34 +0800

    nsd2008 devweb day03 14:20

--- nsd2008/devweb/day0304/mysite/polls/urls.py
+++ nsd2008/devweb/day0304/mysite/polls/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from polls import views
 
-urlpatterns = []
+urlpatterns = [
+    # 从http://127.0.0.1/polls/后面开始匹配
+    # 如果polls/后面为空，则调用函数views.index进行处理
+    # name='index'是给url http://127.0.0.1/polls/起的名字
+    path('', views.index, name="index"),
+]
--- nsd2008/devweb/day0304/mysite/polls/views.py
+++ nsd2008/devweb/day0304/mysite/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# 用户发起请求，请求将为成为一个对象，作为第一个参数传给函数
+# 因此，函数至少有一个参数，名字自定义
+def index(request):
+    # 通过render函数找到名为index.html的网页模板文件，返回给用户
+    return render(request, 'index.html')
+
