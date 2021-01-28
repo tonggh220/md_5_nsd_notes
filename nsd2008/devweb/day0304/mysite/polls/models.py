9755a7f0fdf34d4a82e277f1bee58781a4ebebdb
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-28 17:27:51 +0800

    nsd2008 devweb day03 17:30

--- nsd2008/devweb/day0304/mysite/polls/models.py
+++ nsd2008/devweb/day0304/mysite/polls/models.py
@@ -4,6 +4,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
 
+    def __str__(self):
+        return "问题:%s" % self.question_text
+
 class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
