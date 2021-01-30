58c639f7892ba92aac92ccf86fa12ca3f4687783
Mrzhangzhg <zhangzg@tedu.cn>
2021-01-30 15:38:57 +0800

    nsd2008 devweb day05 15:40

--- nsd2008/devweb/ansible_project/myansible/ansi_cfg/dhosts.py
+++ nsd2008/devweb/ansible_project/myansible/ansi_cfg/dhosts.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -8,3 +9,21 @@ engine = create_engine(
 )
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
+
+class HostGroup(Base):
+    __tablename__ = 'webadmin_hostgroup'
+    id = Column(Integer, primary_key=True)
+    groupname = Column(String(50), unique=True)
+
+class Host(Base):
+    __tablename__ = 'webadmin_host'
+    id = Column(Integer, primary_key=True)
+    hostname = Column(String(50))
+    ip_addr = Column(String(15))
+    group_id = Column(Integer, ForeignKey('webadmin_hostgroup.id'))
+
+if __name__ == '__main__':
+    session = Session()
+    qset = session.query(HostGroup.groupname, Host.ip_addr).join(Host)
+    print(qset.all())
+
