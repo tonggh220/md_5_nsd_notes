4e914f463b007dc1bf8be5f43b1ac3b89d67e758
Mrzhangzhg <zhangzg@tedu.cn>
2021-02-01 09:57:55 +0800

    nsd2008 devweb day06 9:50

--- nsd2008/devweb/ansible_project/myansible/webadmin/views.py
+++ nsd2008/devweb/ansible_project/myansible/webadmin/views.py
@@ -4,8 +4,17 @@ from webadmin.models import HostGroup
 def index(request):
     return render(request, 'webadmin/index.html')
 
+# 修改视图函数，判断如果request.method是POST，则取出表单提交的数据。并在数据库中添加相应的条目
+# 函数的request参数有一个名为method的属性，记录了http的方法是什么
 def add_hosts(request):
     if request.method == 'POST':
-        pass
+        group = request.POST.get('group').strip()  # 去除字符串两边额外的空格
+        host = request.POST.get('host').strip()
+        ip = request.POST.get('ip').strip()
+        if group:  # 如果group非空
+            # 创建或取出组实例
+            g = HostGroup.objects.get_or_create(groupname=group)[0]
+            if host and ip:  # 如果host和ip也是非空的
+                g.host_set.get_or_create(hostname=host, ip_addr=ip)
     groups = HostGroup.objects.all()
     return render(request, 'webadmin/add_hosts.html', {'groups': groups})
