7cc56ccfaef1347a3261447413bf9e0e502efac8
root <root@localhost.localdomain>
2021-02-04 10:19:34 +0800

    nsd2009 py01 day02 10:15

--- nsd2009/py01/day02/day02.py
+++ nsd2009/py01/day02/day02.py
@@ -2,8 +2,29 @@
 #     print('yes')
 #     print('ok')
 
-if 3 > 10:
-    print('111')
-else:
-    print('222')
+# if 3 > 10:
+#     print('111')
+# else:
+#     print('222')
+
+if 0.0:
+    print('任何值为0的数字都是假')
+
+if 'abc':
+    print('非空字符串为真')
+
+if ' ':
+    print('空格字符也是一个字符，为真')
+
+if '':
+    print('空字符串为假')
+
+if []:
+    print('空列表为假')
+
+if (1, 2):
+    print('非空元组为真')
+
+if {}:
+    print('空字典，为假')
 
