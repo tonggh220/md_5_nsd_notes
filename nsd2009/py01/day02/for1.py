a6ecbf838c91a6656838a44c906d6ce0eb9cf5a9
root <root@localhost.localdomain>
2021-02-04 17:40:11 +0800

    nsd2009 py01 day02 17:45

---
+++ nsd2009/py01/day02/for1.py
@@ -0,0 +1,15 @@
+# s1 = 'abc'
+# for ch in s1:
+#     print(ch)
+
+# users = ['bob', 'alice', 'jack', 'rose']
+# for name in users:
+#     print(name)
+
+# t1 = (10, 20, 3)
+# for i in t1:
+#     print(i)
+
+d1 = {'name': 'nb', 'age': 20}
+for key in d1:
+    print(key, d1[key])
