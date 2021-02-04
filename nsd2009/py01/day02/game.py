8210500ed4d92f3b59e132cf92bd1dd9ae3dab18
root <root@localhost.localdomain>
2021-02-04 14:19:06 +0800

    nsd2009 py01 day02 14:20

---
+++ nsd2009/py01/day02/game.py
@@ -0,0 +1,33 @@
+import random
+
+# 定义计算机可用的选择
+all_choice = ['石头', '剪刀', '布']
+# 计算机随机出拳
+computer = random.choice(all_choice)
+# 人出拳
+player = input("请出拳(石头/剪刀/布): ")
+# 输出人机选择
+print("Your choice: %s, Computer's choice: %s" % (player, computer))
+
+# 判断胜负
+if player == '石头':
+    if computer == '石头':
+        print('平局')
+    elif computer == '剪刀':
+        print('You WIN!!!')
+    else:
+        print('You LOSE!!!')
+elif player == '剪刀':
+    if computer == '石头':
+        print('You LOSE!!!')
+    elif computer == '剪刀':
+        print('平局')
+    else:
+        print('You WIN!!!')
+else:
+    if computer == '石头':
+        print('You WIN!!!')
+    elif computer == '剪刀':
+        print('You LOSE!!!')
+    else:
+        print('平局')
