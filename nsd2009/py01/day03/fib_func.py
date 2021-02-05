5abdce85ea0b02680e2d7eeb62ab75e6811d3b1b
root <root@localhost.localdomain>
2021-02-05 15:52:30 +0800

    nsd2009 py01 day03 15:55

--- nsd2009/py01/day03/fib_func.py
+++ nsd2009/py01/day03/fib_func.py
@@ -1,11 +1,14 @@
-def mk_fib():
+def mk_fib(n):
     fib = [0, 1]
 
-    n = int(input("长度: "))
     for i in range(n - 2):
         fib.append(fib[-1] + fib[-2])
 
     return fib  # 返回fib代表的列表
 
-a = mk_fib()  # 调用mk_fib函数后，得到的值赋值给变量a
+a = mk_fib(10)  # 调用mk_fib函数后，得到的值赋值给变量a
 print(a)
+
+l = int(input("长度: "))
+b = mk_fib(l)
+print(b)
