44b62215a61765f007467c5e91b77a459c76c19e
root <root@localhost.localdomain>
2021-02-07 14:44:18 +0800

    nsd2009 py01 day04 14:42

--- nsd2009/py01/day04/day03.py
+++ nsd2009/py01/day04/day03.py
@@ -1,4 +1,4 @@
-users = ['tom', 'jerry', 'jack', 'rose']
+# users = ['tom', 'jerry', 'jack', 'rose']
 
 # for i in [0, 1, 2, 3]:
 #     print(i, users[i])
@@ -6,5 +6,13 @@ users = ['tom', 'jerry', 'jack', 'rose']
 # for i in range(4):
 #     print(i, users[i])
 
-for i in range(len(users)):
-    print(i, users[i])
+# for i in range(len(users)):
+#     print(i, users[i])
+
+users = ['tom', 'jerry', 'jack', 'rose']
+# for data in enumerate(users):
+#     print(data)
+
+for i, user in enumerate(users):
+    print(i, user)
+
