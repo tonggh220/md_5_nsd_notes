e3057282519cbe731a27df722837d6451ca96e1c
root <root@localhost.localdomain>
2021-02-19 11:59:27 +0800

    nsd2009 py02 day01 11:59

--- nsd2009/py02/day01/mydiv.py
+++ nsd2009/py02/day01/mydiv.py
@@ -1,4 +1,7 @@
-num = int(input("number: "))
-result = 100 / num
-print(result)
-print('Done')
+try:
+    num = int(input("number: "))
+    result = 100 / num
+    print(result)
+    print('Done')
+except ValueError:
+    print('请输入非0数字')
