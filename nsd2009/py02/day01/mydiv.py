2fed17f35209918e750fcdb77bac3a016aa04e99
root <root@localhost.localdomain>
2021-02-19 14:31:25 +0800

    nsd2009 py02 day01 14:30

--- nsd2009/py02/day01/mydiv.py
+++ nsd2009/py02/day01/mydiv.py
@@ -22,6 +22,31 @@
 # except (KeyboardInterrupt, EOFError):
 #     print('\nBye-bye')
 #################################
+# try:
+#     num = int(input("number: "))
+#     result = 100 / num
+# except (ValueError, ZeroDivisionError):
+#     print('请输入非0数字')
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+# else:
+#     print(result)
+#
+# print('Done')
+#################################
+# try:
+#     num = int(input("number: "))
+#     result = 100 / num
+# except (ValueError, ZeroDivisionError):
+#     print('请输入非0数字')
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+#     exit(1)  # 程序遇到exit，将会结束，退出码$?指定为1
+# else:
+#     print(result)
+#
+# print('Done')
+#################################
 try:
     num = int(input("number: "))
     result = 100 / num
@@ -29,8 +54,8 @@ except (ValueError, ZeroDivisionError):
     print('请输入非0数字')
 except (KeyboardInterrupt, EOFError):
     print('\nBye-bye')
+    exit(1)  # 程序遇到exit，将会结束，退出码$?指定为1
 else:
     print(result)
-
-print('Done')
-#################################
+finally:  # 不管异常是否发生，都要执行的语句
+    print('Done')
