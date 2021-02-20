c5a737b0e019788ba408c0530d2957f232d179ae
root <root@localhost.localdomain>
2021-02-20 09:21:00 +0800

    nsd2009 py02 day02 9:20

---
+++ nsd2009/py02/day02/account.py
@@ -0,0 +1,35 @@
+def save():
+    "用于记录收入"
+    print('save')
+
+def cost():
+    "用于记录支出"
+    print('cost')
+
+def query():
+    "用于查询收支"
+    print('query')
+
+def show_menu():
+    "程序主体代码逻辑"
+    funcs = {'0': save, '1': cost, '2': query}
+    prompt = """(0) 收入
+(1) 支出
+(2) 查询
+(3) 退出
+请选择(0/1/2/3): """
+
+    while 1:
+        choice = input(prompt).strip()
+        if choice not in ['0', '1', '2', '3']:
+            print("无效的输入，请重试。")
+            continue
+
+        if choice == '3':
+            print('\nBye-bye')
+            break
+
+        funcs[choice]()
+
+if __name__ == '__main__':
+    show_menu()
