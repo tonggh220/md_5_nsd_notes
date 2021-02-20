6628c6fff110603e9563a93ce4166f0ae066bdb6
root <root@localhost.localdomain>
2021-02-20 15:45:39 +0800

    nsd2009 py02 day02 15:45

--- nsd2009/py02/day02/day02.py
+++ nsd2009/py02/day02/day02.py
@@ -1,9 +1,30 @@
-def func1():
-    print('in func1')
-    func2()
+# def func1():
+#     print('in func1')
+#     func2()
+#
+# def func2():
+#     print('in func2')
+#
+# if __name__ == '__main__':
+#     func1()
 
-def func2():
-    print('in func2')
+from random import randint
+
+def func1(x):
+    # 整数求余，返回值只有1或0。1为真，0为假
+    return x % 2
 
 if __name__ == '__main__':
-    func1()
+    nums = [randint(1, 100) for i in range(10)]
+    result1 = filter(func1, nums)
+    result2 = filter(lambda x: x % 2, nums)
+    print(nums)
+    print(list(result1))
+    print(list(result2))
+
+
+
+
+
+
+
