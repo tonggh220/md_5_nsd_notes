1a8ddf9bc6eb583ce971c0857b0823064b9d1d99
root <root@localhost.localdomain>
2021-02-22 14:42:02 +0800

    nsd2009 py02 day03 14:40

--- nsd2009/py02/day03/myclass.py
+++ nsd2009/py02/day03/myclass.py
@@ -13,8 +13,15 @@ class Role:
         # 绑定在实例身上的属性，在类中任意位置可见可用
         print('我是%s，擅用%s' % (self.name, self.weapon))
 
+    def speak(self, words):
+        # 没有绑在实例身上的变量，只是函数的局部变量，只能用在当前函数中
+        hh = '呵呵'
+        print(hh)
+        print(words)
+
 if __name__ == '__main__':
     # 实例本身将会自动作为第一个参数传递，本例中是lb
     lb = Role('吕布', '方天画戟')  # 自动调用__init__，创建具体的实例对象
     print(lb.name, lb.weapon, lb.hp, lb.mp)
     lb.show_me()
+    lb.speak('马中赤兔，人中吕布')
