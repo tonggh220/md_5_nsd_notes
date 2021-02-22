97ae2d2054c03076b357d03ed1e74bf9591c3342
root <root@localhost.localdomain>
2021-02-22 15:26:09 +0800

    nsd2009 py02 day03 15:30

--- nsd2009/py02/day03/myclass2.py
+++ nsd2009/py02/day03/myclass2.py
@@ -1,3 +1,10 @@
+class Role:
+    def __init__(self, name, weapon, hp=500, mp=300):
+        self.name = name
+        self.weapon = weapon
+        self.hp = hp
+        self.mp = mp
+
 class Weapon:
     def __init__(self, wname, strength, type):
         self.wname = wname
@@ -6,4 +13,6 @@ class Weapon:
 
 if __name__ == '__main__':
     ji = Weapon('方天画戟', 100, '物理')
-    print(ji.wname, ji.strength, type)
+    print(ji.wname, ji.strength, ji.type)
+    lb = Role('吕布', ji)
+    print(lb.weapon.wname, lb.weapon.strength, lb.weapon.type)
