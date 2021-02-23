a5ef675931d8462b8fa8dcb9a5607a01e4309bd0
root <root@localhost.localdomain>
2021-02-23 10:23:51 +0800

    nsd2009 py02 day04 10:20

--- nsd2009/py02/day04/count_patt.py
+++ nsd2009/py02/day04/count_patt.py
@@ -1,5 +1,18 @@
+import re
 
 def count_patt(fname, patt):
+    # 为了更好的执行效率，编译正则模式字符串
+    cpatt = re.compile(patt)
+    # 定义保存结果的变量
+    result = {}
+    # 从文件中找到匹配项，并保存
+    with open(fname) as fobj:
+        for line in fobj:
+            m = cpatt.search(line)
+            if m:  # 没有匹配到返回None，为假；匹配到返回匹配对象，为真
+                k = m.group()
+                result[k] = result.get(k, 0) + 1
+    return result
 
 if __name__ == '__main__':
     fname = 'access_log'
