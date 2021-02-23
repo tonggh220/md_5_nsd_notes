ccb5ce65cea4c998bdd7a5ba3164d267ee71cad4
root <root@localhost.localdomain>
2021-02-23 09:55:38 +0800

    nsd2009 py02 day04 9:55

--- nsd2009/py02/day04/count_patt.py
+++ nsd2009/py02/day04/count_patt.py
@@ -3,8 +3,8 @@ def count_patt(fname, patt):
 
 if __name__ == '__main__':
     fname = 'access_log'
-    ip = ''
-    br = ''
+    ip = '^(\d+\.){3}\d+'  # 12345.0.2345.29  192.168.1.25
+    br = 'Chrome|Firefox|MSIE'
     result1 = count_patt(fname, ip)
     result2 = count_patt(fname, br)
     print(result1)
