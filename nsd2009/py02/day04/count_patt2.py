eedffbd69c7dd3644161ec6efe1334c4bcee5452
root <root@localhost.localdomain>
2021-02-23 10:38:13 +0800

    nsd2009 py02 day04 10:40

---
+++ nsd2009/py02/day04/count_patt2.py
@@ -0,0 +1,26 @@
+import re
+
+class CountPatt:
+    def count_patt(self, fname, patt):
+        # 为了更好的执行效率，编译正则模式字符串
+        cpatt = re.compile(patt)
+        # 定义保存结果的变量
+        result = {}
+        # 从文件中找到匹配项，并保存
+        with open(fname) as fobj:
+            for line in fobj:
+                m = cpatt.search(line)
+                if m:  # 没有匹配到返回None，为假；匹配到返回匹配对象，为真
+                    k = m.group()
+                    result[k] = result.get(k, 0) + 1
+        return result
+
+if __name__ == '__main__':
+    fname = 'access_log'
+    ip = '^(\d+\.){3}\d+'  # 12345.0.2345.29  192.168.1.25
+    br = 'Chrome|Firefox|MSIE'
+    cp = CountPatt()
+    result1 = cp.count_patt(fname, ip)
+    result2 = cp.count_patt(fname, br)
+    print(result1)
+    print(result2)
