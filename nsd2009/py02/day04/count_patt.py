19dd279910b4cb52c1f83dd9e5bfe1e2f269118a
root <root@localhost.localdomain>
2021-02-23 09:43:44 +0800

    nsd2009 py02 day04 9:50

---
+++ nsd2009/py02/day04/count_patt.py
@@ -0,0 +1,11 @@
+
+def count_patt(fname, patt):
+
+if __name__ == '__main__':
+    fname = 'access_log'
+    ip = ''
+    br = ''
+    result1 = count_patt(fname, ip)
+    result2 = count_patt(fname, br)
+    print(result1)
+    print(result2)
