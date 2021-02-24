dd7629f4adc72389d07f3a9550cfdc73350dc92f
root <root@localhost.localdomain>
2021-02-24 10:57:24 +0800

    nsd2009 devops day01 11:00

--- nsd2009/devops/day01/myfork.py
+++ nsd2009/devops/day01/myfork.py
@@ -4,11 +4,25 @@ import os
 # os.fork()
 # print('hello world!')
 ################
+# print('starting...')
+# rc = os.fork()
+#
+# if rc:
+#     print('in parent')
+# else:
+#     print('in child')
+#
+# print('hello world!')
+################
+
 print('starting...')
-rc = os.fork()
-if rc:
-    print('in parent')
-else:
-    print('in child')
-print('hello world!')
+for i in range(3):
+    rc = os.fork()
+    if not rc:
+        print('Hello World!')
+        exit()
+
+
+
+
 
