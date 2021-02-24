793fe20a3b838e4e3292974f9a7c3202bf000dc9
root <root@localhost.localdomain>
2021-02-24 14:16:47 +0800

    nsd2009 devops day01 14:20

---
+++ nsd2009/devops/day01/mthello.py
@@ -0,0 +1,12 @@
+import time
+import threading
+
+def hello():
+    time.sleep(3)
+    print('Hello World!')
+
+if __name__ == '__main__':
+    for i in range(3):
+        # hello()
+        t = threading.Thread(target=hello)  # 创建工作线程
+        t.start()  # 启动线程，将会调用target()
