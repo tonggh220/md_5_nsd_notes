2a1e32881fcd7faa657dd4c9f9f5ff87ee14fa68
root <root@localhost.localdomain>
2021-02-24 17:19:38 +0800

    nsd2009 devops day01 17:15

--- nsd2009/devops/day01/get_web.py
+++ nsd2009/devops/day01/get_web.py
@@ -1,8 +1,18 @@
 import os
 import wget
+import re
 
-def get_patt(fname, patt):
+def get_patt(fname, patt, charset=None):
+    cpatt = re.compile(patt)
+    result = []
 
+    with open(fname, encoding=charset) as fobj:
+        for line in fobj:
+            m = cpatt.search(line)
+            if m:
+                result.append(m.group())
+
+    return result
 
 if __name__ == '__main__':
     # 下载网易首页
@@ -15,8 +25,8 @@ if __name__ == '__main__':
         wget.download(url163, fname163)
 
     # 在网易首页中找到图片地址
-    img_patt = ''
-    img_list = get_patt(fname163, img_patt)
+    img_patt = '(http|https)://[\w./-]+\.(jpg|jpeg|gif|png)'
+    img_list = get_patt(fname163, img_patt, 'gbk')
 
     # 下载图片
     for img_url in img_list:
