6b30cf0c6480934f6ef73f21fce88bd1c401fd3c
root <root@localhost.localdomain>
2021-02-25 16:52:24 +0800

    nsd2009 devops day02 16:50

--- nsd2009/devops/day02/weather.py
+++ nsd2009/devops/day02/weather.py
@@ -1,10 +1,11 @@
 import requests
+import pprint
 
 url = 'http://jisutianqi.market.alicloudapi.com/weather/query'
 headers = {
     'Content-Type': 'application/json; charset=UTF-8',
-    'Authorization': 'APPCODE 你管理后台中查询到的appcode'
+    'Authorization': 'APPCODE 369deb7e22cc4808b2a5a5ba3effbb7e'
 }
 params = {'citycode': '101010100'}
 r = requests.get(url, headers=headers, params=params)
-print(r.json())
+pprint.pprint(r.json())
