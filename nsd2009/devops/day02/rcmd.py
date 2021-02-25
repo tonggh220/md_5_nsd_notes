1b19ba1390827c1fe0957872952170c34a1fe857
root <root@localhost.localdomain>
2021-02-25 09:50:14 +0800

    nsd2009 devops day02 9:50

--- nsd2009/devops/day02/rcmd.py
+++ nsd2009/devops/day02/rcmd.py
@@ -1,6 +1,8 @@
 import paramiko
 import sys
+import os
 import getpass
+import threading
 
 def rcmd(host, port=22, user='root', passwd=None, cmd=None):
     ssh = paramiko.SSHClient()
@@ -18,11 +20,21 @@ def rcmd(host, port=22, user='root', passwd=None, cmd=None):
 
 if __name__ == '__main__':
     # python3 rcmd.py servers.txt id root
+    if len(sys.argv) < 3:
+        print('Usage: %s ipfile commands' % sys.argv[0])
+        exit(1)
+
+    if not os.path.isfile(sys.argv[1]):
+        print('No such file:', sys.argv[1])
+        exit(2)
+
     ipfile = sys.argv[1]
     cmds = ' '.join(sys.argv[2:])  # 将命令拼接成字符串
     passwd = getpass.getpass()
     with open(ipfile) as fobj:
         for line in fobj:
             ip = line.strip()  # 去除行尾的\n
-            rcmd(ip, 22, 'root', passwd, cmds)
+            t = threading.Thread(target=rcmd, args=(ip, 22, 'root', passwd, cmds))
+            t.start()
+            # rcmd(ip, 22, 'root', passwd, cmds)
     # rcmd('127.0.0.1', 22, 'root', 'redhat', 'id root; id zhangsan')
