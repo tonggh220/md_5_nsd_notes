4d3427adb113110293c06ea78abea5882ddbe6c9
root <root@localhost.localdomain>
2021-03-05 15:55:51 +0800

    nsd2009 devweb day03 15:50

--- nsd2009/devweb/day0304/mysite/polls/urls.py
+++ nsd2009/devweb/day0304/mysite/polls/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     # polls/后面空的，则调用相关函数views.index
     # name='index'是给http://server_ip/polls/起的名字
     path('', views.index, name='index'),
-    path('1', views.detail, name='detail'),
+    # path()函数中的路径，可以支持变量。声明一个名为qid，类型为int的变量
+    # 类型除了int外，还有str和slug
+    path('<int:qid>', views.detail, name='detail'),
 ]
--- nsd2009/devweb/day0304/mysite/polls/views.py
+++ nsd2009/devweb/day0304/mysite/polls/views.py
@@ -6,6 +6,8 @@ def index(request):
     # index函数通过render函数找到一个网页模板文件，返回给客户端
     return render(request, 'index.html')
 
-def detail(request):
-    return render(request, 'detail.html')
+# qid用于接收来自于url的参数
+def detail(request, qid):
+    # {'qid': qid}将成为detail.html的变量和值，即 qid=数字
+    return render(request, 'detail.html', {'qid': qid})
 
