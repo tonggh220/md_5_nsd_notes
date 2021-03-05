35c4f798b1fa6e734f80dda2e36f4ad26b85d4d7
root <root@localhost.localdomain>
2021-03-05 15:16:56 +0800

    nsd2009 devweb day03 15:20

--- nsd2009/devweb/day0304/mysite/polls/urls.py
+++ nsd2009/devweb/day0304/mysite/polls/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from polls import views
 
-urlpatterns = []
+urlpatterns = [
+    # 从http://server_ip/polls/后面进行匹配
+    # polls/后面空的，则调用相关函数views.index
+    # name='index'是给http://server_ip/polls/起的名字
+    path('', views.index, name='index'),
+]
