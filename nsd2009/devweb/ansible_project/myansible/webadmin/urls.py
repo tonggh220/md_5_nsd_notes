8640e520bd942da29aa39a50b29f8511f0fbef93
root <root@localhost.localdomain>
2021-03-09 16:38:46 +0800

    nsd2009 devweb day05 16:40

--- nsd2009/devweb/ansible_project/myansible/webadmin/urls.py
+++ nsd2009/devweb/ansible_project/myansible/webadmin/urls.py
@@ -3,5 +3,6 @@ from webadmin import views
 
 urlpatterns = [
     path('', views.index, name='webadmin_index'),
+    path('add_hosts', views.add_hosts, name='add_hosts'),
 ]
 
--- nsd2009/devweb/ansible_project/myansible/webadmin/views.py
+++ nsd2009/devweb/ansible_project/myansible/webadmin/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'webadmin/index.html')
+
+def add_hosts(request):
+    return render(request, 'webadmin/add_hosts.html')
