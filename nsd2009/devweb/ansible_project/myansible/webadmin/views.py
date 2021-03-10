6367cd5cd1bd1282815a53d0df29a44fe704bacc
root <root@localhost.localdomain>
2021-03-10 10:06:30 +0800

    nsd2009 devweb day06 10:10

--- nsd2009/devweb/ansible_project/myansible/webadmin/views.py
+++ nsd2009/devweb/ansible_project/myansible/webadmin/views.py
@@ -9,5 +9,13 @@ def index(request):
 def add_hosts(request):
     if request.method == 'POST':
         group = request.POST.get('group').strip()  # 去除字符串两边额外的空格
+        host = request.POST.get('host').strip()
+        ip = request.POST.get('ip').strip()
+        if group:  # 如果group非空
+            # 创建或取出组实例
+            g = HostGroup.objects.get_or_create(groupname=group)[0]
+            if host and ip:  # 如果host和ip也是非空的
+                g.host_set.get_or_create(hostname=host, ip_addr=ip)
+
     groups = HostGroup.objects.all()
     return render(request, 'webadmin/add_hosts.html', {'groups': groups})
