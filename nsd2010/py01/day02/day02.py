5b8681c8f6e866c6f1baa8d0ca4e7fb91ebed8b7
root <root@localhost.localdomain>
2021-03-15 10:29:39 +0800

    nsd2010 py01 day02 10:30

--- nsd2010/py01/day02/day02.py
+++ nsd2010/py01/day02/day02.py
@@ -1,3 +1,15 @@
-if 3 > 0:
-    print('yes')
-    print('ok')
+# if 3 > 0:
+#     print('yes')
+#     print('ok')
+
+if 0:
+    print('任何值为0的数字都是假')
+
+if ' ':
+    print('空格字符也是一个字符，为真')
+
+if []:
+    print('任何空对象都是假')
+
+if not None:
+    print('None为假，取反为真')
