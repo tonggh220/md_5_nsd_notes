3099411e9722e7de8ae0768f564a1851ff1e7fdc
root <root@localhost.localdomain>
2021-03-16 09:41:22 +0800

    nsd2010 py01 day03 9:40

--- nsd2010/py01/day03/mtable.py
+++ nsd2010/py01/day03/mtable.py
@@ -7,6 +7,18 @@
 #     print('hello', end='\t')
 
 # 在一行内打印3个hello，然后打印回车
+# for i in range(1, 4):
+#     print('hello', end='\t')
+# print()  # print默认在打印结束时再打印一个\n
+
+# 将上面的代码当成一个整体，执行3遍
+# for i in range(1, 4):
+#     for j in range(1, 4):
+#         print('hello', end='\t')
+#     print()
+
+# 第1行打印1个hello，第2行2个，第3行3个，第i行i个
 for i in range(1, 4):
-    print('hello', end='\t')
-print()  # print默认在打印结束时再打印一个\n
+    for j in range(1, i + 1):
+        print('hello', end='\t')
+    print()
