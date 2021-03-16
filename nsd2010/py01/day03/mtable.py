9ca4778145851a6c767b40b025428bd0da190951
root <root@localhost.localdomain>
2021-03-16 09:49:09 +0800

    nsd2010 py01 day03 9:50

--- nsd2010/py01/day03/mtable.py
+++ nsd2010/py01/day03/mtable.py
@@ -18,7 +18,12 @@
 #     print()
 
 # 第1行打印1个hello，第2行2个，第3行3个，第i行i个
-for i in range(1, 4):
+# for i in range(1, 4):
+#     for j in range(1, i + 1):
+#         print('hello', end='\t')
+#     print()
+
+for i in range(1, 10):
     for j in range(1, i + 1):
-        print('hello', end='\t')
+        print(f'{j}x{i}={i*j}', end='\t')
     print()
