ec93adf11ca6fc97d1fd67ae2b99d9a2ff5ca655
root <root@localhost.localdomain>
2021-03-16 09:27:55 +0800

    nsd2010 py01 day03 9:30

---
+++ nsd2010/py01/day03/mtable.py
@@ -0,0 +1,12 @@
+# 打印3个hello
+# for i in range(1, 4):
+#     print('hello')
+
+# 在一行内打印3个hello，print默认在打印结束时再打印一个\n，可以通过end=进行修改
+# for i in range(1, 4):
+#     print('hello', end='\t')
+
+# 在一行内打印3个hello，然后打印回车
+for i in range(1, 4):
+    print('hello', end='\t')
+print()  # print默认在打印结束时再打印一个\n
