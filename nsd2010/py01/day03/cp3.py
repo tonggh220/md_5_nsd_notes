79c06800371051c9cb469e1f79e8e35b9c69c6d3
root <root@localhost.localdomain>
2021-03-16 16:54:00 +0800

    nsd2010 py01 day03 16:50

---
+++ nsd2010/py01/day03/cp3.py
@@ -0,0 +1,16 @@
+import sys
+
+def copy(src_fname, dst_fname):
+    src_fobj = open(src_fname, 'rb')
+    dst_fobj = open(dst_fname, 'wb')
+
+    while 1:
+        data = src_fobj.read(4096)  # 从源文件中最多读取4096字节
+        if not data:  # 任何空对象都是假，非空为真
+            break
+        dst_fobj.write(data)
+
+    src_fobj.close()
+    dst_fobj.close()
+
+copy(sys.argv[1], sys.argv[2])
