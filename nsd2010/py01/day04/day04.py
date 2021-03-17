c8c4eadbae9ce785269688b8595d8a3f6d89154e
root <root@localhost.localdomain>
2021-03-17 15:53:45 +0800

    nsd2010 py01 day04 15:50

--- nsd2010/py01/day04/day04.py
+++ nsd2010/py01/day04/day04.py
@@ -1,7 +1,16 @@
-import shutil
-import string
+names = ['tom', 'jerry', 'jack', 'rose']
 
-shutil.copyfileobj()
-shutil.copy()
-shutil.chown()
+# for i in [0, 1, 2, 3]:
+#     print(i, names[i])
 
+# for i in range(4):
+#     print(i, names[i])
+
+# for i in range(len(names)):
+#     print(i, names[i])
+
+# for data in enumerate(names):
+#     print(data)
+
+for i, name in enumerate(names):
+    print(i, name)
