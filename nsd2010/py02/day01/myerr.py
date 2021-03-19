de50c81c8f50884d101cfdff74bda0c923116681
root <root@localhost.localdomain>
2021-03-19 14:24:38 +0800

    nsd2010 py02 day01 14:25

--- nsd2010/py02/day01/myerr.py
+++ nsd2010/py02/day01/myerr.py
@@ -3,5 +3,10 @@ def get_info(name, age):
         raise ValueError('年龄超出范围(1~119)')
     print(f'{name} is {age} years old.')
 
+def get_info2(name, age):
+    assert 0 < age < 120, '年龄超出范围(1~119)'
+    print(f'{name} is {age} years old.')
+
 if __name__ == '__main__':
-    get_info('nb', 200)
+    get_info('nb', 20)
+    get_info2('plj', 188)
