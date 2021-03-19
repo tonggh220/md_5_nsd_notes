6f24ab17794384316b507a51324ca7ff9a4a82e2
root <root@localhost.localdomain>
2021-03-19 11:19:23 +0800

    nsd2010 py02 day01 11:25

---
+++ nsd2010/py02/day01/cut_log2.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+# 定义9点和12点的时间
+t9 = datetime.strptime('2019-05-15 09:00:00', '%Y-%m-%d %H:%M:%S')
+t12 = datetime.strptime('2019-05-15 12:00:00', '%Y-%m-%d %H:%M:%S')
+
+# 遍历文件，如果某一行的时间是在9点到12点之间，则打印
+with open('mylog.txt') as fobj:
+    for line in fobj:
+        t = datetime.strptime(line[:19], '%Y-%m-%d %H:%M:%S')
+        if t > t12:
+            break
+        if t >= t9:
+            print(line, end='')
+
