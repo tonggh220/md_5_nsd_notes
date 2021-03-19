28c1c36df0687944badb703b9cdcf3a82a388767
root <root@localhost.localdomain>
2021-03-19 14:05:50 +0800

    nsd2010 py02 day01 14:05

--- nsd2010/py02/day01/mydiv.py
+++ nsd2010/py02/day01/mydiv.py
@@ -28,6 +28,20 @@
 # print('Done')
 
 ####################################
+# try:
+#     num = int(input("number: "))
+#     result = 100 / num
+# except (ValueError, ZeroDivisionError):
+#     print('请输入非0数字')
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+#     exit(1)
+# else:
+#     print(result)
+#
+# print('Done')
+
+####################################
 try:
     num = int(input("number: "))
     result = 100 / num
@@ -38,6 +52,5 @@ except (KeyboardInterrupt, EOFError):
     exit(1)
 else:
     print(result)
-
-print('Done')
-
+finally:
+    print('Done')
