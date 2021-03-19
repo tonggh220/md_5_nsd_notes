d52c727dd32ec0dfa4b26c3985b364eddd851457
root <root@localhost.localdomain>
2021-03-19 11:56:15 +0800

    nsd2010 py02 day01 11:55

--- nsd2010/py02/day01/mydiv.py
+++ nsd2010/py02/day01/mydiv.py
@@ -1,16 +1,43 @@
+# try:
+#     num = int(input("number: "))
+#     result = 100 / num
+#     print(result)
+# except ValueError:
+#     print('请输入非0数字')
+# except ZeroDivisionError:
+#     print('请输入非0数字')
+# except KeyboardInterrupt:
+#     print('\nBye-bye')
+#     exit(1)
+# except EOFError:
+#     print('\nBye-bye')
+#     exit(1)
+#
+# print('Done')
+####################################
+# try:
+#     num = int(input("number: "))
+#     result = 100 / num
+#     print(result)
+# except (ValueError, ZeroDivisionError):
+#     print('请输入非0数字')
+# except (KeyboardInterrupt, EOFError):
+#     print('\nBye-bye')
+#     exit(1)
+#
+# print('Done')
+
+####################################
 try:
     num = int(input("number: "))
     result = 100 / num
+except (ValueError, ZeroDivisionError):
+    print('请输入非0数字')
+except (KeyboardInterrupt, EOFError):
+    print('\nBye-bye')
+    exit(1)
+else:
     print(result)
-except ValueError:
-    print('请输入非0数字')
-except ZeroDivisionError:
-    print('请输入非0数字')
-except KeyboardInterrupt:
-    print('\nBye-bye')
-    exit(1)
-except EOFError:
-    print('\nBye-bye')
-    exit(1)
 
 print('Done')
+
