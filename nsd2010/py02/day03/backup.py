a7d3766de08b164aeed0c9f97b2450b13097105d
root <root@localhost.localdomain>
2021-03-23 09:33:07 +0800

    nsd2010 py02 day03 9:35

---
+++ nsd2010/py02/day03/backup.py
@@ -0,0 +1,21 @@
+import os
+from time import strftime
+
+def full_backup(src, dst, md5file):
+    '完全备份'
+
+def incr_backup(src, dst, md5file):
+    '增量备份'
+
+if __name__ == '__main__':
+    src = '/tmp/demo/security'
+    dst = '/tmp/demo/backup'
+    md5file = '/tmp/demo/backup/md5.data'
+    # 如果是周一，或者从来没有备份过，则进行完全备份
+    if (strftime('%a') == 'Mon') or (not os.path.exists(md5file)):
+        full_backup(src, dst, md5file)
+    else:
+        incr_backup(src, dst, md5file)
+
+# mkdir -p /tmp/demo/backup
+# cp -r /etc/security /tmp/demo/
