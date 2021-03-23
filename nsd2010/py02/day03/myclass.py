0dc2f639a06b12677bae6f015e3e75d0d2809592
root <root@localhost.localdomain>
2021-03-23 14:07:41 +0800

    nsd2010 py02 day03 14:10

--- nsd2010/py02/day03/myclass.py
+++ nsd2010/py02/day03/myclass.py
@@ -7,8 +7,14 @@ class Role:
         self.mp = mp
         self.weapon = weapon
 
+    def show_me(self):
+        # 绑定在实例身上的属性，在类中任意位置可见可用
+        print(f'我是{self.name}，擅用{self.weapon}')
+
 if __name__ == '__main__':
     lb = Role('吕布', 500, 200, '方天画戟')  # 自动调用__init__，创建具体的实例对象
     print(lb.name, lb.mp, lb.hp, lb.weapon)
+    lb.show_me()
     zf = Role('张飞', 500, 200, '丈八蛇矛')
     print(zf.name, zf.mp, zf.hp, zf.weapon)
+    zf.show_me()
