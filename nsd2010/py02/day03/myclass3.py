5c113278580e3beaed0cbb48fbfeb1ca28383a42
root <root@localhost.localdomain>
2021-03-23 14:58:35 +0800

    nsd2010 py02 day03 15:00

--- nsd2010/py02/day03/myclass3.py
+++ nsd2010/py02/day03/myclass3.py
@@ -13,13 +13,17 @@ class Role:
         print(hh, words)
 
 class Warrior(Role):  # 括号中的Role是该class的父类，也叫基类
-    pass
+    def attack(self, target):
+        print(f'与{target}近身肉搏')
 
 class Mage(Role):
-    pass
+    def attack(self, target):
+        print(f'远程打击：{target}')
 
 if __name__ == '__main__':
     lb = Warrior('吕布', 500, 200, '方天画戟')
     zgl = Mage('诸葛亮', 400, 300, '羽扇')
     lb.show_me()
     zgl.show_me()
+    lb.attack('刘备')
+    zgl.attack('曹操')
