4a5b94cc8ed51c012f0cb732b7bf3a4b37fdc549
root <root@localhost.localdomain>
2021-03-23 14:20:33 +0800

    nsd2010 py02 day03 14:20

--- nsd2010/py02/day03/myclass.py
+++ nsd2010/py02/day03/myclass.py
@@ -11,10 +11,16 @@ class Role:
         # 绑定在实例身上的属性，在类中任意位置可见可用
         print(f'我是{self.name}，擅用{self.weapon}')
 
+    def speak(self, words):
+        hh = 'hehe'
+        print(hh, words)
+
 if __name__ == '__main__':
     lb = Role('吕布', 500, 200, '方天画戟')  # 自动调用__init__，创建具体的实例对象
     print(lb.name, lb.mp, lb.hp, lb.weapon)
     lb.show_me()
+    lb.speak('马中赤兔，人中吕布')
     zf = Role('张飞', 500, 200, '丈八蛇矛')
     print(zf.name, zf.mp, zf.hp, zf.weapon)
     zf.show_me()
+    zf.speak('吾乃燕人张飞张翼德')
