cc6795ccc21164647501f5db32cde6bb0e954c60
root <root@localhost.localdomain>
2021-03-24 09:29:10 +0800

    nsd2010 py02 day04 9:30

---
+++ nsd2010/py02/day04/count_patt.py
@@ -0,0 +1,13 @@
+
+def count_patt(fname, patt):
+    '在一个文件中，统计某个模式字符串出现的次数'
+
+
+if __name__ == '__main__':
+    fname = 'access_log'
+    ip = ''
+    br = ''
+    result1 = count_patt(fname, ip)
+    result2 = count_patt(fname, br)
+    print(result1)
+    print(result2)
