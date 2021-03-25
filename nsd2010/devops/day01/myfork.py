849e98a04a5556d8a5096fab9f0e08c0851331a5
root <root@localhost.localdomain>
2021-03-25 10:16:56 +0800

    nsd2010 devops day01 10:13

--- nsd2010/devops/day01/myfork.py
+++ nsd2010/devops/day01/myfork.py
@@ -1,8 +1,17 @@
 import os
 
+# print('starting...')
+# os.fork()
+# print('hello world!')
+##################################
 print('starting...')
-os.fork()
-print('hello world!')
+ret_val = os.fork()
 
+if ret_val:    # 如果是父进程，值非0，为真；如果是子进程，值是0，为假
+    print('from parent')
+else:
+    print('from child')
+
+print('hello world!')
 
 
