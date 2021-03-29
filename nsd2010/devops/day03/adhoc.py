1bb2f53284ae553c94b1f16073ee404912776b23
root <root@localhost.localdomain>
2021-03-29 14:54:06 +0800

    nsd2010 devops day03 14:55

--- nsd2010/devops/day03/adhoc.py
+++ nsd2010/devops/day03/adhoc.py
@@ -7,30 +7,37 @@ from ansible.playbook.play import Play
 from ansible.executor.task_queue_manager import TaskQueueManager
 import ansible.constants as C
 
-# since API is constructed for CLI it expects certain options to always be set, named tuple 'fakes' the args parsing options object
-Options = namedtuple('Options', ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'diff'])
-options = Options(connection='local', module_path=['/to/mymodules'], forks=10, become=None, become_method=None, become_user=None, check=False, diff=False)
+# ansible adhoc选项。connection是连接方式，可以设置为local表示本地执行，ssh表示ssh执行，smart表示自动选择，一般也会选择ssh
+# forks表示多少台机器分成一批，执行命令
+Options = namedtuple('Options',
+                     ['connection', 'module_path', 'forks', 'become', 'become_method', 'become_user', 'check', 'diff'])
+options = Options(connection='smart', module_path=['/to/mymodules'], forks=10, become=None, become_method=None,
+                  become_user=None, check=False, diff=False)
+# options = Options(connection='local', module_path=['/to/mymodules'], forks=10, become=None, become_method=None, become_user=None, check=False, diff=False)
 
-# initialize needed objects
-loader = DataLoader() # Takes care of finding and reading yaml, json and ini files
+# loader负责分析ini/yaml/json等格式的文件，并将其转换成python可以识别的形式
+loader = DataLoader()  # Takes care of finding and reading yaml, json and ini files
+# vault password
 passwords = dict(vault_pass='secret')
 
-# create inventory, use path to host config file as source or hosts in a comma separated string
-inventory = InventoryManager(loader=loader, sources='localhost,')
+# 主机清单，可以将所有被管的主机用逗号分隔，形成字符串
+# 也可以使用主机清单文件
+inventory = InventoryManager(loader=loader, sources=['myansible/hosts'])
+# inventory = InventoryManager(loader=loader, sources='localhost,')
 
-# variable manager takes care of merging all the different sources to give you a unifed view of variables available in each context
+# 变量管理器
 variable_manager = VariableManager(loader=loader, inventory=inventory)
 
-# create datastructure that represents our play, including tasks, this is basically what our YAML loader does internally.
-play_source =  dict(
-        name = "Ansible Play",
-        hosts = 'localhost',
-        gather_facts = 'no',
-        tasks = [
-            dict(action=dict(module='shell', args='ls'), register='shell_out'),
-            dict(action=dict(module='debug', args=dict(msg='{{shell_out.stdout}}')))
-         ]
-    )
+# 将收集的信息组合成一个Play
+play_source = dict(
+    name="Ansible Play",
+    hosts='webservers',   # 在哪些主机上执行任务
+    gather_facts='no',   # 不收集facts变量
+    tasks=[              # 在目标主机上执行的任务
+        dict(action=dict(module='shell', args='ls'), register='shell_out'),
+        dict(action=dict(module='debug', args=dict(msg='{{shell_out.stdout}}')))
+    ]
+)
 
 # Create play object, playbook objects use .load instead of init or new methods,
 # this will also automatically create the task objects from the info provided in play_source
@@ -40,13 +47,13 @@ play = Play().load(play_source, variable_manager=variable_manager, loader=loader
 tqm = None
 try:
     tqm = TaskQueueManager(
-              inventory=inventory,
-              variable_manager=variable_manager,
-              loader=loader,
-              options=options,
-              passwords=passwords,
-          )
-    result = tqm.run(play) # most interesting data for a play is actually sent to the callback's methods
+        inventory=inventory,
+        variable_manager=variable_manager,
+        loader=loader,
+        options=options,
+        passwords=passwords,
+    )
+    result = tqm.run(play)  # most interesting data for a play is actually sent to the callback's methods
 finally:
     # we always need to cleanup child procs and the structres we use to communicate with them
     if tqm is not None:
