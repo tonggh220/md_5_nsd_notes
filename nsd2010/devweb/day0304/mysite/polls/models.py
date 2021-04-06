9a12b762d2c5778365191aac9d39f15f2c3a8666
root <root@localhost.localdomain>
2021-04-06 16:52:09 +0800

    nsd2010 devweb day03 16:50

--- nsd2010/devweb/day0304/mysite/polls/models.py
+++ nsd2010/devweb/day0304/mysite/polls/models.py
@@ -3,3 +3,9 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)  # varchar(200)
     pub_date = models.DateTimeField()  # 日期时间
+
+class Choice(models.Model):
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    # models.CASCADE表示级联删除，也就是在删除问题的时候，把相关的选项也一并删除
+    q = models.ForeignKey(Question, on_delete=models.CASCADE)
