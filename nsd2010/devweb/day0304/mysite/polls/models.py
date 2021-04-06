57c89e3e6d4b27e10a4f265b1f953395b0468476
root <root@localhost.localdomain>
2021-04-06 17:40:13 +0800

    nsd2010 devweb day03 17:40

--- nsd2010/devweb/day0304/mysite/polls/models.py
+++ nsd2010/devweb/day0304/mysite/polls/models.py
@@ -4,6 +4,9 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)  # varchar(200)
     pub_date = models.DateTimeField()  # 日期时间
 
+    def __str__(self):
+        return f'问题:{self.question_text}'
+
 class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
