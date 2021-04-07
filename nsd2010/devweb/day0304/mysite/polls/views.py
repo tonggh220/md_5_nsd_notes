10c046a9d0161c753cded0f939177e7ecf7f2fc4
root <root@localhost.localdomain>
2021-04-07 17:21:50 +0800

    nsd2010 devweb day04 17:20

--- nsd2010/devweb/day0304/mysite/polls/views.py
+++ nsd2010/devweb/day0304/mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from polls.models import Question
 
 # 用户发起的请求，请求将会成为一个对象，作为第一个参数传给函数
@@ -21,11 +21,21 @@ def result(request, qid):   # qid用于接收来自于url的参数
     return render(request, 'result.html', {'qid': qid})
 
 def vote(request, qid):
-    print('#' * 50)
-    print(dir(request))  # 查看request有哪些属性
-    print('#' * 50)
-    print(request.method)  # 查看request使用的方法
-    print('#' * 50)
-    print(request.POST)    # request.POST是一个类字典对象，记录表单数据
-    print('#' * 50)
-    return render(request, 'result.html')
+    question = Question.objects.get(id=qid)
+    # 从request中取出用户的选项
+    choice_id = request.POST.get('choice_id')
+    # 取出相应的选项实例，并将票数加1
+    choice = question.choice_set.get(id=choice_id)
+    choice.votes += 1
+    choice.save()
+    # 投票结束后，跳转到结果页
+    return redirect('result', question.id)
+
+    # print('#' * 50)
+    # print(dir(request))  # 查看request有哪些属性
+    # print('#' * 50)
+    # print(request.method)  # 查看request使用的方法
+    # print('#' * 50)
+    # print(request.POST)    # request.POST是一个类字典对象，记录表单数据
+    # print('#' * 50)
+    # return render(request, 'result.html')
