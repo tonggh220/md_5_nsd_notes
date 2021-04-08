cd637bdf97800ce81c5d47cbe7539278c92a100a
root <root@localhost.localdomain>
2021-04-08 17:25:46 +0800

    nsd2010 devweb day05 17:25

--- nsd2010/devweb/ansible_project/myansible/webadmin/views.py
+++ nsd2010/devweb/ansible_project/myansible/webadmin/views.py
@@ -6,5 +6,15 @@ def index(request):
     return render(request, 'webadmin/index.html')
 
 def add_hosts(request):
+    if request.method == 'POST':  # 如果用户提交表单
+        group = request.POST.get('group').strip()
+        host = request.POST.get('host').strip()
+        ip = request.POST.get('ip').strip()
+        if group:  # 如果group非空
+            # 创建或取出组实例
+            g = HostGroup.objects.get_or_create(groupname=group)[0]
+            if host and ip:  # 如果host和ip也是非空的
+                g.host_set.get_or_create(hostname=host, ip_addr=ip)
+
     groups = HostGroup.objects.all()
     return render(request, 'webadmin/add_hosts.html', {'groups': groups})
