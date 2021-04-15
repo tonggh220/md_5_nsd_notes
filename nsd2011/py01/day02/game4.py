a2d6d61c0c293c418dd46d862494215a1b9be91d
root <root@localhost.localdomain>
2021-04-15 15:30:46 +0800

    nsd2011 py01 day02 15:30

---
+++ nsd2011/py01/day02/game4.py
@@ -0,0 +1,26 @@
+import random
+
+all_choice = ['石头', '剪刀', '布']
+win_list = [['石头', '剪刀'], ['剪刀', '布'], ['布', '石头']]
+prompt = '''(0) 石头
+(1) 剪刀
+(2) 布
+请选择(0/1/2): '''
+pwin = 0  # 玩家的计分牌
+cwin = 0  # 计算机的计分牌
+
+while pwin < 2 and cwin < 2:  # 人机都没有赢够2次就继续
+    computer = random.choice(all_choice)
+    i = int(input(prompt))   # 获取用户输入的数字
+    player = all_choice[i]   # 根据下标取出对应的值
+    # 输出人机的选择, 字符串加上前缀f，可以在字符串中使用{}来书写变量
+    print(f'计算机出拳:{computer}, 你出拳:{player}')
+
+    if player == computer:
+        print('\033[32;1m平局\033[0m')
+    elif [player, computer] in win_list:
+        pwin += 1
+        print('\033[31;1mYou WIN!!!\033[0m')
+    else:
+        cwin += 1
+        print('\033[31;1mYou LOSE!!!\033[0m')
