5297e868810924997a11c984bae86ecfa1baedb1
root <root@localhost.localdomain>
2021-04-15 10:29:34 +0800

    nsd2011 py01 day02 10:30

--- nsd2011/py01/day02/day02.py
+++ nsd2011/py01/day02/day02.py
@@ -2,11 +2,26 @@
 #     print('Yes')
 #     print('OK')
 
-if 0.1:
-    print('任何非0的数字都为真')
+# if 0.1:
+#     print('任何非0的数字都为真')
+#
+# if ' ':
+#     print('空格字符也是一个字符，为真')
+#
+# if []:
+#     print('空列表为假')
 
-if ' ':
-    print('空格字符也是一个字符，为真')
+# a = 10
+# b = 20
+# if a < b:
+#     s = a
+# else:
+#     s = b
+# print(s)
 
-if []:
-    print('空列表为假')
+a = 10
+b = 20
+s = a if a < b else b
+print(s)
+
+
---
+++ nsd2011/py01/day02/login2.py
@@ -0,0 +1,7 @@
+username = input('username: ')
+password = input('password: ')
+
+if username == 'bob' and password == '123456':
+    print('Login Successful')
+else:
+    print('Login Failed')
