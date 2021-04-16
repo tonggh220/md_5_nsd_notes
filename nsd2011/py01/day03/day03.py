91b96f27e98d1364ce06f7bd5eebbc29e1b102aa
root <root@localhost.localdomain>
2021-04-16 09:46:46 +0800

    nsd2011 py01 day03 9:50

---
+++ nsd2011/py01/day03/day03.py
@@ -0,0 +1,6 @@
+result = 0
+
+for i in range(1, 11):
+    result += i
+
+print(result)
--- nsd2011/py01/day03/mtable.py
+++ nsd2011/py01/day03/mtable.py
@@ -11,7 +11,20 @@
 # print()   # 打印回车\n
 
 # 将上面打印3行hello的代码作为一个整体，运行3遍
-for i in range(3):
-    for j in range(1, 4):
+# for i in range(3):
+#     for j in range(1, 4):
+#         print('hello', end='\t')
+#     print()
+
+for i in range(1, 4):
+    for j in range(1, i + 1):
         print('hello', end='\t')
     print()
+
+# for i in range(1, 10):
+#     for j in range(1, i + 1):
+#         print(f'{j}x{i}={i * j}', end='\t')
+#     print()
+
+
+
