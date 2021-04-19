96e662856d3cb340a6b253e4d8099bfbc3988cde
root <root@localhost.localdomain>
2021-04-19 16:13:28 +0800

    nsd2011 py01 day04 16:10

--- nsd2011/py01/day04/day04.py
+++ nsd2011/py01/day04/day04.py
@@ -6,5 +6,11 @@ users = ['tom', 'jerry', 'zhangsan', 'lisi']
 # for i in range(4):
 #     print(i, users[i])
 
-for i in range(len(users)):
-    print(i, users[i])
+# for i in range(len(users)):
+#     print(i, users[i])
+
+# for data in enumerate(users):
+#     print(data)
+
+for i, user in enumerate(users):
+    print(i, user)
