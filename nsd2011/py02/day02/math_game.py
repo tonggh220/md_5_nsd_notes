09a70ba5dc2c4a73838d2d32e5de8c6f73f3c4c8
root <root@localhost.localdomain>
2021-04-22 14:32:29 +0800

    nsd2011 py02 day03 14:30

--- nsd2011/py02/day02/math_game.py
+++ nsd2011/py02/day02/math_game.py
@@ -13,10 +13,30 @@ The Answer:
 Continue(Y/n)? no
 Bye-bye
 '''
+from random import randint, choice
 
 def exam():
     '用于出题，用户作答'
-    print('exam')
+    # 随机生成两个数字
+    nums = [randint(1, 100) for i in range(2)]
+    nums.sort(reverse=True)  # 降序排列
+    # nums.reverse()
+    # 随机取出加减号
+    op = choice('+-')
+    # 计算出标准答案
+    if op == '+':
+        result = nums[0] + nums[1]
+    else:
+        result = nums[0] - nums[1]
+    # 用户作答
+    prompt = f'{nums[0]} {op} {nums[1]} = '
+    answer = int(input(prompt))
+    if answer == result:
+        print('Very Good!!!')
+    else:
+        print('Wrong Answer!!!')
+
+    print('The Answer:\n', f'{prompt}{result}')
 
 def main():
     '主程序代码逻辑'
