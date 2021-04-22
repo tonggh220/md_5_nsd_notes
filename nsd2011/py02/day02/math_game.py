f8d677093214cce96a093a4e8801f6b085ea438b
root <root@localhost.localdomain>
2021-04-22 14:07:27 +0800

    nsd2011 py02 day03 14:10

---
+++ nsd2011/py02/day02/math_game.py
@@ -0,0 +1,24 @@
+'''
+1 + 1 = 2
+Very Good!!!
+Continue(Y/n)? y
+34 + 52 = 100
+Wrong Answer!!!
+34 + 52 = 10
+Wrong Answer!!!
+34 + 52 = 200
+Wrong Answer!!!
+The Answer:
+34 + 52 = 86
+Continue(Y/n)? no
+Bye-bye
+'''
+
+def exam():
+    '用于出题，用户作答'
+
+def main():
+    '主程序代码逻辑'
+
+if __name__ == '__main__':
+    main()
