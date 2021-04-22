c8cab6201855da5937050fa9267d1796fe490ace
root <root@localhost.localdomain>
2021-04-22 14:58:33 +0800

    nsd2011 py02 day03 15:00

--- nsd2011/py02/day02/math_game.py
+++ nsd2011/py02/day02/math_game.py
@@ -33,7 +33,12 @@ def exam():
     # 用户作答
     prompt = f'{nums[0]} {op} {nums[1]} = '
     while i < 3:
-        answer = int(input(prompt))
+        try:
+            answer = int(input(prompt))
+        except:  # 裸except语句，可以捕获所有异常。不推荐
+            print()  # 打印回车
+            continue
+
         if answer == result:
             print('Very Good!!!')
             break
@@ -46,7 +51,13 @@ def main():
     '主程序代码逻辑'
     while 1:
         exam()
-        yn = input('Continue(Y/n)? ').strip()[0]  # 取出第一个字符
+        try:
+            yn = input('Continue(Y/n)? ').strip()[0]  # 取出第一个字符
+        except (KeyboardInterrupt, EOFError):
+            yn = 'n'
+        except IndexError:
+            continue
+
         if yn in 'nN':
             print('\nBye-bye')
             break
