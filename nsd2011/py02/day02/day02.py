1357adb858998c35913b30df5531605da309464d
root <root@localhost.localdomain>
2021-04-22 15:59:31 +0800

    nsd2011 py02 day03 16:00

--- nsd2011/py02/day02/day02.py
+++ nsd2011/py02/day02/day02.py
@@ -8,17 +8,29 @@
 # if __name__ == '__main__':
 #     func1()
 
+# from random import randint
+#
+# def func1(x):
+#     return x % 2   # x%2的结果只有1和0两种情况，1为真0为假
+#
+# if __name__ == '__main__':
+#     nums = [randint(1, 100) for i in range(10)]
+#     print(nums)
+#     result = filter(func1, nums)
+#     result2 = filter(lambda x: x % 2, nums)
+#     print(list(result))
+#     print(list(result2))
+
 from random import randint
 
 def func1(x):
-    return x % 2   # x%2的结果只有1和0两种情况，1为真0为假
+    return x + 100
 
 if __name__ == '__main__':
     nums = [randint(1, 100) for i in range(10)]
     print(nums)
-    result = filter(func1, nums)
-    result2 = filter(lambda x: x % 2, nums)
+    result = map(func1, nums)
+    result2 = map(lambda x: x + 100, nums)
     print(list(result))
     print(list(result2))
-
 
