3146a6e5a87fe10c25a7c4347ba46abd623f6170
root <root@localhost.localdomain>
2021-04-22 15:48:46 +0800

    nsd2011 py02 day03 15:50

--- nsd2011/py02/day02/day02.py
+++ nsd2011/py02/day02/day02.py
@@ -1,9 +1,24 @@
-def func1():
-    print('in func1')
-    func2()
+# def func1():
+#     print('in func1')
+#     func2()
+#
+# def func2():
+#     print('in func2')
+#
+# if __name__ == '__main__':
+#     func1()
 
-def func2():
-    print('in func2')
+from random import randint
+
+def func1(x):
+    return x % 2   # x%2的结果只有1和0两种情况，1为真0为假
 
 if __name__ == '__main__':
-    func1()
+    nums = [randint(1, 100) for i in range(10)]
+    print(nums)
+    result = filter(func1, nums)
+    result2 = filter(lambda x: x % 2, nums)
+    print(list(result))
+    print(list(result2))
+
+
