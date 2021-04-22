592dadc4cfd3ac4744c95fb3deccde11f200ffa8
root <root@localhost.localdomain>
2021-04-22 14:17:43 +0800

    nsd2011 py02 day03 14:20

--- nsd2011/py02/day02/math_game.py
+++ nsd2011/py02/day02/math_game.py
@@ -16,9 +16,16 @@ Bye-bye
 
 def exam():
     '用于出题，用户作答'
+    print('exam')
 
 def main():
     '主程序代码逻辑'
+    while 1:
+        exam()
+        yn = input('Continue(Y/n)? ').strip()[0]  # 取出第一个字符
+        if yn in 'nN':
+            print('\nBye-bye')
+            break
 
 if __name__ == '__main__':
     main()
