9858aac74f868da9fb589d1329d77de198b48aa9
root <root@localhost.localdomain>
2021-04-23 14:45:40 +0800

    nsd2011 py02 day03 14:50

---
+++ nsd2011/py02/day03/myclass2.py
@@ -0,0 +1,18 @@
+class Role:
+    def __init__(self, name, weapon, hp=500, mp=300):
+        self.name = name
+        self.weapon = weapon
+        self.hp = hp
+        self.mp = mp
+
+class Weapon:
+    def __init__(self, wname, strength, type):
+        self.wname = wname
+        self.strength = strength
+        self.type = type
+
+if __name__ == '__main__':
+    ji = Weapon('方天画戟', 100, '物理')
+    print(ji.wname, ji.strength, ji.type)
+    lb = Role('吕布', ji)
+    print(lb.weapon.wname, lb.weapon.strength, lb.weapon.type)
