4b06c90dc1651cd1ac35de21a2a9ca3d34e1774b
root <root@localhost.localdomain>
2021-04-23 16:29:01 +0800

    nsd2011 py02 day03 16:30

--- nsd2011/py02/day03/book.py
+++ nsd2011/py02/day03/book.py
@@ -8,6 +8,11 @@ class Book:
         '打印、显示实例时，展示这个方法返回的字符串'
         return f'《{self.title}》'
 
+    def __call__(self):
+        '将实例作为函数调用时，执行此方法'
+        print(f'《{self.title}》是{self.author}编写的')
+
 if __name__ == '__main__':
     linux = Book('运维之道', '丁明一')  # 调用__init__
     print(linux)      # 调用__str__
+    linux()           # 调用__call__
