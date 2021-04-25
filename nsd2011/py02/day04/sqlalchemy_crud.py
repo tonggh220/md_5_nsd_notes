f4ca29ebbbf4f1c1336d43801c7c2106b3208c44
root <root@localhost.localdomain>
2021-04-25 16:22:05 +0800

    nsd2011 py02 day04 16:20

--- nsd2011/py02/day04/sqlalchemy_crud.py
+++ nsd2011/py02/day04/sqlalchemy_crud.py
@@ -4,8 +4,18 @@ from dbconn import Session, Department, Employee
 session = Session()
 #########################################
 # 通过会话实例对数据库进行增删改查
-
-
+#########################################
+# 增加一个部门
+hr = Department(id=1, dep_name='人事部')
+session.add(hr)
+# 新增多个部门
+ops = Department(id=2, dep_name='运维部')
+dev = Department(id=3, dep_name='开发部')
+qa = Department(id=4, dep_name='测试部')
+finance = Department(id=5, dep_name='财务部')
+sales = Department(id=6, dep_name='销售部')
+market = Department(id=7, dep_name='市场部')
+session.add_all([ops, dev, qa, finance, sales, market])
 
 #########################################
 # 确认
