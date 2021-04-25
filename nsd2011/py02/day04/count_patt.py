25f27429f04257b47155c9229a174fe039fd9c30
root <root@localhost.localdomain>
2021-04-25 09:17:50 +0800

    nsd2011 py02 day04 9:20

---
+++ nsd2011/py02/day04/count_patt.py
@@ -0,0 +1,12 @@
+
+def count_patt(fname, patt):
+
+
+if __name__ == '__main__':
+    weblog = 'access_log'
+    ip = ''
+    br = ''
+    result1 = count_patt(weblog, ip)
+    result2 = count_patt(weblog, br)
+    print(result1)
+    print(result2)
