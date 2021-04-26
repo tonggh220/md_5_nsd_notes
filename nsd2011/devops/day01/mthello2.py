8bce0831a74dfa5bd7f7685f17b6122f6c9d9380
root <root@localhost.localdomain>
2021-04-26 11:47:13 +0800

    nsd2011 devops day01 11:45

---
+++ nsd2011/devops/day01/mthello2.py
@@ -0,0 +1,12 @@
+import threading
+import time
+
+def hello(word):
+    time.sleep(3)
+    print(f'Hello {word}!')
+
+if __name__ == '__main__':
+    for w in ['World', 'China', 'Tedu']:
+        t = threading.Thread(target=hello, args=(w,))  # 创建工作线程
+        t.start()  # 启动工作线程，相当于调用target(*args)
+        # hello()
