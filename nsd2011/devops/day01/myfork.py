f4869f471666762eedcc883c112cfeb68b91234f
root <root@localhost.localdomain>
2021-04-26 09:57:13 +0800

    nsd2011 devops day01 9:55

--- nsd2011/devops/day01/myfork.py
+++ nsd2011/devops/day01/myfork.py
@@ -1,6 +1,15 @@
 import os
 
-print('starting...')
-os.fork()
-print('Hello World!')
+# print('starting...')
+# os.fork()
+# print('Hello World!')
 
+print('starting...')
+
+rc = os.fork()  # os.fork()的返回值是整数数字
+if rc:  # 如果是父进程，值非0，为真；如果是子进程，值是0，为假
+    print('Hello from parent')
+else:
+    print('Hello from child')
+
+print('Hello World!')
