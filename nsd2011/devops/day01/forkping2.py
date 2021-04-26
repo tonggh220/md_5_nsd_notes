4e6d070dbe1210165f23dc2dc1c0b70a2ec80bad
root <root@localhost.localdomain>
2021-04-26 10:58:02 +0800

    nsd2011 devops day01 10:55

---
+++ nsd2011/devops/day01/forkping2.py
@@ -0,0 +1,22 @@
+import subprocess
+import os
+import time
+
+def ping(host):
+    result = subprocess.run(
+        f'ping -c2 {host} &> /dev/null', shell=True
+    )
+    if result.returncode == 0:
+        print(f'{host}:up')
+    else:
+        print(f'{host}:down')
+
+if __name__ == '__main__':
+    ips = (f'172.40.0.{i}' for i in range(1, 255))
+    for ip in ips:
+        rc = os.fork()
+        if not rc:
+            ping(ip)
+            exit()
+
+    time.sleep(60)
