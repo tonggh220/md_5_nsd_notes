212309032433b90443d46b3192a286f6fe59e78b
root <root@localhost.localdomain>
2021-04-27 16:54:42 +0800

    nsd2011 devops day02 16:50

--- nsd2011/devops/day02/zabbix.py
+++ nsd2011/devops/day02/zabbix.py
@@ -4,7 +4,25 @@ import json
 url = 'http://192.168.1.100/zabbix/api_jsonrpc.php'
 headers = {'Content-Type': 'application/json; charset=UTF-8'}
 ######################################
-data = {}
+# 对于不需要认证即可获得的信息，可以直接查
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "apiinfo.version",
+#     "params": [],
+#     "id": 1
+# }
+##################################
+# 获取隐私信息，需要token。
+# 获取用户的token: 10b50e8ba54728a0b976fc1ce3ea74c5
+data = {
+    "jsonrpc": "2.0",
+    "method": "user.login",
+    "params": {
+        "user": "Admin",
+        "password": "zabbix"
+    },
+    "id": 1
+}
 ######################################
 r = requests.post(url, headers=headers, data=json.dumps(data))
 print(r.json())  # 只关心result对应的部分
