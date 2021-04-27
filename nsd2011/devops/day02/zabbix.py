b2e600299bb1bc6ea90ac3ec08e4f4b6cbab30f2
root <root@localhost.localdomain>
2021-04-27 17:40:53 +0800

    nsd2011 devops day02 17:40

--- nsd2011/devops/day02/zabbix.py
+++ nsd2011/devops/day02/zabbix.py
@@ -25,24 +25,74 @@ headers = {'Content-Type': 'application/json; charset=UTF-8'}
 # }
 ##################################
 # 查询组，获取Linux servers组ID: 2
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "hostgroup.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "name": [
+#                 # "Zabbix servers",
+#                 "Linux servers"
+#             ]
+#         }
+#     },
+#     "auth": "10b50e8ba54728a0b976fc1ce3ea74c5",
+#     "id": 1
+# }
+##################################
+# 获取Template os linux模板信息: 10001
+# data = {
+#     "jsonrpc": "2.0",
+#     "method": "template.get",
+#     "params": {
+#         "output": "extend",
+#         "filter": {
+#             "host": [
+#                 "Template OS Linux",
+#                 # "Template OS Windows"
+#             ]
+#         }
+#     },
+#     "auth": "10b50e8ba54728a0b976fc1ce3ea74c5",
+#     "id": 1
+# }
+##################################
+# 新建名为myweb1的主机，在Linux servers组中，应用Template os linux模板
 data = {
     "jsonrpc": "2.0",
-    "method": "hostgroup.get",
+    "method": "host.create",
     "params": {
-        "output": "extend",
-        "filter": {
-            "name": [
-                # "Zabbix servers",
-                "Linux servers"
-            ]
+        "host": "myweb1",
+        "interfaces": [  # 通过哪种方式进行监控
+            {
+                "type": 1,
+                "main": 1,
+                "useip": 1,
+                "ip": "192.168.1.10",
+                "dns": "",
+                "port": "10050"
+            }
+        ],
+        "groups": [
+            {
+                "groupid": "2"
+            }
+        ],
+        "templates": [
+            {
+                "templateid": "10001"
+            }
+        ],
+        "inventory_mode": 0,
+        "inventory": {
+            "macaddress_a": "01234",
+            "macaddress_b": "56768"
         }
     },
     "auth": "10b50e8ba54728a0b976fc1ce3ea74c5",
     "id": 1
 }
-##################################
-# 获取Template os linux模板信息
-
 ######################################
 r = requests.post(url, headers=headers, data=json.dumps(data))
 print(r.json())  # 只关心result对应的部分
