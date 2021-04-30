e00648a8d1ca2e3248e53bfe201eb67ffa69c5aa
root <root@localhost.localdomain>
2021-04-30 15:18:52 +0800

    nsd2011 devops day05 15:20

---
+++ nsd2011/devops/day05/deploy.py
@@ -0,0 +1,23 @@
+
+def has_new_ver(ver_url, ver_fname):
+    '判断是否有新版本，有新版本返回True，否则返回False'
+
+
+
+if __name__ == '__main__':
+    # 判断是否有新版本，没有新版本则退出
+    ver_url = 'http://192.168.1.103/deploy/live_ver'
+    ver_fname = '/var/www/deploy/live_ver'
+    if not has_new_ver(ver_url, ver_fname):
+        print('未发现新版本')
+        exit(1)
+
+
+    # 下载新版本软件
+
+    # 判断文件是否损坏，如已损坏则删除它并退出
+
+    # 部署软件
+
+    # 更新本地版本
+
